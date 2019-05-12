b6cb51880aaedbe088fc0bceb08a0ca61591b17b
Michael Beaton <beatonma@gmail.com>
2019-05-12 18:36:59 +0100

    Disk IO utilities. Implemented Context.copyFile extension function.

---
+++ app/src/main/java/org/beatonma/commons/io/Disk.kt
@@ -0,0 +1,26 @@
+@file:JvmName("Disk")
+
+package org.beatonma.commons.io
+
+import android.content.Context
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+
+suspend fun Context.copyFile(sourcePath: String, targetPath: String): Long {
+    val sourceFile = File(sourcePath)
+    if (!sourceFile.exists()) throw FileNotFoundException("source path='$sourcePath' [target path='$targetPath']")
+
+    val inputStream = assets.open(sourcePath)
+    val outputStream = FileOutputStream(targetPath)
+
+    val bytesCopied = inputStream.copyTo(outputStream, bufferSize = 8 * 1024)
+    inputStream.close()
+
+    outputStream.apply {
+        flush()
+        close()
+    }
+
+    return bytesCopied
+}
