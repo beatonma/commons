fded7dca35c3ba07e3ea4d64cbcef1a7bc1c6e7c
Michael Beaton <beatonma@gmail.com>
2019-05-17 16:08:45 +0100

    Converted Groovy -> Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven(url = "http://localhost:8081/artifactory/gradle-release") {
+            val artifactory_username: String by project
+            val artifactory_password: String by project
+            credentials {
+                username = artifactory_username
+                password = artifactory_password
+            }
+        }
+    }
+}
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
+}
