7a6072e8ec87fc9d9703efde2dec3622f10cbe0e
Michael Beaton <beatonma@gmail.com>
2019-05-18 15:43:06 +0100

    Implemented dagger injection for database

--- app/src/main/java/org/beatonma/commons/CommonsApplication.kt
+++ app/src/main/java/org/beatonma/commons/CommonsApplication.kt
@@ -1,9 +1,24 @@
-@file:JvmName("CommonsApplication")
-
 package org.beatonma.commons
 
+import android.app.Activity
 import android.app.Application
+import org.beatonma.commons.app.dagger.AppModule
+import org.beatonma.commons.app.dagger.DaggerAppComponent
+import org.beatonma.commons.data.CommonsDatabase
+import javax.inject.Inject
 
-class CommonsApplication: Application() {
-    // TODO retrofit services for bma, twfy, puk, wiki
+class CommonsApplication : Application() {
+    @Inject
+    lateinit var db: CommonsDatabase
+
+    override fun onCreate() {
+        super.onCreate()
+        DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+            .inject(this)
+    }
 }
+
+val Activity.commonsApp
+    get() = application as CommonsApplication
