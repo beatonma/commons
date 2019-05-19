c6fcb8a4e9dc04a80d8535053e528906e042785d
Michael Beaton <beatonma@gmail.com>
2019-05-19 15:18:39 +0100

    Functions for getting info about the latest git commit

---
+++ buildSrc/src/main/java/Git.kt
@@ -0,0 +1,40 @@
+import org.gradle.api.Project
+import java.io.File
+
+private fun String?.letIfEmpty(fallback: String): String {
+    return if (this == null || isEmpty()) {
+        fallback
+    } else {
+        this
+    }
+}
+
+private fun String?.execute(workingDir: File, fallback: String): String {
+    Runtime.getRuntime().exec(this, null, workingDir).let { process ->
+        process.waitFor()
+        return try {
+            process.inputStream.reader().readText().trim().letIfEmpty(fallback)
+        } catch (e: Exception) {
+            fallback
+        }
+    }
+}
+
+fun String.wrapped(char: Char = '\"'): String {
+    return "$char$this$char"
+}
+
+object Git {
+    fun sha(project: Project): String {
+        // query git for the SHA, Tag and commit count. Use these to automate versioning.
+        return "git rev-parse --short HEAD".execute(project.rootDir, "none")
+    }
+
+    fun tag(project: Project): String {
+        return "git describe --tags".execute(project.rootDir, "dev")
+    }
+
+    fun commitCount(project: Project): Int {
+        return "git rev-list --count HEAD".execute(project.rootDir, "0").toInt()
+    }
+}
