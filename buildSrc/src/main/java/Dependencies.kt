f8fc28bb4beb1676ecedd20a7f8264b11da8a056
Michael Beaton <beatonma@gmail.com>
2019-05-19 15:18:01 +0100

    Minor refactoring - all dependencies defined by dependency(), or one of its group proxies [androidx(), bma(), dagger(), etc]

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,16 +1,27 @@
 import org.gradle.api.JavaVersion
 
 private fun dependency(group: String, artifact: String, version: String) = "$group:$artifact:$version"
-private fun androidx(artifact: String, version: String, group: String = artifact) = dependency("androidx.$group", artifact, version)
+private fun androidx(artifact: String, version: String, group: String = artifact) =
+    dependency("androidx.$group", artifact, version)
+
 private fun bma(artifact: String, version: String = Versions.BMA) = dependency("org.beatonma.lib", artifact, version)
-private fun dagger(artifact: String, version: String = Versions.DAGGER) = dependency("com.google.dagger", artifact, version)
-private fun retrofit(artifact: String, version: String = Versions.RETROFIT) = dependency("com.squareup.retrofit2", artifact, version)
-private fun room(artifact: String, version: String = Versions.ROOM) = androidx(group = "room", artifact = artifact, version = version)
+private fun dagger(artifact: String, version: String = Versions.DAGGER) =
+    dependency("com.google.dagger", artifact, version)
+
+private fun glide(artifact: String, version: String = Versions.GLIDE) =
+    dependency("com.github.bumptech.glide", artifact, version)
+
+private fun retrofit(artifact: String, version: String = Versions.RETROFIT) =
+    dependency("com.squareup.retrofit2", artifact, version)
+
+private fun room(artifact: String, version: String = Versions.ROOM) =
+    androidx(group = "room", artifact = artifact, version = version)
 
 object Versions {
     const val BMA = "0.9.29"
     const val COROUTINES = "1.0.1"
     const val DAGGER = "2.22.1"
+    const val GLIDE = "3.7.0"
     val JAVA = JavaVersion.VERSION_1_8
     const val KOTLIN = "1.3.31"
     const val RETROFIT = "2.1.0"
@@ -18,7 +29,6 @@ object Versions {
 }
 
 object Dependencies {
-    val GLIDE = dependency("com.github.bumptech.glide", "glide", "3.7.0")
     val GOOGLE_MATERIAL = dependency("com.google.android.material", "material", "1.1.0-alpha06")
 
     object AndroidX {
@@ -41,21 +51,40 @@ object Dependencies {
 
     object Dagger {
         val DAGGER = dagger("dagger")
-        val SUPPORT =  dagger("dagger-android-support")
+        val RUNTIME = dagger("dagger-android")
+        val SUPPORT = dagger("dagger-android-support")
         val COMPILER = dagger("dagger-compiler")
         val AP = dagger("dagger-android-processor")
+        val SPI = dagger("dagger-spi")
+    }
+
+    object Glide {
+        val COMPILER = glide("compiler")
+
+        val ANNOTATIONS = glide("annotations")
+        val CORE = glide("glide")
+        val OKHTTP = glide("okhttp3-integration")
+        val RECYCLERVIEW = glide("recyclerview-integration")
     }
 
     object Kotlin {
-        val STDLIB = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.KOTLIN}"
-        val REFLECT = "org.jetbrains.kotlin:kotlin-reflect:${Versions.KOTLIN}"
-        val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES}"
-        val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINES}"
+        val STDLIB = dependency("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", Versions.KOTLIN)
+        val REFLECT = dependency("org.jetbrains.kotlin", "kotlin-reflect", Versions.KOTLIN)
+
+        object Coroutines {
+            val CORE = dependency("org.jetbrains.kotlinx", "kotlinx-coroutines-core", Versions.COROUTINES)
+            val ANDROID =
+                dependency("org.jetbrains.kotlinx", "kotlinx-coroutines-android", Versions.COROUTINES)
+        }
     }
 
     object Retrofit {
         val RETROFIT = retrofit("retrofit")
-        val RETROFIT_CONVERTER_GSON = retrofit("converter-gson")
+
+        object Converter {
+            val GSON = retrofit("converter-gson")
+            val TEXT = retrofit("converter-scalars")
+        }
     }
 
     object Room {
@@ -65,9 +94,12 @@ object Dependencies {
     }
 
     object Test {
-        val ANDROIDX_TEST_CORE = androidx(group = "test", artifact = "core", version = "1.1.0")
-        val ANDROIDX_TEST_RUNNER = androidx(group = "test", artifact = "runner", version = "1.1.1-alpha01")
-        val ANDROIDX_TEST_ESPRESSO = androidx(group = "test.espresso", artifact = "espresso-core", version = "3.1.1")
+        object AndroidX {
+            val CORE = androidx(group = "test", artifact = "core", version = "1.1.0")
+            val RUNNER = androidx(group = "test", artifact = "runner", version = "1.1.1-alpha01")
+            val ESPRESSO =
+                androidx(group = "test.espresso", artifact = "espresso-core", version = "3.1.1")
+        }
 
         val BMA_TESTING = bma("testing")
         val MOCKITO = dependency("org.mockito", "mockito-all", "1.10.19")
