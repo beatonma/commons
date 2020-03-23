09cd74a6db0bd58e000100bf1dd14b5a52e36677
Michael Beaton <beatonma@gmail.com>
2020-03-23 16:55:09 +0000

    Updated kotlin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.31"
+    kotlin("jvm") version "1.3.70"
 }
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
