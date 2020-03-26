76930fa98e661d9ec484250ca0d993b2e212c02b
Michael Beaton <beatonma@gmail.com>
2020-03-26 19:44:48 +0000

    Implemented Snippets for weblinks, posts, birthdays

--- app/src/main/java/org/beatonma/commons/app/memberprofile/MemberProfileViewModel.kt
+++ app/src/main/java/org/beatonma/commons/app/memberprofile/MemberProfileViewModel.kt
@@ -1,19 +1,163 @@
 package org.beatonma.commons.app.memberprofile
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import androidx.annotation.StringRes
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.beatonma.commons.CommonsApplication
+import org.beatonma.commons.R
+import org.beatonma.commons.app.ui.datarendering.formatDate
 import org.beatonma.commons.data.IoResult
 import org.beatonma.commons.data.core.CommonsRepository
-import org.beatonma.commons.data.core.room.entities.MemberProfile
+import org.beatonma.commons.data.core.Member
+import org.beatonma.commons.data.core.room.entities.Birthplace
+import org.beatonma.commons.data.core.room.entities.Post
+import org.beatonma.commons.data.core.room.entities.WebAddress
+import org.beatonma.lib.util.kotlin.extensions.autotag
+import org.beatonma.lib.util.kotlin.extensions.stringCompat
+import java.util.*
 import javax.inject.Inject
 
 
 class MemberProfileViewModel @Inject constructor(
-    private val repository: CommonsRepository
-) : ViewModel() {
-    lateinit var member: LiveData<IoResult<MemberProfile>>
+    private val repository: CommonsRepository,
+    private val application: CommonsApplication
+) : AndroidViewModel(application) {
+
+    // Data
+    lateinit var member: LiveData<IoResult<Member>>
+    lateinit var weblinks: LiveData<List<WebAddress>>
+    lateinit var posts: LiveData<List<Post>>
+
+    val snippets: MutableLiveData<List<Snippet>> = MutableLiveData()
+
+    private val observer = Observer<Any> { generateSnippets() }
+    private val calendar: Calendar = Calendar.getInstance()
+
 
     fun forMember(parliamentdotuk: Int) {
         member = repository.observeMember(parliamentdotuk)
+        member.observeForever(observer)
+
+        weblinks = repository.observeWebAddresses(parliamentdotuk)
+        weblinks.observeForever(observer)
+
+        posts = repository.observePosts(parliamentdotuk)
+        posts.observeForever(observer)
     }
+
+    override fun onCleared() {
+        arrayOf(
+            member,
+            weblinks,
+            posts
+        ).forEach { it.removeObserver(observer) }
+        super.onCleared()
+    }
+
+
+    private fun generateSnippets() {
+        val member = member.value?.data ?: return
+
+        viewModelScope.launch {
+            Log.i(autotag, "Shuffling snippets")
+            snippets.value = listOfNotNull(
+                *memberSnippets(member),
+                *webAddressSnippets(weblinks.value),
+                *postSnippets(posts.value)
+            ).shuffled()
+        }
+    }
+
+    private fun memberSnippets(member: Member?): Array<Snippet> {
+        member ?: return arrayOf()
+        return listOfNotNull(
+            birthSnippet(member)
+        ).toTypedArray()
+    }
+
+    private fun birthSnippet(member: Member): Snippet? {
+        val dob: String? = member.profile.dateOfBirth?.let { formatDate(it) }
+        val place: Birthplace? = member.profile.placeOfBirth
+        val age: Int? = member.profile.age
+
+        application.run {
+            return when {
+                dob != null && place != null && age != null -> Snippet(
+                    stringCompat(R.string.snippet_birth_date, dob),
+                    stringCompat(R.string.snippet_birth_place, place.town, place.country),
+                    subtitle = stringCompat(R.string.snippet_birth_age, age))
+
+                else -> null
+            }
+        }
+    }
+
+    private fun webAddressSnippets(addresses: List<WebAddress>?): Array<Snippet> {
+        addresses ?: return arrayOf()
+        return addresses.map { it }
+            .mapNotNull { webAddressSnippet(it) }
+            .toTypedArray()
+    }
+
+    private fun webAddressSnippet(address: WebAddress): Snippet? {
+        return Snippet(
+            address.description,
+            address.url,
+            clickActionText = stringCompat(R.string.action_open_url),
+            onclick = { context ->
+                context.startActivity(
+                    Intent(Intent.ACTION_VIEW).apply {
+                        data = Uri.parse(address.url)
+                    })
+            }
+        )
+    }
+
+    private fun postSnippets(posts: List<Post>?): Array<Snippet> {
+        posts ?: return arrayOf()
+        return posts.map { post ->
+            val postType = stringCompat(when (post.postType) {
+                Post.PostType.GOVERNMENTAL -> R.string.post_type_governmental
+                Post.PostType.PARLIAMENTARY -> R.string.post_type_parliamentary
+                Post.PostType.OPPOSITION -> R.string.post_type_opposition
+            })
+
+            val dateRange = when {
+                post.start == null -> null
+                post.end == null -> stringCompat(R.string.snippet_date_since, date(post.start))
+                else -> stringCompat(R.string.snippet_date_range, date(post.start), date(post.end))
+            }
+
+            Snippet(
+                post.name,
+                postType,
+                subcontent = dateRange
+            )
+        }.toTypedArray()
+    }
+
+    private inline fun stringCompat(@StringRes resId: Int, vararg formatArgs: Any): String =
+        application.stringCompat(resId, *formatArgs)
+
+    private inline fun date(yyyy_mm__dd: String) = formatDate(yyyy_mm__dd, calendar)
 }
+
+/**
+ * Container for small chunks of information to display in
+ */
+data class Snippet(
+    val title: String,
+    val content: String,
+    val subtitle: String? = null,
+    val subcontent: String? = null,
+    val clickActionText: String? = null, // If onclick is set this text will be displayed on the button
+    val onclick: ((Context) -> Unit)? = null
+)
