1151914de41db89dc56324467ee4233390b877a8
Michael Beaton <beatonma@gmail.com>
2020-03-26 19:48:44 +0000

    Implemented observeMember with caching of profile, party, constituency, addresses, and posts.

--- app/src/main/java/org/beatonma/commons/data/core/CommonsRepository.kt
+++ app/src/main/java/org/beatonma/commons/data/core/CommonsRepository.kt
@@ -3,10 +3,12 @@ package org.beatonma.commons.data.core
 import android.content.Context
 import androidx.lifecycle.LiveData
 import org.beatonma.commons.data.CommonsRemoteDataSource
-import org.beatonma.commons.data.Result
+import org.beatonma.commons.data.IoResult
 import org.beatonma.commons.data.core.room.CommonsDatabase
 import org.beatonma.commons.data.core.room.entities.FeaturedMember
 import org.beatonma.commons.data.core.room.entities.FeaturedMemberProfile
+import org.beatonma.commons.data.core.room.entities.Post
+import org.beatonma.commons.data.core.room.entities.WebAddress
 import org.beatonma.commons.data.resultLiveData
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,7 +21,7 @@ class CommonsRepository @Inject constructor(
 ) {
     private val memberDao = commonsDatabase.memberDao()
 
-    fun observeFeaturedPeople(): LiveData<Result<List<FeaturedMemberProfile>>> = resultLiveData(
+    fun observeFeaturedPeople(): LiveData<IoResult<List<FeaturedMemberProfile>>> = resultLiveData(
         databaseQuery = { memberDao.getFeaturedProfiles() },
         networkCall = { commonsRemoteDataSource.getFeaturedPeople() },
         saveCallResult = { profiles ->
@@ -36,4 +38,38 @@ class CommonsRepository @Inject constructor(
             }
         }
     )
+
+    fun observeMember(parliamentdotuk: Int): LiveData<IoResult<Member>> = resultLiveData(
+        databaseQuery = { memberDao.getMember(parliamentdotuk) },
+        networkCall = { commonsRemoteDataSource.getMember(parliamentdotuk) },
+        saveCallResult = { member ->
+            memberDao.apply {
+                insertParty(member.profile.party)
+                insertConstituency(member.profile.constituency)
+                insertProfile(member.profile)
+
+                insertPhysicalAddresses(
+                    member.addresses.physical?.map { it.copy(personId = parliamentdotuk) }
+                )
+                insertWebAddresses(
+                    member.addresses.web?.map { it.copy(personId = parliamentdotuk) }
+                )
+
+                insertPosts(member.posts.governmental.map { post ->
+                    post.copy(memberId = parliamentdotuk, postType = Post.PostType.GOVERNMENTAL)
+                })
+                insertPosts(member.posts.parliamentary.map { post ->
+                    post.copy(memberId = parliamentdotuk, postType = Post.PostType.PARLIAMENTARY)
+                })
+                insertPosts(member.posts.opposition.map { post ->
+                    post.copy(memberId = parliamentdotuk, postType = Post.PostType.OPPOSITION)
+                })
+            }
+
+        }
+    )
+
+    fun observeWebAddresses(parliamentdotuk: Int): LiveData<List<WebAddress>> = memberDao.getWebAddresses(parliamentdotuk)
+
+    fun observePosts(parliamentdotuk: Int): LiveData<List<Post>> = memberDao.getPosts(parliamentdotuk)
 }
