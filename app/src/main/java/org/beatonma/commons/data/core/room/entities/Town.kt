24c72002ccda7fa0584eab6176f988fe9ded2485
Michael Beaton <beatonma@gmail.com>
2020-03-27 15:40:02 +0000

    Renamed Birthplace -> Town

--- app/src/main/java/org/beatonma/commons/data/core/room/entities/Town.kt
+++ app/src/main/java/org/beatonma/commons/data/core/room/entities/Town.kt
@@ -6,9 +6,9 @@ import com.squareup.moshi.Json
 
 @Entity(
     primaryKeys = ["town_name", "country_name"],
-    tableName = "birthplace"
+    tableName = "towns"
 )
-data class Birthplace(
+data class Town(
     @field:Json(name = "town") @ColumnInfo(name = "town_name") val town: String,
     @field:Json(name = "country") @ColumnInfo(name = "country_name") val country: String = "UK"
 )
