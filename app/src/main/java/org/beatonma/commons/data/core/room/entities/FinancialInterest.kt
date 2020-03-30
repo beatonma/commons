d7d38fd2430ddf59b6e0c53f21c86df8e88878fe
Michael Beaton <beatonma@gmail.com>
2020-03-30 17:33:46 +0100

    Entity for member financial interests

---
+++ app/src/main/java/org/beatonma/commons/data/core/room/entities/FinancialInterest.kt
@@ -0,0 +1,24 @@
+package org.beatonma.commons.data.core.room.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import com.squareup.moshi.Json
+import org.beatonma.commons.data.PARLIAMENTDOTUK
+
+@Entity(
+    primaryKeys = [
+        "interest_member_id",
+        "interest_id"
+    ],
+    tableName = "financial_interests"
+)
+data class FinancialInterest(
+    @ColumnInfo(name = "interest_member_id") val memberId: Int,
+    @field:Json(name = PARLIAMENTDOTUK) @ColumnInfo(name = "interest_id") val interestId: Int,
+    @field:Json(name = "category") @ColumnInfo(name = "interest_category") val category: String,
+    @field:Json(name = "description") @ColumnInfo(name = "interest_description") val description: String,
+    @field:Json(name = "created") @ColumnInfo(name = "interest_created") val created: String?,
+    @field:Json(name = "amended") @ColumnInfo(name = "interest_amended") val dateAmended: String?,
+    @field:Json(name = "deleted") @ColumnInfo(name = "interest_deleted") val dateDeleted: String?,
+    @field:Json(name = "registered_late") @ColumnInfo(name = "interest_registered_late") val registeredLate: Boolean
+)
