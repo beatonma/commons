9c7d2f84900996a92f11c1dabf1d8334c75529be
Michael Beaton <beatonma@gmail.com>
2020-03-30 11:47:40 +0100

    Handled nullable Constituency. Added generic converter for serializing Enums. Added converters for deserializing Post.PostType, HouseMembership.House Enums.

--- app/src/main/java/org/beatonma/commons/data/core/Converters.kt
+++ app/src/main/java/org/beatonma/commons/data/core/Converters.kt
@@ -2,9 +2,7 @@ package org.beatonma.commons.data.core
 
 import androidx.room.TypeConverter
 import com.squareup.moshi.Moshi
-import org.beatonma.commons.data.core.room.entities.Birthplace
-import org.beatonma.commons.data.core.room.entities.Constituency
-import org.beatonma.commons.data.core.room.entities.Party
+import org.beatonma.commons.data.core.room.entities.*
 import java.util.*
 
 object Converters {
@@ -17,23 +15,34 @@ object Converters {
     fun deserializeParty(key: Int): Party? = Party(parliamentdotuk = key, name = "")
 
     @TypeConverter @JvmStatic
-    fun serializeConstituency(obj: Constituency): Int = obj.parliamentdotuk  // Convert to foreign key
+    fun serializeConstituency(obj: Constituency?): Int? = obj?.parliamentdotuk  // Convert to foreign key
 
     @TypeConverter @JvmStatic
-    fun deserializeConstituency(key: Int): Constituency? = Constituency(parliamentdotuk = key, name = "")
+    fun deserializeConstituency(key: Int?): Constituency? = key?.let{ Constituency(parliamentdotuk = key, name = "") }
 
     @TypeConverter @JvmStatic
-    fun serializeBirthplace(town: Birthplace?): String = town.toJson()
+    fun serializeTown(town: Town?): String = town.toJson()
 
     @TypeConverter @JvmStatic
-    fun deserializeBirthplace(json: String): Birthplace? = json.fromJson(
-        Birthplace::class.java)
+    fun deserializeTown(json: String): Town? = json.fromJson(Town::class.java)
 
     @TypeConverter @JvmStatic
     fun fromTimestamp(value: Long?): Date? = value?.let { Date(it) }
 
     @TypeConverter @JvmStatic
     fun dateToTimestamp(date: Date?): Long? = date?.time
+
+    @TypeConverter @JvmStatic
+    fun serializeEnum(type: Enum<*>): Int = type.ordinal
+
+    @TypeConverter @JvmStatic
+    fun deserializePostType(ordinal: Int): Post.PostType? = ordinal.toEnum<Post.PostType>()
+
+    @TypeConverter @JvmStatic
+    fun deserializeHouse(ordinal: Int): HouseMembership.House? = ordinal.toEnum<HouseMembership.House>()
+
+
+    private inline fun <reified T: Enum<T>> Int.toEnum(): T = enumValues<T>()[this]
 
     private inline fun <reified T> T.toJson(): String = moshi.adapter(T::class.java).toJson(this)
 
