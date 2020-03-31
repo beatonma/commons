df5d7af53fbac43e155624fbe997dcea97593d92
Michael Beaton <beatonma@gmail.com>
2020-03-31 11:27:26 +0100

    Added currentPost to MemberProfile. Added new class MinimalProfile with reduced fields

--- app/src/main/java/org/beatonma/commons/data/core/room/entities/MemberProfile.kt
+++ app/src/main/java/org/beatonma/commons/data/core/room/entities/MemberProfile.kt
@@ -13,7 +13,7 @@ data class MemberProfile(
     @PrimaryKey @field:Json(name = PARLIAMENTDOTUK) @ColumnInfo(name = PARLIAMENTDOTUK) val parliamentdotuk: Int,
     @field:Json(name = "name") @ColumnInfo(name = "name") val name: String,
     @field:Json(name = "party") @ColumnInfo(name = "party_id") val party: Party,
-    @field:Json(name = "constituency") @ColumnInfo(name = "constituency_id") val constituency: Constituency,
+    @field:Json(name = "constituency") @ColumnInfo(name = "constituency_id") val constituency: Constituency?,
     @field:Json(name = "active") @ColumnInfo(name = "active") val active: Boolean? = null,
     @field:Json(name = "is_mp") @ColumnInfo(name = "is_mp") val isMp: Boolean? = null,
     @field:Json(name = "is_lord") @ColumnInfo(name = "is_lord") val isLord: Boolean? = null,
@@ -22,11 +22,19 @@ data class MemberProfile(
     @field:Json(name = "date_of_death") @ColumnInfo(name = "date_of_death") val dateOfDeath: String? = null,
     @field:Json(name = "gender") @ColumnInfo(name = "gender") val gender: String? = null,
     @Embedded(prefix = "birth_") @field:Json(name = "place_of_birth") val placeOfBirth: Town? = null,
-    @field:Json(name = "portrait") @ColumnInfo(name = "portrait_url") val portraitUrl: String? = null
+    @field:Json(name = "portrait") @ColumnInfo(name = "portrait_url") val portraitUrl: String? = null,
+    @field:Json(name = "current_post") @ColumnInfo(name = "current_post") val currentPost: String? = null
 )
 
 data class MemberProfileWithRelatedObjects(
-    @Embedded val profile: MemberProfile,
+    @Embedded val profile: MinimalProfile,
     @Embedded val party: Party,
-    @Embedded val constituency: Constituency
+    @Embedded val constituency: Constituency?
+)
+
+data class MinimalProfile(
+    @ColumnInfo(name = PARLIAMENTDOTUK) val parliamentdotuk: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "portrait_url") val portraitUrl: String?,
+    @ColumnInfo(name = "current_post") val currentPost: String?
 )
