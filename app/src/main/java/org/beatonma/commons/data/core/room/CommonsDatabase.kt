d59c9bdcae142deaed51c16c3bd50a0d17367793
Michael Beaton <beatonma@gmail.com>
2020-03-31 12:47:16 +0100

    Add entities CommitteeChairship, Experience, FinancialInterest, HouseMembership, TopicOfInterest Add BillDao

--- app/src/main/java/org/beatonma/commons/data/core/room/CommonsDatabase.kt
+++ app/src/main/java/org/beatonma/commons/data/core/room/CommonsDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import org.beatonma.commons.data.core.Converters
+import org.beatonma.commons.data.core.room.dao.BillDao
 import org.beatonma.commons.data.core.room.dao.MemberDao
 import org.beatonma.commons.data.core.room.entities.*
 
@@ -13,18 +14,24 @@ const val COMMONS_DB_FILENAME = "commons.db"
 @Database(
     entities = [
         MemberProfile::class,
+        CommitteeChairship::class,
         CommitteeMembership::class,
         Constituency::class,
+        Experience::class,
         FeaturedMember::class,
+        FinancialInterest::class,
+        HouseMembership::class,
         Party::class,
         PhysicalAddress::class,
         Post::class,
+        TopicOfInterest::class,
         Town::class,
         WebAddress::class
     ],
-    version = 2
+    version = 1
 )
 @TypeConverters(Converters::class)
 abstract class CommonsDatabase : RoomDatabase() {
     abstract fun memberDao(): MemberDao
+    abstract fun billDao(): BillDao
 }
