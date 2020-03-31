66da9636f9573c64fbf6c11f586c4e61d3e9bc93
Michael Beaton <beatonma@gmail.com>
2020-03-31 12:46:33 +0100

    Provide BillDao

--- app/src/main/java/org/beatonma/commons/data/core/dagger/CommonsDataModule.kt
+++ app/src/main/java/org/beatonma/commons/data/core/dagger/CommonsDataModule.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
+import org.beatonma.commons.CommonsApplication
 import org.beatonma.commons.data.CommonsRemoteDataSource
 import org.beatonma.commons.data.core.CommonsRepository
 import org.beatonma.commons.data.core.room.COMMONS_DB_FILENAME
 import org.beatonma.commons.data.core.room.CommonsDatabase
+import org.beatonma.commons.data.core.room.dao.BillDao
 import org.beatonma.commons.data.core.room.dao.MemberDao
 import org.beatonma.commons.network.HttpClientModule
 import org.beatonma.commons.network.dagger.MoshiModule
@@ -24,21 +26,28 @@ import javax.inject.Singleton
         MoshiModule::class
     ]
 )
-class CommonsDataModule(val context: Context) {
+class CommonsDataModule(val application: CommonsApplication) {
+
+
     @Provides
-    fun providesContext(): Context = context
+    fun providesContext(): Context = application.applicationContext
+
+    @Provides
+    fun providesApplciation(): CommonsApplication = application
 
     @Singleton
     @Provides
     fun provideCommonsDatabase(context: Context): CommonsDatabase = Room.databaseBuilder(
-        context.applicationContext,
-        CommonsDatabase::class.java,
-        COMMONS_DB_FILENAME
-    ).build()
+            context.applicationContext,
+            CommonsDatabase::class.java,
+            COMMONS_DB_FILENAME
+        ).fallbackToDestructiveMigration()
+        .build()
 
     @Singleton
     @Provides
     fun providesCommonsRepository(
+        context: Context,
         commonsDatabase: CommonsDatabase,
         commonsRemoteDataSource: CommonsRemoteDataSource
     ): CommonsRepository = CommonsRepository(context, commonsRemoteDataSource, commonsDatabase)
@@ -52,4 +61,8 @@ class CommonsDataModule(val context: Context) {
     @Singleton
     @Provides
     fun providesPeopleDao(commonsDatabase: CommonsDatabase): MemberDao = commonsDatabase.memberDao()
+
+    @Singleton
+    @Provides
+    fun providesBillDao(commonsDatabase: CommonsDatabase): BillDao = commonsDatabase.billDao()
 }
