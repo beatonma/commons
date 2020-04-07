785e0329675ef5f3ec4663d67288389c8d0ae892
Michael Beaton <beatonma@gmail.com>
2020-04-07 12:05:21 +0100

    Added BillDaoTestSuite to DataTestSuite. Fixed typo in DataTestSuite name :$

--- app/src/androidTest/java/org/beatonma/commons/CommonsTestSuite.kt
+++ app/src/androidTest/java/org/beatonma/commons/CommonsTestSuite.kt
@@ -1,11 +1,11 @@
 package org.beatonma.commons
 
-import org.beatonma.commons.data.DateTestSuite
+import org.beatonma.commons.data.DataTestSuite
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    DateTestSuite::class,
+    DataTestSuite::class,
 )
 class CommonsTestSuite
--- app/src/androidTest/java/org/beatonma/commons/data/DataTestSuite.kt
+++ app/src/androidTest/java/org/beatonma/commons/data/DataTestSuite.kt
@@ -1,11 +1,13 @@
 package org.beatonma.commons.data
 
+import org.beatonma.commons.data.core.room.dao.BillDaoTestSuite
 import org.beatonma.commons.data.core.room.dao.MemberDaoTestSuite
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
+    BillDaoTestSuite::class,
     MemberDaoTestSuite::class,
 )
-class DateTestSuite
+class DataTestSuite
