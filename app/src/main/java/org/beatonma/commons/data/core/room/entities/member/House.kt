688910c19582bfa745bdad8c23e9bacbc6fbe76e
Michael Beaton <beatonma@gmail.com>
2020-04-09 16:19:15 +0100

    Moved House enum out of HouseMembership class.

--- app/src/main/java/org/beatonma/commons/data/core/room/entities/member/House.kt
+++ app/src/main/java/org/beatonma/commons/data/core/room/entities/member/House.kt
@@ -6,6 +6,12 @@ import androidx.room.ForeignKey
 import com.squareup.moshi.Json
 import org.beatonma.commons.data.PARLIAMENTDOTUK
 
+enum class House {
+    Commons,
+    Lords
+}
+
+
 @Entity(
     foreignKeys = [
         ForeignKey(
@@ -28,9 +34,4 @@ data class HouseMembership(
     @ColumnInfo(name = "house_member_id", index = true) val memberId: Int,
     @field:Json(name = "start") @ColumnInfo(name = "house_start") val start: String,
     @field:Json(name = "end") @ColumnInfo(name = "house_end") val end: String?
-) {
-    enum class House {
-        Commons,
-        Lords
-    }
-}
+)
