862df6a1ba54ded0a4cd523f670c2f1826f8237a
Michael Beaton <beatonma@gmail.com>
2020-04-28 11:16:27 +0100

    Moved theme preference check to application onCreate

--- app/src/main/java/org/beatonma/commons/CommonsApplication.kt
+++ app/src/main/java/org/beatonma/commons/CommonsApplication.kt
@@ -3,13 +3,17 @@ package org.beatonma.commons
 import android.app.Activity
 import android.app.Application
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import org.beatonma.commons.app.SystemTheme
 import org.beatonma.commons.app.dagger.AppInjector
 import org.beatonma.commons.data.core.repository.CommonsRepository
+import org.beatonma.lib.util.kotlin.extensions.getPrefs
 import javax.inject.Inject
+
 
 class CommonsApplication : Application(), HasAndroidInjector {
     @Inject lateinit var androidInjector: DispatchingAndroidInjector<Any>
@@ -19,10 +23,25 @@ class CommonsApplication : Application(), HasAndroidInjector {
 
     override fun onCreate() {
         super.onCreate()
+
         AppInjector.init(this)
+
+        initTheme()
     }
 
     override fun androidInjector(): AndroidInjector<Any> = androidInjector
+
+    private fun initTheme() {
+        val forceNight = getPrefs(SystemTheme.THEME_PREFS)
+            .getBoolean(SystemTheme.UI_FORCE_DARK_THEME, false)
+
+        val mode = if (forceNight) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        val current = AppCompatDelegate.getDefaultNightMode()
+
+        if (mode != current) {
+            AppCompatDelegate.setDefaultNightMode(mode)
+        }
+    }
 }
 
 val Activity.commonsApp: CommonsApplication
--- app/src/main/java/org/beatonma/commons/app/DayNightActivity.kt
+++ app/src/main/java/org/beatonma/commons/app/DayNightActivity.kt
@@ -2,33 +2,29 @@ package org.beatonma.commons.app
 
 import android.content.Context
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.core.content.edit
 import dagger.android.support.DaggerAppCompatActivity
 import org.beatonma.lib.util.kotlin.extensions.getPrefs
 
-private const val THEME_PREFS = "theme"
-private const val UI_FORCE_DARK_THEME = "force_dark_theme"
+object SystemTheme {
+    const val THEME_PREFS = "theme"
+    const val UI_FORCE_DARK_THEME = "force_dark_theme"
+}
 
 abstract class DayNightActivity: DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val forceNight = getPrefs(THEME_PREFS).getBoolean(UI_FORCE_DARK_THEME, false)
-
-        AppCompatDelegate.setDefaultNightMode(if (forceNight) MODE_NIGHT_YES else MODE_NIGHT_FOLLOW_SYSTEM)
         delegate.applyDayNight()
     }
 
     fun forceNight(forceNight: Boolean) {
-        getPrefs(THEME_PREFS).edit { putBoolean(UI_FORCE_DARK_THEME, forceNight) }
+        getPrefs(SystemTheme.THEME_PREFS).edit { putBoolean(SystemTheme.UI_FORCE_DARK_THEME, forceNight) }
         recreate()
     }
 
     fun toggleForceNight() {
-        forceNight(!getSharedPreferences(THEME_PREFS, Context.MODE_PRIVATE)
-            .getBoolean(UI_FORCE_DARK_THEME, true))
+        forceNight(!getSharedPreferences(SystemTheme.THEME_PREFS, Context.MODE_PRIVATE)
+            .getBoolean(SystemTheme.UI_FORCE_DARK_THEME, true))
     }
 }
