24aa21384f635ba8eb5f43261b9f1ea6c07bb0a9
Michael Beaton <beatonma@gmail.com>
2020-04-28 15:52:31 +0100

    Basic division detail view

---
+++ app/src/main/java/org/beatonma/commons/app/division/DivisionDetailFragment.kt
@@ -0,0 +1,123 @@
+package org.beatonma.commons.app.division
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import org.beatonma.commons.R
+import org.beatonma.commons.app.ui.BaseViewmodelFragment
+import org.beatonma.commons.data.NetworkError
+import org.beatonma.commons.data.PARLIAMENTDOTUK
+import org.beatonma.commons.data.core.room.entities.division.Division
+import org.beatonma.commons.data.core.room.entities.division.Vote
+import org.beatonma.commons.data.core.room.entities.member.House
+import org.beatonma.commons.databinding.FragmentDivisionProfileBinding
+import org.beatonma.commons.databinding.ItemWideTitleBinding
+import org.beatonma.commons.kotlin.extensions.navigateTo
+import org.beatonma.commons.kotlin.extensions.networkErrorSnackbar
+import org.beatonma.commons.kotlin.extensions.stringCompat
+import org.beatonma.commons.ui.colors.colorResId
+import org.beatonma.lib.ui.recyclerview.BaseRecyclerViewAdapter
+import org.beatonma.lib.ui.recyclerview.BaseViewHolder
+import org.beatonma.lib.ui.recyclerview.kotlin.extensions.setupGrid
+import org.beatonma.lib.util.kotlin.extensions.colorCompat
+import org.beatonma.lib.util.kotlin.extensions.dp
+
+private const val TAG = "DivisionProfileFragment"
+
+class DivisionDetailFragment : BaseViewmodelFragment() {
+    companion object {
+        const val HOUSE = "house"
+    }
+
+    private lateinit var binding: FragmentDivisionProfileBinding
+    private val viewmodel: DivisionDetailViewModel by viewModels { viewmodelFactory }
+    private val adapter = VotesAdapter()
+
+    private fun getDivisionFromBundle(): Pair<House, Int>? {
+        val args = arguments ?: return null
+        val house = enumValues<House>()[args.getInt(HOUSE)]
+        val parliamentdotuk = args.getInt(PARLIAMENTDOTUK)
+
+        return Pair(house, parliamentdotuk)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val division = getDivisionFromBundle() ?: return
+        viewmodel.forDivision(division.first, division.second)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentDivisionProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerview.setupGrid(adapter, context.dp(160), columnSpace = context.dp(8))
+
+        viewmodel.liveData.observe(viewLifecycleOwner) { result ->
+            if (result is NetworkError) networkErrorSnackbar()
+
+            val data = result.data
+            if (data != null) {
+                updateUI(data.division, data.votes)
+            }
+        }
+
+        viewmodel.voteLiveData.observe(viewLifecycleOwner) { votes -> updateVotes(votes) }
+    }
+
+    private fun updateUI(division: Division, votes: List<Vote>) {
+        binding.apply {
+            title.text = division.title
+            date.text = division.date
+            house.text = division.house.name
+            ayes.text = stringCompat(R.string.division_ayes, division.ayes)
+            noes.text = stringCompat(R.string.division_noes, division.noes)
+
+            chart.categories = viewmodel.getVoteChartData(division)
+        }
+    }
+
+    private fun updateVotes(votes: List<Vote>) {
+        adapter.items = votes
+        adapter.notifyDataSetChanged()
+    }
+}
+
+private class VotesAdapter(var items: List<Vote>? = null) : BaseRecyclerViewAdapter() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder =
+        VoteViewHolder(inflate(parent, R.layout.item_wide_title))
+
+    override fun getItemCount(): Int = items?.size ?: 0
+
+    private inner class VoteViewHolder(view: View) : BaseViewHolder(view) {
+        private val vh = ItemWideTitleBinding.bind(view)
+
+        override fun bind(position: Int) {
+            val vote = items?.get(position) ?: return
+            vh.apply {
+                title.text = vote.memberName
+                accent.setBackgroundColor(root.colorCompat(vote.voteType.colorResId))
+            }
+            itemView.setOnClickListener { view ->
+                view.navigateTo(
+                    R.id.action_divisionProfileFragment_to_memberProfileFragment,
+                    bundleOf(
+                        PARLIAMENTDOTUK to vote.memberId,
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/beatonma/commons/app/division/DivisionDetailViewModel.kt
@@ -0,0 +1,64 @@
+package org.beatonma.commons.app.division
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.beatonma.commons.CommonsApplication
+import org.beatonma.commons.R
+import org.beatonma.commons.app.ui.Snippet
+import org.beatonma.commons.app.ui.SnippetGeneratorAndroidViewModel
+import org.beatonma.commons.app.ui.views.BarChartCategory
+import org.beatonma.commons.data.IoResult
+import org.beatonma.commons.data.core.repository.CommonsRepository
+import org.beatonma.commons.data.core.room.entities.division.Division
+import org.beatonma.commons.data.core.room.entities.division.DivisionWithVotes
+import org.beatonma.commons.data.core.room.entities.division.Vote
+import org.beatonma.commons.data.core.room.entities.member.House
+import org.beatonma.commons.kotlin.extensions.colorCompat
+import javax.inject.Inject
+
+class DivisionDetailViewModel @Inject constructor(
+    private val repository: CommonsRepository,
+    application: CommonsApplication,
+) : SnippetGeneratorAndroidViewModel<DivisionWithVotes>(application) {
+
+    private val votesObserver = Observer<IoResult<DivisionWithVotes>> {
+        viewModelScope.launch {
+            voteLiveData.postValue(
+                sortedVotes(it.data?.votes ?: listOf())
+            )
+        }
+    }
+    var voteLiveData: MutableLiveData<List<Vote>> = MutableLiveData()
+
+    fun forDivision(house: House, parliamentdotuk: Int) {
+        liveData = repository.observeDivision(house, parliamentdotuk)
+        observe()
+    }
+
+    override fun observe() {
+        super.observe()
+        liveData.observeForever(votesObserver)
+    }
+
+    override fun onCleared() {
+        liveData.removeObserver(votesObserver)
+        super.onCleared()
+    }
+
+    override suspend fun generateSnippets(data: DivisionWithVotes): List<Snippet> = listOfNotNull(
+
+    )
+
+    fun getVoteChartData(division: Division) = listOf(
+        BarChartCategory(division.ayes, colorCompat(R.color.vote_aye), "Ayes"),
+        BarChartCategory(division.abstentions ?: 0, colorCompat(R.color.vote_abstain), "Abstentions"),
+        BarChartCategory(division.didNotVote ?: 0, colorCompat(R.color.vote_didnotvote), "Did not vote/not present"),
+        BarChartCategory(division.noes, colorCompat(R.color.vote_no), "Noes"),
+    ).filter { it.value > 0 }
+
+    suspend fun sortedVotes(votes: List<Vote>): List<Vote> = votes.sortedWith(
+        compareBy({ it.voteType }, { it.memberName })
+    )
+}
