9679f3bfaa5829cec4cd2c8ebc9229711a51ccfd
Michael Beaton <beatonma@gmail.com>
2020-04-28 15:44:08 +0100

    Added alpha property. Added hsv(), alpha() and coerce() methods.

--- app/src/main/java/org/beatonma/commons/kotlin/data/InlineClass.kt
+++ app/src/main/java/org/beatonma/commons/kotlin/data/InlineClass.kt
@@ -6,5 +6,27 @@ inline class Color(val color: Int) {
     val red get() = PlatformColor.red(color)
     val green get() = PlatformColor.green(color)
     val blue get() = PlatformColor.blue(color)
+    val alpha get() = PlatformColor.alpha(color)
+
     val hex get() = Integer.toHexString(color)
+
+    fun hsv(out: FloatArray = FloatArray(3)): FloatArray {
+        PlatformColor.RGBToHSV(red, green, blue, out)
+        return out
+    }
+
+    fun alpha(value: Int) = Color(PlatformColor.argb(value, red, green, blue))
+    fun alpha(value: Float) = alpha((value * 255F).toInt())
+
+    fun coerce(
+        minSaturation: Float = 0F,
+        maxSaturation: Float = 1F,
+        minLuminance: Float = 0F,
+        maxLuminance: Float = 1F,
+    ): Color {
+        val hsv = hsv()
+        hsv[1] = hsv[1].coerceIn(minSaturation, maxSaturation)
+        hsv[2] = hsv[2].coerceIn(minLuminance, maxLuminance)
+        return Color(PlatformColor.HSVToColor(hsv))
+    }
 }
