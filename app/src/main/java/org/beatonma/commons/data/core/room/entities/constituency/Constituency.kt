de3ae260c7a0186bf74fac9975caaf7c85a1e36b
Michael Beaton <beatonma@gmail.com>
2020-04-30 20:24:56 +0100

    Added election results to ConstituencyWIthDetails

--- app/src/main/java/org/beatonma/commons/data/core/room/entities/constituency/Constituency.kt
+++ app/src/main/java/org/beatonma/commons/data/core/room/entities/constituency/Constituency.kt
@@ -3,7 +3,9 @@ package org.beatonma.commons.data.core.room.entities.constituency
 import androidx.room.*
 import com.squareup.moshi.Json
 import org.beatonma.commons.data.PARLIAMENTDOTUK
-import org.beatonma.commons.data.core.room.entities.member.MemberProfile
+import org.beatonma.commons.data.core.room.entities.election.ApiConstituencyResult
+import org.beatonma.commons.data.core.room.entities.election.ConstituencyResult
+import org.beatonma.commons.data.core.room.entities.election.ConstituencyResultWithDetails
 
 @Entity(
     tableName = "constituencies"
@@ -22,6 +24,7 @@ data class ApiConstituency(
     @field:Json(name = "start") val start: String?,
     @field:Json(name = "end") val end: String?,
     @field:Json(name = "boundary") val boundary: ConstituencyBoundary?,
+    @field:Json(name = "results") val results: List<ApiConstituencyResult>,
 ) {
     fun toConstituency(): Constituency {
         return Constituency(
@@ -34,22 +37,13 @@ data class ApiConstituency(
 }
 
 
-data class ConstituencyWithBoundary(
+data class ConstituencyWithDetails(
     @Embedded
     val constituency: Constituency,
 
     @Relation(parentColumn = "constituency_$PARLIAMENTDOTUK", entityColumn = "boundary_constituency_id")
     val boundary: ConstituencyBoundary?,
-)
 
-
-
-data class ConstituencyDetails(
-    @Embedded val mp: MemberProfile,
-
-    @Relation(parentColumn = "constituency_id", entityColumn = "constituency_$PARLIAMENTDOTUK")
-    val constituency: Constituency,
-
-    @Relation(parentColumn = "constituency_id", entityColumn = "boundary_constituency_id")
-    val boundary: ConstituencyBoundary?,
+    @Relation(parentColumn = "constituency_$PARLIAMENTDOTUK", entityColumn = "result_constituency_id", entity = ConstituencyResult::class)
+    val electionResults: List<ConstituencyResultWithDetails>,
 )
