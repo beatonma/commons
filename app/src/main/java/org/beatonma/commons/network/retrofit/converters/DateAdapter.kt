2760dddf6bffe8e627d3a063c0ce140ea624cc28
Michael Beaton <beatonma@gmail.com>
2020-05-22 13:10:34 +0100

    Migrated from java.util.Date to java.time.LocalDate

--- app/src/main/java/org/beatonma/commons/network/retrofit/converters/DateAdapter.kt
+++ app/src/main/java/org/beatonma/commons/network/retrofit/converters/DateAdapter.kt
@@ -2,28 +2,25 @@ package org.beatonma.commons.network.retrofit.converters
 
 import com.squareup.moshi.*
 import org.beatonma.commons.kotlin.takeFirstSuccessful
-import org.beatonma.lib.util.kotlin.extensions.dump
-import java.text.SimpleDateFormat
-import java.util.*
+import java.time.LocalDate
 
 private const val API_FORMAT = "yyyy-MM-dd"
 
-class DateAdapter: JsonAdapter<Date>() {
-    private val format = SimpleDateFormat(API_FORMAT, Locale.getDefault()).dump()
+class DateAdapter: JsonAdapter<LocalDate>() {
 
     @FromJson
-    override fun fromJson(reader: JsonReader): Date? {
+    override fun fromJson(reader: JsonReader): LocalDate? {
         return takeFirstSuccessful(
-            { format.parse(reader.nextString()) },
+            { LocalDate.parse(reader.nextString()) },
             { reader.nextNull() },
-            { Date(reader.nextLong()) }
+            { LocalDate.ofEpochDay(reader.nextLong()) }
         )
     }
 
     @ToJson
-    override fun toJson(writer: JsonWriter, value: Date?) {
+    override fun toJson(writer: JsonWriter, value: LocalDate?) {
         if (value != null) {
-            writer.value(value.time)
+            writer.value(value.toString())
         }
         else {
             writer.nullValue()
