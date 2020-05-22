4a92a2024756b3d02b61f75a8d210246f84f54c2
Michael Beaton <beatonma@gmail.com>
2020-05-22 15:52:38 +0100

    Created compressConsecutiveItems for Periodic implementations. Implemented tests for party/constituency compression in MemberProfileViewModel.kt

--- app/src/main/java/org/beatonma/commons/data/core/Periodic.kt
+++ app/src/main/java/org/beatonma/commons/data/core/Periodic.kt
@@ -1,6 +1,7 @@
 package org.beatonma.commons.data.core
 
 import java.time.LocalDate
+import java.time.Period
 
 /**
  * Something with and start and/or end date
@@ -8,4 +9,49 @@ import java.time.LocalDate
 interface Periodic {
     val start: LocalDate?
     val end: LocalDate?
+}
+
+
+suspend fun <T: Periodic> compressConsecutiveItems(
+    periodic: List<T>?,
+    areItemsTheSame: (T, T) -> Boolean,
+    ifMonthsLessThan: Int = 2,  // If a gap between 2 same items < this value they should be combined into a single item
+    combineFunc: (T, T) -> T,
+): List<T>? {
+    periodic ?: return null
+
+    /**
+     * Returns true if the items can be squished into one.
+     */
+    fun shouldCombine(a: T, b: T): Boolean {
+        if (!areItemsTheSame(a, b)) return false
+
+        val firstEnd = a.end ?: return false
+        val secondStart = b.start ?: return false
+
+        val difference = Period.between(firstEnd, secondStart)
+        println("$firstEnd to $secondStart = ${difference.toTotalMonths()} months")
+        return difference.toTotalMonths() < ifMonthsLessThan
+    }
+
+    val compressed = mutableListOf<T>()
+
+    val sorted = periodic.sortedBy { it.start }
+    var current: T = sorted.firstOrNull() ?: return compressed
+    sorted.forEach { item ->
+        if (current === item) {
+            return@forEach
+        }
+        current = if (shouldCombine(current, item)) {
+            combineFunc(current, item)
+        }
+        else {
+            compressed.add(current)
+            item
+        }
+    }
+
+    compressed.add(current)
+
+    return compressed
 }
