099f99d56429a47086bc807a9c10e8d4d900f811
Michael Beaton <beatonma@gmail.com>
2020-05-22 15:53:03 +0100

    Added ListResponse<T> to replace Response<List<T>

--- app/src/main/java/org/beatonma/commons/data/Types.kt
+++ app/src/main/java/org/beatonma/commons/data/Types.kt
@@ -1,11 +1,13 @@
 package org.beatonma.commons.data
 
 import androidx.lifecycle.LiveData
+import retrofit2.Response
 
 typealias ParliamentID = Int
 
 // Wrappers to reduce deep nesting of type definitions <<<>>>
 typealias IoResultList<T> = IoResult<List<T>>
+typealias ListResponse<T> = Response<List<T>>
 typealias LiveDataIoResult<T> = LiveData<IoResult<T>>
 typealias LiveDataIoResultList<T> = LiveData<IoResultList<T>>
 typealias LiveDataList<T> = LiveData<List<T>>
