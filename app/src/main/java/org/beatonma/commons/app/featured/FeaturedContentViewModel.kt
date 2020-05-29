7d67f035cb7f4a6866644f4045c30248826fbb00
Michael Beaton <beatonma@gmail.com>
2020-05-29 21:13:28 +0100

    Added motd, bills, divisions

--- app/src/main/java/org/beatonma/commons/app/featured/FeaturedContentViewModel.kt
+++ app/src/main/java/org/beatonma/commons/app/featured/FeaturedContentViewModel.kt
@@ -1,11 +1,15 @@
 package org.beatonma.commons.app.featured
 
 import androidx.lifecycle.ViewModel
-import org.beatonma.commons.data.core.CommonsRepository
+import org.beatonma.commons.data.core.repository.CommonsRepository
 import javax.inject.Inject
 
 class FeaturedContentViewModel @Inject constructor(
     repository: CommonsRepository
 ): ViewModel() {
     val featuredPeople = repository.observeFeaturedPeople()
+    val featuredBills = repository.observeFeaturedBills()
+    val featuredDivisions = repository.observeFeaturedDivisions()
+
+    val motd = repository.observeMotd()
 }
