73d5eac67aa8b5b53ad89b25085484cfc826ea30
Michael Beaton <beatonma@gmail.com>
2020-06-01 18:05:21 +0100

    Converted to collapsible adapter. Actions are now displayed in recyclerview

--- app/src/main/java/org/beatonma/commons/app/memberprofile/adapter/PhysicalAddressAdapter.kt
+++ app/src/main/java/org/beatonma/commons/app/memberprofile/adapter/PhysicalAddressAdapter.kt
@@ -1,19 +1,34 @@
 package org.beatonma.commons.app.memberprofile.adapter
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import org.beatonma.commons.R
 import org.beatonma.commons.app.ui.recyclerview.ThemedAdapter
+import org.beatonma.commons.app.ui.recyclerview.ThemedCollapsibleAdapter
+import org.beatonma.commons.app.ui.recyclerview.defaultItemSpace
+import org.beatonma.commons.app.ui.views.ChipData
+import org.beatonma.commons.app.ui.views.CollapsibleChipHolder
 import org.beatonma.commons.data.core.room.entities.member.PhysicalAddress
 import org.beatonma.commons.databinding.MemberItemPhysicalAddressBinding
 import org.beatonma.commons.kotlin.extensions.bindText
+import org.beatonma.commons.kotlin.extensions.context
 import org.beatonma.commons.kotlin.extensions.inflate
+import org.beatonma.lib.ui.recyclerview.TypedRecyclerViewAdapter
+import org.beatonma.lib.ui.recyclerview.kotlin.extensions.horizontalLinearLayoutManager
+import org.beatonma.lib.ui.recyclerview.kotlin.extensions.setup
 
-class PhysicalAddressAdapter: ThemedAdapter<PhysicalAddress>() {
-    override fun onCreateDefaultViewHolder(parent: ViewGroup) =
+class PhysicalAddressAdapter: ThemedCollapsibleAdapter<PhysicalAddress>() {
+    override fun onCreateDefaultViewHolder(parent: ViewGroup): TypedRecyclerViewAdapter<PhysicalAddress>.TypedViewHolder =
         object: TypedViewHolder(parent.inflate(R.layout.member_item_physical_address)) {
-            private val vh =
-                MemberItemPhysicalAddressBinding.bind(
-                    itemView)
+            private val vh = MemberItemPhysicalAddressBinding.bind(itemView)
+            private val adapter = ActionAdapter()
+            init {
+                vh.actions.setup(
+                    adapter,
+                    horizontalLinearLayoutManager(parent.context),
+                    space = defaultItemSpace(context, horizontal = true)
+                )
+            }
 
             override fun bind(item: PhysicalAddress) {
                 vh.apply {
@@ -21,12 +36,22 @@ class PhysicalAddressAdapter: ThemedAdapter<PhysicalAddress>() {
                         description to item.description,
                         address to item.address,
                         postcode to item.postcode,
-                        email to item.email,
-                        phone to item.phone,
-                        fax to item.fax,
-                        linkColor = theme?.accent,
                     )
                 }
+
+                adapter.items = listOfNotNull(
+                    if (item.phone != null) ChipData.forPhoneNumber(context, item.phone) else null,
+                    if (item.email != null) ChipData.forEmail(context, item.email) else null
+                )
             }
         }
+
+    private class ActionAdapter: ThemedAdapter<ChipData>() {
+        override fun onCreateDefaultViewHolder(parent: ViewGroup): RecyclerView.ViewHolder = CollapsibleChipHolder(parent)
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val item = items?.get(position) ?: return
+            (holder as CollapsibleChipHolder).bind(item)
+        }
+    }
 }
