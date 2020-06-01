cd514d9868a54c3bab0af93851ba1066e3c7b157
Michael Beaton <beatonma@gmail.com>
2020-06-01 18:05:41 +0100

    Converted to collapsible adapter.

--- app/src/main/java/org/beatonma/commons/app/memberprofile/adapter/CurrentAdapter.kt
+++ app/src/main/java/org/beatonma/commons/app/memberprofile/adapter/CurrentAdapter.kt
@@ -3,12 +3,12 @@ package org.beatonma.commons.app.memberprofile.adapter
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.beatonma.commons.R
-import org.beatonma.commons.app.ui.recyclerview.ThemedAdapter
+import org.beatonma.commons.app.ui.recyclerview.ThemedCollapsibleAdapter
 import org.beatonma.commons.data.core.room.entities.constituency.Constituency
 import org.beatonma.commons.databinding.ItemWideTitleActionBinding
 import org.beatonma.commons.kotlin.extensions.*
 
-class CurrentAdapter: ThemedAdapter<Any>() {
+class CurrentAdapter: ThemedCollapsibleAdapter<Any>() {
     override fun onCreateDefaultViewHolder(parent: ViewGroup): RecyclerView.ViewHolder =
         object: TypedViewHolder(parent.inflate(R.layout.item_wide_title_action)) {
             private val vh = ItemWideTitleActionBinding.bind(
