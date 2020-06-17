a1100cfc2aa5a2200aef9732541f369474c6a46d
Michael Beaton <beatonma@gmail.com>
2020-06-17 18:21:20 +0100

    Renamed file. Add lighten(), darken(), asStateList()

--- app/src/main/java/org/beatonma/commons/kotlin/data/Color.kt
+++ app/src/main/java/org/beatonma/commons/kotlin/data/Color.kt
@@ -1,7 +1,11 @@
 package org.beatonma.commons.kotlin.data
 
+import android.content.res.ColorStateList
+import androidx.annotation.ColorInt
+import androidx.annotation.FloatRange
 import android.graphics.Color as PlatformColor
 
+@Suppress("MemberVisibilityCanBePrivate")
 inline class Color(val color: Int) {
     val red get() = PlatformColor.red(color)
     val green get() = PlatformColor.green(color)
@@ -29,4 +33,22 @@ inline class Color(val color: Int) {
         hsv[2] = hsv[2].coerceIn(minLuminance, maxLuminance)
         return Color(PlatformColor.HSVToColor(hsv))
     }
+
+    @ColorInt
+    fun lighten(@FloatRange(from = 0.0, to = 1.0) amount: Float): Int {
+        val hsv = hsv()
+        hsv[1] = 0f.coerceAtLeast(1f.coerceAtMost(hsv[1] - amount))    // saturation
+        hsv[2] = 0f.coerceAtLeast(1f.coerceAtMost(hsv[2] + amount))    // brightness
+        return PlatformColor.HSVToColor(hsv)
+    }
+
+    @ColorInt
+    fun darken(@FloatRange(from = 0.0, to = 1.0) amount: Float): Int {
+        val hsv = hsv()
+        hsv[1] = 0f.coerceAtLeast(1f.coerceAtMost(hsv[1] + amount))    // saturation
+        hsv[2] = 0f.coerceAtLeast(1f.coerceAtMost(hsv[2] - amount))   // brightness
+        return PlatformColor.HSVToColor(hsv)
+    }
 }
+
+fun Int.asStateList() = ColorStateList.valueOf(this)
