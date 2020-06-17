296abc1b15d06db05e0f8984381529033171625b
Michael Beaton <beatonma@gmail.com>
2020-06-17 18:20:06 +0100

    Moved URL logging to withDefaultHeaders() so it is applied to all HttpClients

--- app/src/main/java/org/beatonma/commons/network/HttpClientModule.kt
+++ app/src/main/java/org/beatonma/commons/network/HttpClientModule.kt
@@ -3,13 +3,10 @@ package org.beatonma.commons.network
 import android.util.Log
 import dagger.Module
 import dagger.Provides
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import okhttp3.*
 import org.beatonma.commons.BuildConfig.*
+import org.beatonma.commons.kotlin.extensions.autotag
 import org.beatonma.commons.network.dagger.WebService
-import org.beatonma.lib.util.kotlin.extensions.autotag
 import javax.inject.Named
 import javax.inject.Singleton
 import android.os.Build as Device
@@ -36,10 +33,12 @@ private val userAgent: String by lazy {
     "$USER_AGENT_APP/$VERSION_NAME ($USER_AGENT_WEBSITE $USER_AGENT_EMAIL) Android[${Device.MANUFACTURER}/${Device.PRODUCT}] ${Device.VERSION.RELEASE}/${Device.VERSION.SDK_INT}/${APPLICATION_ID}v$VERSION_CODE"
 }
 
-private inline fun Interceptor.Chain.withDefaultHeaders(block: (Request.Builder) -> Unit): Response {
+private inline fun Interceptor.Chain.withDefaultHeaders(block: (Request.Builder, HttpUrl) -> Unit): Response {
     return proceed(request().newBuilder().apply {
         header(HttpHeader.USER_AGENT, userAgent)
-        block(this)
+        val originalUrl = request().url()
+        Log.d(autotag, "Request: $originalUrl")
+        block(this, originalUrl)
     }.build())
 }
 
@@ -49,17 +48,19 @@ class HttpClientModule {
     @Provides
     @Named(WebService.GENERIC)
     fun provideHttpClient(): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor { chain -> chain.withDefaultHeaders { } }.build()
+        .addInterceptor { chain ->
+            chain.withDefaultHeaders { _, _ ->
+
+            }
+        }.build()
 
     @Singleton
     @Provides
     @Named(WebService.COMMONS)
     fun provideCommonsHttpClient(): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor { chain ->
-            chain.withDefaultHeaders { requestBuilder ->
+            chain.withDefaultHeaders { requestBuilder, originalUrl ->
                 requestBuilder.addHeader(HttpHeader.CONTENT_TYPE, HttpContentType.JSON)
-                val originalUrl = chain.request().url()
-                Log.d(autotag, "Request: $originalUrl")
                 requestBuilder.url(originalUrl.newBuilder().apply {
                     addQueryParameter(CommonsParam.API_KEY, COMMONS_API_KEY)
                 }.build())
@@ -71,8 +72,7 @@ class HttpClientModule {
     @Named(WebService.TWFY)
     fun provideTwfyHttpClient(): OkHttpClient = OkHttpClient.Builder()
         .addInterceptor{ chain ->
-            chain.withDefaultHeaders { requestBuilder ->
-                val originalUrl = chain.request().url()
+            chain.withDefaultHeaders { requestBuilder, originalUrl ->
                 requestBuilder.url(originalUrl.newBuilder().apply {
                     addQueryParameter(TwfyParam.API_KEY, TWFY_API_KEY)
                     addQueryParameter(TwfyParam.OUTPUT_FORMAT, "js")
