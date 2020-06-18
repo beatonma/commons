aa2d974306c1452002ccf5b6b9ec86c0c6acdf59
Michael Beaton <beatonma@gmail.com>
2020-06-18 14:12:47 +0100

    Android R. Add Social.MAX_COMMENT_LENGTH

--- buildSrc/src/main/java/Commons.kt
+++ buildSrc/src/main/java/Commons.kt
@@ -2,8 +2,12 @@ object Commons {
     const val APPLICATION_ID = "org.beatonma.commons"
 
     object Sdk {
-        const val MIN = 21
-        const val TARGET = 29
-        const val COMPILE = "android-29"
+        const val MIN = 26
+        const val TARGET = 30
+        const val COMPILE = "android-R"
+    }
+
+    object Social {
+        const val MAX_COMMENT_LENGTH = 240
     }
 }
