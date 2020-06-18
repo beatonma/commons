22214d889428219fb47a3e668d7f2eaecd38d9f8
Michael Beaton <beatonma@gmail.com>
2020-06-18 13:47:03 +0100

    Add asSocialTarget() helper to Sociable

--- app/src/main/java/org/beatonma/commons/data/core/interfaces/Sociable.kt
+++ app/src/main/java/org/beatonma/commons/data/core/interfaces/Sociable.kt
@@ -1,9 +1,11 @@
 package org.beatonma.commons.data.core.interfaces
 
-import org.beatonma.commons.data.core.social.SocialContentTarget
+import org.beatonma.commons.data.core.repository.SocialTarget
+import org.beatonma.commons.data.core.social.SocialTargetType
 
 interface Sociable: Parliamentdotuk {
-    fun getSocialContentTarget(): SocialContentTarget
+    fun getSocialContentTarget(): SocialTargetType
+    fun asSocialTarget() = SocialTarget(this)
 }
 
 interface Commentable: Sociable
