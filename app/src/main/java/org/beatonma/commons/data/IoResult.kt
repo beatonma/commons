99096b3f58b0608f1310884a1468a9ba0b49b107
Michael Beaton <beatonma@gmail.com>
2020-06-18 13:58:47 +0100

    Add NoBodySuccessResult for network functions that only need to return a result code. Added NotSignedInError

--- app/src/main/java/org/beatonma/commons/data/IoResult.kt
+++ app/src/main/java/org/beatonma/commons/data/IoResult.kt
@@ -8,11 +8,21 @@ sealed class IoResult<out T>(
 )
 
 class SuccessResult<T>(data: T, message: String?): IoResult<T>(data, message)
+class NoBodySuccessResult<T>(val responseCode: Int, message: String?): IoResult<T>(null, message) {
+    override fun toString(): String {
+        return "[${this.javaClass.canonicalName}] responseCode=$responseCode"
+    }
+}
 
-sealed class IoError<T>(data: T?, message: String?, val error: Exception?): IoResult<T>(data, message)
+sealed class IoError<T>(data: T?, message: String?, val error: Exception?): IoResult<T>(data, message) {
+    override fun toString(): String {
+        return "[${this.javaClass.canonicalName}] data=`$data`, message=`$message`, error=`$error`"
+    }
+}
 class NetworkError<T>(message: String?, error: Exception?): IoError<T>(null, message, error)
 class GenericError<T>(message: String?, error: Exception?): IoError<T>(null, message, error)
 class LocalError<T>(message: String?, error: Exception?): IoError<T>(null, message, error)
 class UnexpectedValueError<T>(message: String?, error: Exception?): IoError<T>(null, message, error)
+class NotSignedInError(message: String?, error: Exception? = null): IoError<Nothing>(null, message, error)
 
 class LoadingResult<T>(data: T? = null, message: String? = null): IoResult<T>(data, message)
