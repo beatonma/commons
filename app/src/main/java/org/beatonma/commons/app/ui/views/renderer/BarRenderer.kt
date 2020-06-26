654f788834102ba311cf1dbc07790ef280a7d901
Michael Beaton <beatonma@gmail.com>
2020-06-26 19:33:35 +0100

    Reimplemented to allow line gradient to be chosen, rather than drawing lines based on arbitrary endpoints

--- app/src/main/java/org/beatonma/commons/app/ui/views/renderer/BarRenderer.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/views/renderer/BarRenderer.kt
@@ -4,8 +4,9 @@ import android.graphics.Canvas
 import android.graphics.Paint
 
 private const val TAG = "BarRenderer"
-private const val DEFAULT_LEAN_STEP = 7
-private const val DEFAULT_WITH_OUTLINE = false
+private const val DEFAULT_WITH_OUTLINE = true
+
+private const val DEFAULT_STRIPE_GRADIENT = -1F
 
 
 abstract class BarRenderer(paintInit: (Paint.() -> Unit)? = null) {
@@ -79,7 +80,7 @@ abstract class BaseStripeBarRenderer(
     protected val lineThickness: Float,
     protected val spacing: Float,
     private val withOutline: Boolean = DEFAULT_WITH_OUTLINE,
-    protected val leanStep: Int = DEFAULT_LEAN_STEP,
+    protected val stripeGradient: Float = DEFAULT_STRIPE_GRADIENT,
     paintInit: (Paint.() -> Unit)? = null,
 ): BarRenderer(paintInit) {
 
@@ -93,10 +94,8 @@ abstract class BaseStripeBarRenderer(
     ) {
         val topY = centerY - thickness / 2
         val bottomY = centerY + thickness / 2
-        val increments = ((endX - startX) / spacing).toInt()
-        for (n in -leanStep..increments) {
-            drawIncrement(canvas, n, startX, endX, topY, bottomY, color)
-        }
+
+        drawStripes(canvas, startX, endX, topY, bottomY, color)
 
         if (withOutline) {
             canvas.drawRect(startX, centerY - thickness / 2, endX, centerY + thickness / 2, paint.apply {
@@ -106,7 +105,14 @@ abstract class BaseStripeBarRenderer(
         }
     }
 
-    abstract fun drawIncrement(canvas: Canvas, increment: Int, startX: Float, endX: Float, topY: Float, bottomY: Float, color: Int)
+    abstract fun drawStripes(
+        canvas: Canvas,
+        startX: Float,
+        endX: Float,
+        topY: Float,
+        bottomY: Float,
+        color: Int,
+    )
 }
 
 
@@ -115,77 +121,112 @@ class StripeBarRenderer(
     spacing: Float,
     withOutline: Boolean = DEFAULT_WITH_OUTLINE,
     paintInit: (Paint.() -> Unit)? = null,
-): BaseStripeBarRenderer(lineThickness, spacing, withOutline, leanStep = 0, paintInit) {
-    override fun drawIncrement(
+): BaseStripeBarRenderer(lineThickness, spacing, withOutline, 0F, paintInit) {
+    override fun drawStripes(
         canvas: Canvas,
-        increment: Int,
         startX: Float,
         endX: Float,
         topY: Float,
         bottomY: Float,
-        color: Int,
+        color: Int
     ) {
-        if (increment < 0) return
-        val x = startX + increment * spacing
-        canvas.drawLine(
-            x,
-            topY,
-            x,
-            bottomY,
-            paint.apply {
-                this.color = color
-                this.strokeWidth = lineThickness
-            }
-        )
+        var n = 0
+        var done: Boolean
+        do {
+            val x = startX + n++ * spacing
+            canvas.drawLine(
+                x,
+                topY,
+                x,
+                bottomY,
+                paint.apply {
+                    this.color = color
+                    this.strokeWidth = lineThickness
+                }
+            )
+            done = x + spacing > endX
+        } while (!done)
     }
 }
 
-
-class ForwardStripeBarRenderer(
+open class ForwardStripeBarRenderer(
     lineThickness: Float,
     spacing: Float,
     withOutline: Boolean = DEFAULT_WITH_OUTLINE,
-    leanStep: Int = DEFAULT_LEAN_STEP,
+    stripeGradient: Float = DEFAULT_STRIPE_GRADIENT,
     paintInit: (Paint.() -> Unit)? = null,
-): BaseStripeBarRenderer(lineThickness, spacing, withOutline, leanStep, paintInit) {
-    override fun drawIncrement(
+): BaseStripeBarRenderer(lineThickness, spacing, withOutline, stripeGradient, paintInit) {
+    internal open fun plotHorizontal(
+        gap: Float,
+        startX: Float,
+        endX: Float,
+        topY: Float,
+        bottomY: Float,
+    ): Line {
+        val x1 = startX + gap
+        val c = topY - (stripeGradient * x1)
+        val x2 = (bottomY - c) / stripeGradient
+
+        return Line(x1, topY, x2, bottomY).confineTo(startX, topY, endX, bottomY)
+    }
+
+    internal open fun plotVertical(
+        gap: Float,
+        startX: Float,
+        endX: Float,
+        topY: Float,
+        bottomY: Float,
+    ): Line {
+        val y1 = topY + gap
+        val c = y1 - (stripeGradient * startX)
+        val x2 = (bottomY - c) / stripeGradient
+
+        return Line(startX, y1, x2, bottomY).confineTo(startX, topY, endX, bottomY)
+    }
+
+    override fun drawStripes(
         canvas: Canvas,
-        increment: Int,
         startX: Float,
         endX: Float,
         topY: Float,
         bottomY: Float,
         color: Int,
     ) {
-        var x1 = startX + (increment * spacing)
-        var x2 = startX + (increment + leanStep) * spacing
+        var n = 0
+        var done: Boolean
 
-        var y1 = bottomY
-        var y2 = topY
+        // Across
+        do {
+            val gap = n++ * spacing
+            val (x1, y1, x2, y2) = plotHorizontal(gap, startX, endX, topY, bottomY)
 
-        val m = (y2 - y1) / (x2 - x1)
+            canvas.drawLine(
+                x1, y1, x2, y2,
+                paint.apply {
+                    this.color = color
+                    this.strokeWidth = lineThickness
+                }
+            )
 
-        if (x1 < startX) {
-            val c1 = y1 - m * x1
-            x1 = startX
-            y1 = x1 * m + c1
-        }
-        if (x2 > endX) {
-            val c2 = y2 - m * x2
-            x2 = endX
-            y2 = x2 * m + c2
-        }
+            done = x1 + spacing > endX && x2 + spacing > endX
+        } while (!done)
 
-        canvas.drawLine(
-            x1,
-            y1,
-            x2,
-            y2,
-            paint.apply {
-                this.color = color
-                this.strokeWidth = lineThickness
-            }
-        )
+        // Down/backfilling the corner
+        n = 0
+        do {
+            val gap = n++ * spacing
+            val (x1, y1, x2, y2) = plotVertical(gap, startX, endX, topY, bottomY)
+
+            canvas.drawLine(
+                x1, y1, x2, y2,
+                paint.apply {
+                    this.color = color
+                    this.strokeWidth = lineThickness
+                }
+            )
+
+            done = y1 + spacing > bottomY && y2 + spacing > bottomY
+        } while (!done)
     }
 }
 
@@ -194,46 +235,42 @@ class BackwardStripeBarRenderer(
     lineThickness: Float,
     spacing: Float,
     withOutline: Boolean = DEFAULT_WITH_OUTLINE,
-    leanStep: Int = DEFAULT_LEAN_STEP,
+    stripeGradient: Float = DEFAULT_STRIPE_GRADIENT,
     paintInit: (Paint.() -> Unit)? = null,
-): BaseStripeBarRenderer(lineThickness, spacing, withOutline, leanStep, paintInit) {
-    override fun drawIncrement(
-        canvas: Canvas,
-        increment: Int,
-        startX: Float,
-        endX: Float,
-        topY: Float,
-        bottomY: Float,
-        color: Int,
-    ) {
-        var x1 = startX + (increment * spacing)
-        var x2 = startX + (increment + leanStep) * spacing
+): ForwardStripeBarRenderer(lineThickness, spacing, withOutline, -stripeGradient, paintInit)
 
-        var y1 = topY
-        var y2 = bottomY
 
-        val m = (y2 - y1) / (x2 - x1)
 
-        if (x1 < startX) {
-            val c1 = y1 - m * x1
-            x1 = startX
-            y1 = x1 * m + c1
+internal data class Line(val startX: Float, val startY: Float, val endX: Float, val endY: Float) {
+    private val m: Float = (endY - startY) / (endX - startX)
+    private val c: Float = startY - (m * startX)
+
+    private fun x(y: Float): Float = (y - c) / m
+    private fun y(x: Float): Float = (m * x) + c
+
+    fun confineTo(startX: Float, startY: Float, endX: Float, endY: Float): Line {
+        var x1 = this.startX
+        var x2 = this.endX
+        var y1 = this.startY
+        var y2 = this.endY
+
+        if (x1 < startX || x1 > endX) {
+            x1 = x1.coerceIn(startX, endX)
+            y1 = y(x1)
         }
-        if (x2 > endX) {
-            val c2 = y2 - m * x2
-            x2 = endX
-            y2 = x2 * m + c2
+        if (x2 < startX || x2 > endX) {
+            x2 = x2.coerceIn(startX, endX)
+            y2 = y(x2)
+        }
+        if (y1 < startY) {
+            y1 = startY
+            x1 = x(y1)
+        }
+        if (y2 > endY) {
+            y2 = endY
+            x2 = x(y2)
         }
 
-        canvas.drawLine(
-            x1,
-            y1,
-            x2,
-            y2,
-            paint.apply {
-                this.color = color
-                this.strokeWidth = lineThickness
-            }
-        )
+        return Line(x1, y1, x2, y2)
     }
 }
