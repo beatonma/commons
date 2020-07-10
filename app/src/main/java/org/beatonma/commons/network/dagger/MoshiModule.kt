35c0a8d1f5d41edc2a4cdd0ccf0b6b13be54aa2d
Michael Beaton <beatonma@gmail.com>
2020-07-10 13:44:44 +0100

    Add HouseAdapter to convert case for enum matching.

--- app/src/main/java/org/beatonma/commons/network/dagger/MoshiModule.kt
+++ app/src/main/java/org/beatonma/commons/network/dagger/MoshiModule.kt
@@ -3,14 +3,18 @@ package org.beatonma.commons.network.dagger
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import org.beatonma.commons.data.core.room.entities.member.House
 import org.beatonma.commons.network.retrofit.converters.DateAdapter
 import org.beatonma.commons.network.retrofit.converters.DateTimeAdapter
 import org.beatonma.commons.network.retrofit.converters.DeEnvelopeFactory
+import org.beatonma.commons.network.retrofit.converters.HouseAdapter
 import java.time.LocalDate
 import java.time.LocalDateTime
 import javax.inject.Singleton
 
-@Module
+@Module @InstallIn(ApplicationComponent::class)
 class MoshiModule {
     @Singleton
     @Provides
@@ -19,5 +23,6 @@ class MoshiModule {
             .add(DeEnvelopeFactory())
             .add(LocalDate::class.java, DateAdapter())
             .add(LocalDateTime::class.java, DateTimeAdapter())
+            .add(House::class.java, HouseAdapter())
             .build()
 }
