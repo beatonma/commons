248bc0e820355a4f8e05112d5fa0aa69e5ae253a
Michael Beaton <beatonma@gmail.com>
2020-07-15 16:19:33 +0100

    Added BillDetailViewModeTest to AppTestSuite

--- app/src/test/java/org/beatonma/commons/app/AppTestSuite.kt
+++ app/src/test/java/org/beatonma/commons/app/AppTestSuite.kt
@@ -1,11 +1,13 @@
 package org.beatonma.commons.app
 
+import org.beatonma.commons.app.bill.BillDetailViewModelTest
 import org.beatonma.commons.app.memberprofile.ProfileHistoryCompressionTestSuite
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
+    BillDetailViewModelTest::class,
     ProfileHistoryCompressionTestSuite::class,
 )
 class AppTestSuite
