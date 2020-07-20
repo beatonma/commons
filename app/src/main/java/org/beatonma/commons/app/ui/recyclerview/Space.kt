276203bb4b265767942241a502e7b2e7489d28b3
Michael Beaton <beatonma@gmail.com>
2020-07-20 13:12:45 +0100

    Fixed item spacing in horizontal layouts

--- app/src/main/java/org/beatonma/commons/app/ui/recyclerview/Space.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/recyclerview/Space.kt
@@ -8,12 +8,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import org.beatonma.commons.R
+import org.beatonma.commons.data.ShouldNotHappen
 import org.beatonma.commons.kotlin.extensions.dimenCompat
 
-
-data class RvSpacing(
-    val verticalSpace: Int = -1,
-    val horizontalSpace: Int = -1,
+class RvSpacing(
+    spaceAround: Int = -1,
+    verticalSpace: Int = spaceAround,
+    horizontalSpace: Int = spaceAround,
     val topSpace: Int = verticalSpace,
     val bottomSpace: Int = verticalSpace,
     val startSpace: Int = horizontalSpace,
@@ -29,68 +30,16 @@ data class RvSpacing(
     }
 
     fun decorator(layoutManager: RecyclerView.LayoutManager) = when(layoutManager) {
-        is LinearLayoutManager -> linearDecorator()
+        is LinearLayoutManager -> linearDecorator(layoutManager.orientation)
         is GridLayoutManager -> gridDecorator(layoutManager.spanCount)
         is StaggeredGridLayoutManager -> gridDecorator(layoutManager.spanCount)
         else -> linearDecorator()
     }
 
-    fun linearDecorator() = object: RecyclerView.ItemDecoration() {
-        override fun getItemOffsets(
-            outRect: Rect,
-            view: View,
-            parent: RecyclerView,
-            state: RecyclerView.State,
-        ) {
-            super.getItemOffsets(outRect, view, parent, state)
-
-            val itemCount = state.itemCount
-            val itemPosition = parent.getChildAdapterPosition(view)
-
-            if (itemCount == 1) {
-                outRect.updateIfSet(
-                    l = startSpace,
-                    t = topSpace,
-                    r = endSpace,
-                    b = bottomSpace
-                )
-                return
-            }
-
-            val halfVertical = verticalItemSpace / 2
-            val halfHorizontal = horizontalItemSpace / 2
-
-            when (itemPosition) {
-                // First item
-                0 -> {
-                    outRect.updateIfSet(
-                        l = startSpace,
-                        t = topSpace,
-                        r = halfHorizontal,
-                        b = halfVertical
-                    )
-                }
-
-                // Last item
-                itemCount - 1 -> {
-                    outRect.updateIfSet(
-                        l = halfHorizontal,
-                        t = halfVertical,
-                        r = endSpace,
-                        b = bottomSpace
-                    )
-                }
-
-                else -> {
-                    outRect.updateIfSet(
-                        l = halfHorizontal,
-                        t = halfVertical,
-                        r = halfHorizontal,
-                        b = halfVertical
-                    )
-                }
-            }
-        }
+    fun linearDecorator(@RecyclerView.Orientation orientation: Int = RecyclerView.VERTICAL) = when(orientation) {
+        RecyclerView.VERTICAL -> linearDecoratorVertical()
+        RecyclerView.HORIZONTAL -> linearDecoratorHorizontal()
+        else -> throw ShouldNotHappen("orientation should only accept VERTICAL or HORIZONTAL values")
     }
 
     fun gridDecorator(spanCount: Int) = object: RecyclerView.ItemDecoration() {
@@ -133,10 +82,168 @@ data class RvSpacing(
             }
         }
     }
+
+    private fun linearDecoratorVertical() = object: RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State,
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+
+            val itemCount = state.itemCount
+            val itemPosition = parent.getChildAdapterPosition(view)
+
+            if (itemCount == 1) {
+                outRect.updateIfSet(
+                    l = startSpace,
+                    t = topSpace,
+                    r = endSpace,
+                    b = bottomSpace
+                )
+                return
+            }
+
+            val halfVertical = verticalItemSpace / 2
+
+            when (itemPosition) {
+                // First item
+                0 -> {
+                    outRect.updateIfSet(
+                        l = startSpace,
+                        t = topSpace,
+                        r = endSpace,
+                        b = halfVertical
+                    )
+                }
+
+                // Last item
+                itemCount - 1 -> {
+                    outRect.updateIfSet(
+                        l = startSpace,
+                        t = halfVertical,
+                        r = endSpace,
+                        b = bottomSpace
+                    )
+                }
+
+                else -> {
+                    outRect.updateIfSet(
+                        l = startSpace,
+                        t = halfVertical,
+                        r = endSpace,
+                        b = halfVertical
+                    )
+                }
+            }
+        }
+    }
+
+    private fun linearDecoratorHorizontal() = object: RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State,
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+
+            val itemCount = state.itemCount
+
+            if (itemCount == 1) {
+                outRect.updateIfSet(
+                    l = startSpace,
+                    t = topSpace,
+                    r = endSpace,
+                    b = bottomSpace
+                )
+                return
+            }
+
+            val itemPosition = parent.getChildAdapterPosition(view)
+            val halfHorizontal = horizontalItemSpace / 2
+
+            when (itemPosition) {
+                // First item
+                0 -> {
+                    outRect.updateIfSet(
+                        l = startSpace,
+                        t = topSpace,
+                        r = halfHorizontal,
+                        b = bottomSpace
+                    )
+                }
+
+                // Last item
+                itemCount - 1 -> {
+                    outRect.updateIfSet(
+                        l = halfHorizontal,
+                        t = topSpace,
+                        r = endSpace,
+                        b = bottomSpace
+                    )
+                }
+
+                else -> {
+                    outRect.updateIfSet(
+                        l = halfHorizontal,
+                        t = topSpace,
+                        r = halfHorizontal,
+                        b = bottomSpace
+                    )
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Spacing for a RecyclerView that is embedded in a larger UI - item spacing only, no overscroll or
+ * external margins.
+ */
+fun defaultItemSpace(context: Context, horizontal: Boolean = true, vertical: Boolean = true) = RvSpacing(
+    spaceAround = context.dimenCompat(R.dimen.list_space_around_default),
+    verticalItemSpace = if (vertical) context.dimenCompat(R.dimen.flow_gap_vertical) else -1,
+    horizontalItemSpace = if (horizontal) context.dimenCompat(R.dimen.flow_gap_horizontal) else -1,
+)
+
+
+fun defaultSpace(
+    context: Context,
+    @RecyclerView.Orientation orientation: Int,
+    overscroll: Boolean = true
+) = when {
+    overscroll && orientation == RecyclerView.HORIZONTAL -> defaultHorizontalOverscroll(context)
+    overscroll && orientation == RecyclerView.VERTICAL -> defaultPrimaryContentSpacing(context)
+    else -> defaultItemSpace(context)
 }
 
 
-fun defaultItemSpace(context: Context, horizontal: Boolean = false, vertical: Boolean = false) = RvSpacing(
-    verticalItemSpace = if (vertical) context.dimenCompat(R.dimen.flow_gap_vertical) else -1,
-    horizontalItemSpace = if (horizontal) context.dimenCompat(R.dimen.flow_gap_horizontal) else -1,
+/**
+ * Spacing for a RecyclerView that is the primary content holder on screen.
+ */
+fun defaultPrimaryContentSpacing(context: Context) = RvSpacing(
+    topSpace = context.dimenCompat(R.dimen.list_overscroll_top),
+    bottomSpace = context.dimenCompat(R.dimen.list_overscroll_bottom),
+    verticalItemSpace = context.dimenCompat(R.dimen.flow_gap_vertical),
+    horizontalItemSpace = context.dimenCompat(R.dimen.flow_gap_horizontal),
+)
+
+
+/**
+ * Spacing for a RecyclerView that is the primary content holder on screen.
+ */
+fun primaryContentWithToolbarSpacing(context: Context) = RvSpacing(
+    topSpace = context.dimenCompat(R.dimen.toolbar_height),
+    bottomSpace = context.dimenCompat(R.dimen.list_overscroll_bottom),
+    verticalItemSpace = context.dimenCompat(R.dimen.flow_gap_vertical),
+    horizontalItemSpace = context.dimenCompat(R.dimen.flow_gap_horizontal),
+)
+
+
+fun defaultHorizontalOverscroll(context: Context) = RvSpacing(
+    startSpace = context.dimenCompat(R.dimen.list_overscroll_left),
+    horizontalItemSpace = context.dimenCompat(R.dimen.flow_gap_horizontal),
+    endSpace = context.dimenCompat(R.dimen.list_overscroll_right)
 )
