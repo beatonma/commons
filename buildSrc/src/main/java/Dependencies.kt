b14e8a9fd040d5e85b66808e6d5baf800a330539
Michael Beaton <beatonma@gmail.com>
2020-07-27 15:51:55 +0100

    Add Hilt.TESTING. Version bump Kotlin to 1.4.0-rc.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,51 +1,18 @@
 import org.gradle.api.JavaVersion
 
-private fun dependency(group: String, artifact: String, version: String) = "$group:$artifact:$version"
-private fun androidx(artifact: String, version: String, group: String = artifact) =
-    dependency("androidx.$group", artifact, version)
-
-private fun bma(artifact: String, version: String = Versions.BMA) = dependency("org.beatonma.lib", artifact, version)
-private fun dagger(artifact: String, version: String = Versions.DAGGER) =
-    dependency("com.google.dagger", artifact, version)
-
-private fun hilt(artifact: String, version: String = Versions.HILT) = dagger(artifact = artifact, version = version)
-private fun hiltAX(artifact: String, version: String = Versions.AX_HILT) = androidx(group = "hilt", artifact = artifact, version = version)
-
-private fun espresso(artifact: String, version: String = Versions.ESPRESSO) =
-    androidx(group = "test.espresso", artifact = artifact, version = version)
-
-private fun glide(artifact: String, version: String = Versions.GLIDE) =
-    dependency("com.github.bumptech.glide", artifact, version)
-
-private fun gms(artifact: String, version: String) =
-    dependency(group = "com.google.android.gms", artifact = artifact, version = version)
-
-private fun kotlin(artifact: String, version: String = Versions.KOTLIN) =
-    dependency(group = "org.jetbrains.kotlin", artifact = artifact, version = version)
-
-private fun kotlinx(artifact: String, version: String = Versions.KOTLIN) =
-    dependency(group = "org.jetbrains.kotlinx", artifact = artifact, version = version)
-
-private fun retrofit(artifact: String, version: String = Versions.RETROFIT) =
-    dependency("com.squareup.retrofit2", artifact, version)
-
-private fun room(artifact: String, version: String = Versions.ROOM) =
-    androidx(group = "room", artifact = artifact, version = version)
-
-private fun moshi(artifact: String, version: String = Versions.MOSHI) =
-    dependency("com.squareup.moshi", artifact, version)
 
 object Versions {
     const val BMA = "0.9.29"
     const val COMPOSE = "0.1.0-dev07"
-    const val COROUTINES = "1.3.7-1.4-M3"
+    const val COROUTINES = "1.3.8-1.4.0-rc"
     const val DAGGER = "2.28.1"
     const val ESPRESSO = "3.3.0-beta02"
     const val GLIDE = "4.11.0"
+    const val GROUPIE = "2.8.0"
     const val HILT = "2.28.1-alpha"
     const val AX_HILT = "1.0.0-alpha01"
     val JAVA = JavaVersion.VERSION_1_8
-    const val KOTLIN = "1.4-M3"
+    const val KOTLIN = "1.4.0-rc"
     const val RETROFIT = "2.9.0"
     const val ROOM = "2.2.5"
     const val MOSHI = "1.9.2"
@@ -106,6 +73,8 @@ object Dependencies {
         val CORE = hilt("hilt-android")
         val WORK = hiltAX("hilt-work")
 
+        val TESTING = hilt("hilt-android-testing")
+
         val KAPT = hilt("hilt-android-compiler")
         val AX_KAPT = hiltAX("hilt-compiler")
     }
@@ -123,11 +92,17 @@ object Dependencies {
         val MATERIAL = dependency("com.google.android.material", "material", "1.2.0-alpha06")
 
         object Play {
-            val AUTH = gms(artifact= "play-services-auth", version = "18.0.0")
-            val LOCATION = gms(artifact = "play-services-location", version = "17.0.0")
-            val MAPS = gms(artifact = "play-services-maps", version = "17.0.0")
-            val MAPS_UTIL = dependency("com.google.maps.android", artifact="android-maps-utils-sdk-v3-compat", version="0.1")
+            val AUTH = gms("play-services-auth", "18.0.0")
+            val LOCATION = gms("play-services-location", "17.0.0")
+            val MAPS = gms("play-services-maps", "17.0.0")
+            val MAPS_UTIL = dependency("com.google.maps.android", artifact = "android-maps-utils-sdk-v3-compat", version = "0.1")
         }
+    }
+
+    object Groupie {
+        val CORE = dependency("com.xwray", artifact = "groupie", version = Versions.GROUPIE)
+        val KTX = dependency("com.xwray", artifact = "groupie-kotlin-android-extensions", version = Versions.GROUPIE)
+        val VIEWBINDING = dependency("com.xwray", artifact = "groupie-viewbinding", version = Versions.GROUPIE)
     }
 
     object Kotlin {
@@ -188,3 +163,40 @@ object Dependencies {
         val OKHTTP_MOCK_SERVER = dependency("com.squareup.okhttp3", "mockwebserver", "4.7.2")
     }
 }
+
+
+
+private fun dependency(group: String, artifact: String, version: String) = "$group:$artifact:$version"
+private fun androidx(artifact: String, version: String, group: String = artifact) =
+    dependency("androidx.$group", artifact, version)
+
+private fun bma(artifact: String, version: String = Versions.BMA) = dependency("org.beatonma.lib", artifact, version)
+private fun dagger(artifact: String, version: String = Versions.DAGGER) =
+    dependency("com.google.dagger", artifact, version)
+
+private fun hilt(artifact: String, version: String = Versions.HILT) = dagger(artifact = artifact, version = version)
+private fun hiltAX(artifact: String, version: String = Versions.AX_HILT) = androidx(group = "hilt", artifact = artifact, version = version)
+
+private fun espresso(artifact: String, version: String = Versions.ESPRESSO) =
+    androidx(group = "test.espresso", artifact = artifact, version = version)
+
+private fun glide(artifact: String, version: String = Versions.GLIDE) =
+    dependency("com.github.bumptech.glide", artifact, version)
+
+private fun gms(artifact: String, version: String) =
+    dependency(group = "com.google.android.gms", artifact = artifact, version = version)
+
+private fun kotlin(artifact: String, version: String = Versions.KOTLIN) =
+    dependency(group = "org.jetbrains.kotlin", artifact = artifact, version = version)
+
+private fun kotlinx(artifact: String, version: String = Versions.KOTLIN) =
+    dependency(group = "org.jetbrains.kotlinx", artifact = artifact, version = version)
+
+private fun retrofit(artifact: String, version: String = Versions.RETROFIT) =
+    dependency("com.squareup.retrofit2", artifact, version)
+
+private fun room(artifact: String, version: String = Versions.ROOM) =
+    androidx(group = "room", artifact = artifact, version = version)
+
+private fun moshi(artifact: String, version: String = Versions.MOSHI) =
+    dependency("com.squareup.moshi", artifact, version)
