ec64ac0bd8937e009b8f199658d34060b185a555
Michael Beaton <beatonma@gmail.com>
2020-07-28 16:26:58 +0100

    Updated featured network calls to use Api- class variants.

--- app/src/main/java/org/beatonma/commons/data/core/repository/FeaturedRepository.kt
+++ app/src/main/java/org/beatonma/commons/data/core/repository/FeaturedRepository.kt
@@ -1,8 +1,10 @@
 package org.beatonma.commons.data.core.repository
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import org.beatonma.commons.data.*
+import org.beatonma.commons.data.CommonsRemoteDataSource
+import org.beatonma.commons.data.FlowIoResultList
+import org.beatonma.commons.data.IoResultList
+import org.beatonma.commons.data.cachedResultFlow
 import org.beatonma.commons.data.core.room.dao.BillDao
 import org.beatonma.commons.data.core.room.dao.DivisionDao
 import org.beatonma.commons.data.core.room.dao.MemberDao
@@ -10,9 +12,7 @@ import org.beatonma.commons.data.core.room.entities.bill.FeaturedBill
 import org.beatonma.commons.data.core.room.entities.bill.FeaturedBillWithBill
 import org.beatonma.commons.data.core.room.entities.division.FeaturedDivision
 import org.beatonma.commons.data.core.room.entities.division.FeaturedDivisionWithDivision
-import org.beatonma.commons.data.core.room.entities.member.FeaturedMember
 import org.beatonma.commons.data.core.room.entities.member.FeaturedMemberProfile
-import org.beatonma.commons.kotlin.extensions.dump
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -23,39 +23,20 @@ class FeaturedRepository @Inject constructor(
     private val billDao: BillDao,
     private val divisionDao: DivisionDao,
 ) {
-//    fun getFeaturedPeople(): LiveDataIoResultList<FeaturedMemberProfile> = resultLiveData(
-//        databaseQuery = { memberDao.getFeaturedProfiles() },
-//        networkCall = { remoteSource.getFeaturedPeople() },
-//        saveCallResult = { profiles ->
-//            memberDao.safeInsertProfiles(profiles, ifNotExists = true)
-//
-//            memberDao.apply {
-//                insertFeaturedPeople(
-//                    profiles.map { profile -> FeaturedMember(profile.parliamentdotuk) }
-//                )
-//            }
-//        }
-//    )
 
-    fun getFeaturedPeople(): FlowIoResultList<FeaturedMemberProfile> = resultFlow(
+    fun getFeaturedPeople(): FlowIoResultList<FeaturedMemberProfile> = cachedResultFlow(
         databaseQuery = { memberDao.getFeaturedProfiles() },
         networkCall = { remoteSource.getFeaturedPeople() },
         saveCallResult = { profiles ->
-            memberDao.safeInsertProfiles(profiles, ifNotExists = true)
-
-            memberDao.apply {
-                insertFeaturedPeople(
-                    profiles.map { profile -> FeaturedMember(profile.parliamentdotuk) }
-                )
-            }
+            memberDao.saveFeaturedPeople(profiles.map { it.toMemberProfile() })
         }
     )
 
-    fun getFeaturedBills(): FlowIoResultList<FeaturedBillWithBill> = resultFlow(
+    fun getFeaturedBills(): FlowIoResultList<FeaturedBillWithBill> = cachedResultFlow(
         databaseQuery = { billDao.getFeaturedBills() },
         networkCall = { remoteSource.getFeaturedBills() },
         saveCallResult = { bills ->
-            billDao.insertBills(bills)
+            billDao.insertBills(bills.map { it.toBill() })
 
             billDao.insertFeaturedBills(
                 bills.map { FeaturedBill(it.parliamentdotuk) }
@@ -63,34 +44,7 @@ class FeaturedRepository @Inject constructor(
         }
     )
 
-//    fun getFeaturedBills(): LiveDataIoResultList<FeaturedBillWithBill> = resultLiveData(
-//        databaseQuery = { billDao.getFeaturedBills() },
-//        networkCall = { remoteSource.getFeaturedBills() },
-//        saveCallResult = { bills ->
-//            billDao.insertBills(bills)
-//
-//            billDao.insertFeaturedBills(
-//                bills.map { FeaturedBill(it.parliamentdotuk) }
-//            )
-//        }
-//    )
-
-    // This works
-    fun getFeaturedDivisions(): Flow<IoResultList<FeaturedDivisionWithDivision>> =
-        divisionDao.getFeaturedDivisionsFlow().map { SuccessResult(it.dump(), "DBOK") }
-
-    // This works
-//    fun getFeaturedDivisionsFlow(): Flow<IoResultList<FeaturedDivisionWithDivision>> = flow<IoResultList<FeaturedDivisionWithDivision>> {
-//        divisionDao.getFeaturedDivisionsFlow()
-//            .mapLatest {
-//                SuccessResult(it, "DB OK")
-//            }
-//            .collect {
-//                emit(it)
-//            }
-//    }
-
-    fun getFeaturedDivisionsFlow(): Flow<IoResultList<FeaturedDivisionWithDivision>> = resultFlow(
+    fun getFeaturedDivisions(): Flow<IoResultList<FeaturedDivisionWithDivision>> = cachedResultFlow(
         databaseQuery = { divisionDao.getFeaturedDivisionsFlow() },
         networkCall = { remoteSource.getFeaturedDivisions() },
         saveCallResult = { divisions ->
