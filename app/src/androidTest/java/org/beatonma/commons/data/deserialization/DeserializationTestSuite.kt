57e8c6712bcd12972de6d6248a2c84dbb6ff7f58
Michael Beaton <beatonma@gmail.com>
2020-07-28 13:35:55 +0100

    Added LiveDeserializationTest to DeserializationTestSuite

--- app/src/androidTest/java/org/beatonma/commons/data/deserialization/DeserializationTestSuite.kt
+++ app/src/androidTest/java/org/beatonma/commons/data/deserialization/DeserializationTestSuite.kt
@@ -1,10 +1,12 @@
 package org.beatonma.commons.data.deserialization
 
+import org.beatonma.commons.data.deserialization.api.LiveDeserializationTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
     DivisionDeserializeTest::class,
+    LiveDeserializationTest::class,
 )
 class DeserializationTestSuite
