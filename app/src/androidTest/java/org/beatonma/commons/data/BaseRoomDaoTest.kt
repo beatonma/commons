91f0ae540faf84138b011de2c7a5179242dd4b44
Michael Beaton <beatonma@gmail.com>
2020-07-29 15:49:30 +0100

    Removed obsolete daoLiveDataTest()

--- app/src/androidTest/java/org/beatonma/commons/data/BaseRoomDaoTest.kt
+++ app/src/androidTest/java/org/beatonma/commons/data/BaseRoomDaoTest.kt
@@ -1,10 +1,8 @@
 package org.beatonma.commons.data
 
-import androidx.lifecycle.LiveData
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import org.beatonma.commons.androidTest.getOrAwaitValue
 
 private const val TAG = "BaseRoomTest"
 
@@ -15,10 +13,6 @@ abstract class BaseRoomDaoTest<D>: BaseRoomTest() {
     /**
      * Run the given function on the dao with the standard PUK as defined in test data
      */
-    @Deprecated("Dao methods should return a [Flow], not [LiveData]")
-    protected fun <T> daoLiveDataTest(func: D.(Int) -> LiveData<T>, testBlock: T.() -> Unit) =
-        dao.func(testPukId).getOrAwaitValue { testBlock(this) }
-
     protected fun <T> daoTest(func: D.(Int) -> Flow<T>, testBlock: T.() -> Unit) = runBlocking {
         dao.func(testPukId).first().run { testBlock(this) }
     }
