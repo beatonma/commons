293f44f76dc7580a0d78fe6b423a7f88c0b9b6ea
Michael Beaton <beatonma@gmail.com>
2020-07-30 11:47:39 +0100

    LiveData -> Flow

--- app/src/main/java/org/beatonma/commons/data/core/room/dao/ConstituencyDao.kt
+++ app/src/main/java/org/beatonma/commons/data/core/room/dao/ConstituencyDao.kt
@@ -1,8 +1,8 @@
 package org.beatonma.commons.data.core.room.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
-import org.beatonma.commons.data.LiveDataList
+import kotlinx.coroutines.flow.Flow
+import org.beatonma.commons.data.FlowList
 import org.beatonma.commons.data.ParliamentID
 import org.beatonma.commons.data.core.room.dao.shared.SharedConstituencyDao
 import org.beatonma.commons.data.core.room.dao.shared.SharedElectionDao
@@ -10,30 +10,21 @@ import org.beatonma.commons.data.core.room.entities.constituency.*
 import org.beatonma.commons.data.core.room.entities.election.ConstituencyResult
 import org.beatonma.commons.data.core.room.entities.election.ConstituencyResultWithDetails
 import org.beatonma.commons.data.core.room.entities.election.Election
-import org.beatonma.commons.data.core.room.entities.member.Party
 
 @Dao
 interface ConstituencyDao: SharedConstituencyDao, SharedElectionDao {
-
+    // Get operations
     @Transaction
-    @Query("""SELECT * FROM constituencies WHERE constituency_parliamentdotuk = :parliamentdotuk""")
-    fun getConstituencyWithBoundary(parliamentdotuk: ParliamentID): LiveData<ConstituencyWithBoundary>
+    @Query("""SELECT * FROM constituencies WHERE constituency_parliamentdotuk = :constituencyId""")
+    fun getConstituencyWithBoundary(constituencyId: ParliamentID): Flow<ConstituencyWithBoundary>
 
     @Transaction
     @Query("""SELECT * FROM constituency_results
         INNER JOIN member_profiles ON member_profiles.parliamentdotuk = constituency_results.result_member_id
         INNER JOIN elections ON elections.election_parliamentdotuk = constituency_results.result_election_id
         INNER JOIN parties ON parties.party_parliamentdotuk = member_profiles.party_id
-        WHERE constituency_results.result_constituency_id = :parliamentdotuk""")
-    fun getElectionResults(parliamentdotuk: ParliamentID): LiveDataList<ConstituencyResultWithDetails>
-
-    @Query("""SELECT * FROM constituency_election_results
-        WHERE c_e_r_constituency_id = :constituencyId
-        AND c_e_r_election_id = :electionId""")
-    fun getDetailsForElection(
-        constituencyId: ParliamentID,
-        electionId: ParliamentID,
-    ): LiveData<ConstituencyElectionDetails>
+        WHERE constituency_results.result_constituency_id = :constituencyId""")
+    fun getElectionResults(constituencyId: ParliamentID): FlowList<ConstituencyResultWithDetails>
 
     @Transaction
     @Query("""SELECT * FROM constituency_election_results
@@ -42,17 +33,16 @@ interface ConstituencyDao: SharedConstituencyDao, SharedElectionDao {
     fun getDetailsAndCandidatesForElection(
         constituencyId: ParliamentID,
         electionId: ParliamentID,
-    ): LiveData<ConstituencyElectionDetailsWithCandidates?>
-    
-    @Query("""SELECT * FROM parties WHERE party_name LIKE :fuzzyName""")
-    fun getPartyByName(fuzzyName: String): LiveData<Party?>
+    ): Flow<ConstituencyElectionDetailsWithCandidates>
 
-    @Query("""SELECT * FROM constituencies WHERE constituency_parliamentdotuk = :parliamentdotuk""")
-    fun getConstituency(parliamentdotuk: ParliamentID): LiveData<Constituency>
+    @Query("""SELECT * FROM constituencies WHERE constituency_parliamentdotuk = :constituencyId""")
+    fun getConstituency(constituencyId: ParliamentID): Flow<Constituency>
 
-    @Query("""SELECT * FROM elections WHERE election_parliamentdotuk = :parliamentdotuk""")
-    fun getElection(parliamentdotuk: ParliamentID): LiveData<Election>
+    @Query("""SELECT * FROM elections WHERE election_parliamentdotuk = :electionId""")
+    fun getElection(electionId: ParliamentID): Flow<Election>
 
+
+    // Insert operations
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertConstituencies(constituency: List<Constituency>)
 
