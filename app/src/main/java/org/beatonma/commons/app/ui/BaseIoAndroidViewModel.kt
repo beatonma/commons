35c32dec18037ffdddb578ac6a6923bdd7fd7b34
Michael Beaton <beatonma@gmail.com>
2020-08-04 12:30:28 +0100

    Replace context.commonsApp -> context.app

--- app/src/main/java/org/beatonma/commons/app/ui/BaseIoAndroidViewModel.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/BaseIoAndroidViewModel.kt
@@ -2,11 +2,11 @@ package org.beatonma.commons.app.ui
 
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
-import org.beatonma.commons.commonsApp
+import org.beatonma.commons.app
 import org.beatonma.commons.data.LiveDataIoResult
 
 abstract class BaseIoAndroidViewModel<D>(
     context: Context,
-): AndroidViewModel(context.commonsApp) {
+): AndroidViewModel(context.app) {
     lateinit var liveData: LiveDataIoResult<D>
 }
