c474ec54e167eb7c93f8a808a49169f1fa97568e
Michael Beaton <beatonma@gmail.com>
2020-08-04 12:22:48 +0100

    CommonsFragment now implements AsyncDiffHost for background recyclerview diffing.

--- app/src/main/java/org/beatonma/commons/app/ui/CommonsFragment.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/CommonsFragment.kt
@@ -1,11 +1,15 @@
 package org.beatonma.commons.app.ui
 
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.Job
+import org.beatonma.commons.app.ui.recyclerview.adapter.AsyncDiffHost
 import org.beatonma.commons.data.IoResult
 import org.beatonma.commons.data.NetworkError
 import org.beatonma.commons.kotlin.extensions.networkErrorSnackbar
 
-abstract class CommonsFragment : Fragment() {
+abstract class CommonsFragment : Fragment(), AsyncDiffHost {
+    override var diffJob: Job? = null
+
     fun <T> IoResult<T>.report() {
         if (this is NetworkError) networkErrorSnackbar(this)
     }
