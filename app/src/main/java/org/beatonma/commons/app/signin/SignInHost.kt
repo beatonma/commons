4cc8f215ad55744cb43dd2b6cf3dd5cba82f5e9e
Michael Beaton <beatonma@gmail.com>
2020-08-30 15:04:24 +0100

    Only create a new fragment instance if not already created.

--- app/src/main/java/org/beatonma/commons/app/signin/SignInHost.kt
+++ app/src/main/java/org/beatonma/commons/app/signin/SignInHost.kt
@@ -1,13 +1,20 @@
 package org.beatonma.commons.app.signin
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import org.beatonma.commons.R
 import org.beatonma.commons.kotlin.extensions.stringCompat
 
 interface SignInHost {
     fun showSignInDialog(activity: AppCompatActivity) {
-        val transaction = activity.supportFragmentManager.beginTransaction()
-        SignInFragment().show(transaction, activity.stringCompat(R.string.fragment_signin))
+        val manager = activity.supportFragmentManager
+        val tag = activity.stringCompat(R.string.fragment_signin)
+
+        val fragment = (
+                activity.supportFragmentManager.findFragmentByTag(tag)
+                    ?: SignInFragment()
+                ) as? DialogFragment
+        fragment?.show(manager, tag)
     }
 
     fun enableSignIn()
