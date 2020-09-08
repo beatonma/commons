0df14c4a1ade614d064c3e621b21d01701617f3e
Michael Beaton <beatonma@gmail.com>
2020-09-08 19:32:40 +0100

    Implemented Party.theme() for use in @Composable contexts.

--- app/src/main/java/org/beatonma/commons/app/ui/colors/PartyTheme.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/colors/PartyTheme.kt
@@ -1,6 +1,8 @@
 package org.beatonma.commons.app.ui.colors
 
 import android.content.Context
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
 import org.beatonma.commons.BuildConfig.*
 import org.beatonma.commons.R
 import org.beatonma.commons.core.ParliamentID
@@ -8,13 +10,34 @@ import org.beatonma.commons.data.core.room.entities.member.Party
 import org.beatonma.commons.isNightMode
 import org.beatonma.commons.kotlin.data.Color
 import org.beatonma.commons.kotlin.extensions.colorCompat
+import org.beatonma.commons.theme.compose.color.CommonsColor
+import androidx.compose.ui.graphics.Color as ComposeColor
 
 interface Themed {
+
     var theme: PartyColors?
 }
 
 fun Context.getPartyTheme(partyID: ParliamentID?) = getPartyTheme(partyID, this)
 fun Party.getTheme(context: Context): PartyColors = getPartyTheme(parliamentdotuk, context)
+
+@Composable
+fun Party.theme(): ComposePartyColors {
+    val isDarkTheme = isSystemInDarkTheme()
+    val naiveColors = getNaivePartyTheme(parliamentdotuk)
+
+    return if (isDarkTheme) {
+        naiveColors.coerce(
+            minSaturation = .2F,
+            maxSaturation = .8F,
+            minLuminance = .2F,
+            maxLuminance = .8F
+        ).resolve()
+    }
+    else {
+        naiveColors.resolve()
+    }
+}
 
 private fun getPartyTheme(partyID: ParliamentID?, context: Context): PartyColors {
     var naivePartyColors = getNaivePartyTheme(partyID)
@@ -181,163 +204,6 @@ private fun getNaivePartyTheme(partyID: ParliamentID?): NaivePartyColors = when 
     )
 }
 
-//private fun getPartyTheme(
-//    partyID: ParliamentID?,
-//    nightMode: Boolean? = false,
-//): PartyColors = when (getCanonicalParty(partyID)) {
-//    // Alliance
-//    PARTY_ALLIANCE_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_ALLIANCE_PRIMARY,
-//        COLOR_PARTY_ALLIANCE_ACCENT,
-//        COLOR_PARTY_ALLIANCE_PRIMARY_TEXT,
-//        COLOR_PARTY_ALLIANCE_ACCENT_TEXT
-//    )
-//
-//    // ChangeUK
-//    PARTY_THE_INDEPENDENT_GROUP_FOR_CHANGE_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_CHANGEUK_PRIMARY,
-//        COLOR_PARTY_CHANGEUK_ACCENT,
-//        COLOR_PARTY_CHANGEUK_PRIMARY_TEXT,
-//        COLOR_PARTY_CHANGEUK_ACCENT_TEXT
-//    )
-//
-//    // Conservative
-//    PARTY_CONSERVATIVE_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_CONSERVATIVE_PRIMARY,
-//        COLOR_PARTY_CONSERVATIVE_ACCENT,
-//        COLOR_PARTY_CONSERVATIVE_PRIMARY_TEXT,
-//        COLOR_PARTY_CONSERVATIVE_ACCENT_TEXT
-//    )
-//
-//    // DUP
-//    PARTY_DEMOCRATIC_UNIONIST_PARTY_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_DUP_PRIMARY,
-//        COLOR_PARTY_DUP_ACCENT,
-//        COLOR_PARTY_DUP_PRIMARY_TEXT,
-//        COLOR_PARTY_DUP_ACCENT_TEXT
-//    )
-//
-//    // Green
-//    PARTY_GREEN_PARTY_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_GREEN_PRIMARY,
-//        COLOR_PARTY_GREEN_ACCENT,
-//        COLOR_PARTY_GREEN_PRIMARY_TEXT,
-//        COLOR_PARTY_GREEN_ACCENT_TEXT
-//    )
-//
-//    // Labour
-//    PARTY_LABOUR_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_LABOUR_PRIMARY,
-//        COLOR_PARTY_LABOUR_ACCENT,
-//        COLOR_PARTY_LABOUR_PRIMARY_TEXT,
-//        COLOR_PARTY_LABOUR_ACCENT_TEXT
-//    )
-//
-//    // Labour Co-op
-//    PARTY_LABOUR_COOP_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_LABOURCOOP_PRIMARY,
-//        COLOR_PARTY_LABOURCOOP_ACCENT,
-//        COLOR_PARTY_LABOURCOOP_PRIMARY_TEXT,
-//        COLOR_PARTY_LABOURCOOP_ACCENT_TEXT
-//    )
-//
-//    // Liberal Democrat
-//    PARTY_LIBERAL_DEMOCRAT_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_LIBDEM_PRIMARY,
-//        COLOR_PARTY_LIBDEM_ACCENT,
-//        COLOR_PARTY_LIBDEM_PRIMARY_TEXT,
-//        COLOR_PARTY_LIBDEM_ACCENT_TEXT
-//    )
-//
-//    // Plaid Cymru
-//    PARTY_PLAID_CYMRU_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_PLAIDCYMRU_PRIMARY,
-//        COLOR_PARTY_PLAIDCYMRU_ACCENT,
-//        COLOR_PARTY_PLAIDCYMRU_PRIMARY_TEXT,
-//        COLOR_PARTY_PLAIDCYMRU_ACCENT_TEXT
-//    )
-//
-//    // Respect
-//    PARTY_RESPECT_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_RESPECT_PRIMARY,
-//        COLOR_PARTY_RESPECT_ACCENT,
-//        COLOR_PARTY_RESPECT_PRIMARY_TEXT,
-//        COLOR_PARTY_RESPECT_ACCENT_TEXT
-//    )
-//
-//    // SDLP
-//    PARTY_SOCIAL_DEMOCRATIC_LABOUR_PARTY_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_SDLP_PRIMARY,
-//        COLOR_PARTY_SDLP_ACCENT,
-//        COLOR_PARTY_SDLP_PRIMARY_TEXT,
-//        COLOR_PARTY_SDLP_ACCENT_TEXT
-//    )
-//
-//    // SDP
-//    PARTY_SOCIAL_DEMOCRATIC_PARTY_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_SDP_PRIMARY,
-//        COLOR_PARTY_SDP_ACCENT,
-//        COLOR_PARTY_SDP_PRIMARY_TEXT,
-//        COLOR_PARTY_SDP_ACCENT_TEXT
-//    )
-//
-//    // Sinn Fein
-//    PARTY_SINN_FEIN_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_SINNFEIN_PRIMARY,
-//        COLOR_PARTY_SINNFEIN_ACCENT,
-//        COLOR_PARTY_SINNFEIN_PRIMARY_TEXT,
-//        COLOR_PARTY_SINNFEIN_ACCENT_TEXT
-//    )
-//
-//    // SNP
-//    PARTY_SCOTTISH_NATIONAL_PARTY_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_SNP_PRIMARY,
-//        COLOR_PARTY_SNP_ACCENT,
-//        COLOR_PARTY_SNP_PRIMARY_TEXT,
-//        COLOR_PARTY_SNP_ACCENT_TEXT
-//    )
-//
-//    // Speaker
-//    PARTY_SPEAKER_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_SPEAKER_PRIMARY,
-//        COLOR_PARTY_SPEAKER_ACCENT,
-//        COLOR_PARTY_SPEAKER_PRIMARY_TEXT,
-//        COLOR_PARTY_SPEAKER_ACCENT_TEXT
-//    )
-//
-//    // UKIP
-//    PARTY_UK_INDEPENDENCE_PARTY_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_UKIP_PRIMARY,
-//        COLOR_PARTY_UKIP_ACCENT,
-//        COLOR_PARTY_UKIP_PRIMARY_TEXT,
-//        COLOR_PARTY_UKIP_ACCENT_TEXT
-//    )
-//
-//    // UUP
-//    PARTY_ULSTER_UNIONIST_PARTY_PARLIAMENTDOTUK -> PartyColors(
-//        COLOR_PARTY_UUP_PRIMARY,
-//        COLOR_PARTY_UUP_ACCENT,
-//        COLOR_PARTY_UUP_PRIMARY_TEXT,
-//        COLOR_PARTY_UUP_ACCENT_TEXT
-//    )
-//
-//    // Default
-//    else -> PartyColors(
-//        COLOR_PARTY_DEFAULT_PRIMARY,
-//        COLOR_PARTY_DEFAULT_ACCENT,
-//        COLOR_PARTY_DEFAULT_PRIMARY_TEXT,
-//        COLOR_PARTY_DEFAULT_ACCENT_TEXT
-//    )
-//}.apply {
-//    if (nightMode == true) {
-//        return coerce(
-//            minSaturation = .2F,
-//            maxSaturation = .8F,
-//            minLuminance = .2F,
-//            maxLuminance = .8F
-//        )
-//    }
-//}
 
 private data class NaivePartyColors(
     val primary: Int,
@@ -358,16 +224,33 @@ private data class NaivePartyColors(
             accentTextTheme,
         )
 
-    fun resolve(context: Context) = PartyColors(context, primary, accent, primaryTextTheme, accentTextTheme)
+    fun resolve(context: Context) =
+        PartyColors(context, primary, accent, primaryTextTheme, accentTextTheme)
+
+    @Composable
+    fun resolve(): ComposePartyColors = ComposePartyColors(
+        primary = ComposeColor(primary),
+        accent = ComposeColor(accent),
+        colorOnPrimary = if (primaryTextTheme == THEME_TEXT_DARK) CommonsColor.Text.PrimaryLight else CommonsColor.Text.PrimaryDark,
+        colorOnAccent = if (accentTextTheme == THEME_TEXT_DARK) CommonsColor.Text.PrimaryLight else CommonsColor.Text.PrimaryDark,
+    )
 }
+
+class ComposePartyColors(
+    val primary: ComposeColor,
+    val accent: ComposeColor,
+    val colorOnPrimary: ComposeColor,
+    val colorOnAccent: ComposeColor,
+)
 
 class PartyColors(
     context: Context,
     val primary: Int,
     val accent: Int,
-    val primaryTextTheme: Int,
-    val accentTextTheme: Int,
+    primaryTextTheme: Int,
+    accentTextTheme: Int
 ) {
+
     val textPrimaryOnPrimary: Int = resolve(context, primaryTextTheme, true)
     val textSecondaryOnPrimary: Int = resolve(context, primaryTextTheme, false)
     val textPrimaryOnAccent: Int = resolve(context, accentTextTheme, true)
