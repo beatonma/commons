78c910326603d09e6c8c6e0b6e869f5e6fb36edf
Michael Beaton <beatonma@gmail.com>
2020-09-25 19:39:37 +0100

    Moved Float extensions to core module

--- core/src/main/java/org/beatonma/commons/core/extensions/Float.kt
+++ core/src/main/java/org/beatonma/commons/core/extensions/Float.kt
@@ -1,9 +1,18 @@
-package org.beatonma.commons.kotlin.extensions
+package org.beatonma.commons.core.extensions
 
-fun Float.map(fromMin: Float, fromMax: Float, toMin: Float, toMax: Float): Float {
-    return normalizeIn(fromMin, fromMax)
+import java.lang.Float.max
+import java.lang.Float.min
+import kotlin.math.roundToInt
+
+fun Float.map(fromMin: Float, fromMax: Float, toMin: Float, toMax: Float): Float =
+    normalizeIn(fromMin, fromMax)
         .mapTo(toMin, toMax)
-}
+
+fun Float.safeMap(fromLimit1: Float, fromLimit2: Float, toMin: Float, toMax: Float): Float = map(
+    min(fromLimit1, fromLimit2),
+    max(fromLimit1, fromLimit2),
+    toMin, toMax
+)
 
 /**
  * Receiver value is assumed to be between 0F..1F!
@@ -11,7 +20,7 @@ fun Float.map(fromMin: Float, fromMax: Float, toMin: Float, toMax: Float): Float
 fun Float.mapTo(min: Int, max: Int, forcePositive: Boolean = true): Int {
     val range: Float = (max - min).toFloat()
     val self = if (forcePositive) this.coerceIn(0F, 1F) else this
-    return (min + (self * range)).toInt()
+    return (min + (self * range)).roundToInt()
         .coerceIn(min, max)
 }
 
@@ -30,6 +39,11 @@ fun Float.mapTo(min: Float, max: Float): Float {
 }
 
 /**
+ * Convenience for [mapTo] where min == 0F
+ */
+fun Float.mapTo(max: Float): Float = mapTo(0F, max)
+
+/**
  * Map to a value between 0F..1F relative to the given limits.
  */
 fun Float.normalizeIn(min: Float, max: Float): Float {
@@ -39,6 +53,13 @@ fun Float.normalizeIn(min: Float, max: Float): Float {
 }
 
 /**
+ * Convenience for [normalizeIn] where min == 0F
+ */
+fun Float.normalize(max: Float): Float = normalizeIn(0F, max)
+
+/**
  * Receiver value is assumed to be between 0F..1F!
  */
 fun Float.mapToByte(): Int = mapTo(0, 255)
+
+fun Int.normalizeIn(min: Int, max: Int): Float = toFloat().normalizeIn(min.toFloat(), max.toFloat())
