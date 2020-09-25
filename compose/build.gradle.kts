28c055cc51aad1076e5db124f169b17c032591a7
Michael Beaton <beatonma@gmail.com>
2020-09-25 19:37:53 +0100

    OptionalText

---
+++ compose/build.gradle.kts
@@ -0,0 +1,16 @@
+import org.beatonma.commons.buildsrc.kts.extensions.main
+
+plugins {
+    id(Plugins.COMMONS_LIBRARY_CONFIG)
+    id(Plugins.COMMONS_COMPOSE_MODULE)
+}
+
+dependencies {
+    main {
+        implementations(
+            Dependencies.AndroidX.CORE_KTX,
+            project(":core"),
+            project(":theme")
+        )
+    }
+}
