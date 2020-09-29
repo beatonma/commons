f3b3ba108ddbe3796859fbddc948ada4e06df757
Michael Beaton <beatonma@gmail.com>
2020-09-29 17:19:35 +0100

    Added chooseAny() list extension which returns a random selection of items from the receiving list.

--- core/src/main/java/org/beatonma/commons/core/extensions/Collections.kt
+++ core/src/main/java/org/beatonma/commons/core/extensions/Collections.kt
@@ -21,10 +21,20 @@ fun Collection<Any?>.allNotNull(): Boolean {
 fun <T> firstNotNull(vararg args: T?): T? = args.firstOrNull { it != null }
 fun <T> Collection<T?>.firstNotNull() = firstOrNull { it != null }
 
-
 fun <T> List<T>.safeGet(position: Int): T? {
     return when {
         this.size > position -> this[position]
         else -> null
+    }
+}
+
+fun <T> List<T>.chooseAny(population: Int = size / 2): List<T> {
+    val available = (0 until size).toMutableSet()
+
+    return List(population) {
+        val index = available.random()
+        val v = get(index)
+        available.remove(index)
+        v
     }
 }
