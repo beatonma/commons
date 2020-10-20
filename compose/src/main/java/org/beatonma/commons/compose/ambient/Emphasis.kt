d997d2be9b6deb921abbbc738f9d79243405b99f
Michael Beaton <beatonma@gmail.com>
2020-10-20 21:42:29 +0100

    EmphasisAmbient -> AmbientEmphasisLevels

--- compose/src/main/java/org/beatonma/commons/compose/ambient/Emphasis.kt
+++ compose/src/main/java/org/beatonma/commons/compose/ambient/Emphasis.kt
@@ -1,17 +1,17 @@
 package org.beatonma.commons.compose.ambient
 
-import androidx.compose.material.EmphasisAmbient
+import androidx.compose.material.AmbientEmphasisLevels
 import androidx.compose.material.ProvideEmphasis
 import androidx.compose.runtime.Composable
 
 @Composable
 fun withEmphasisHigh(content: @Composable () -> Unit) =
-    ProvideEmphasis(emphasis = EmphasisAmbient.current.high, content = content)
+    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.high, content = content)
 
 @Composable
 fun withEmphasisMedium(content: @Composable () -> Unit) =
-    ProvideEmphasis(emphasis = EmphasisAmbient.current.medium, content = content)
+    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.medium, content = content)
 
 @Composable
 fun withEmphasisDisabled(content: @Composable () -> Unit) =
-    ProvideEmphasis(emphasis = EmphasisAmbient.current.disabled, content = content)
+    ProvideEmphasis(emphasis = AmbientEmphasisLevels.current.disabled, content = content)
