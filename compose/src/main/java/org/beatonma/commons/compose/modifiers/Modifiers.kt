4f96f02467a9c16a8091718d7b939f854585c62e
Michael Beaton <beatonma@gmail.com>
2020-10-20 21:40:19 +0100

    Added Modifier.colorize() to help with figuring out layouts/layout animations.

--- compose/src/main/java/org/beatonma/commons/compose/modifiers/Modifiers.kt
+++ compose/src/main/java/org/beatonma/commons/compose/modifiers/Modifiers.kt
@@ -1,8 +1,10 @@
 package org.beatonma.commons.compose.modifiers
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -10,8 +12,9 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.beatonma.commons.compose.ambient.shapes
+import org.beatonma.commons.compose.util.toPrettyHexString
 import org.beatonma.commons.theme.compose.Whitespace
-
+import org.beatonma.commons.theme.compose.color.anyMaterialColor
 
 @Composable
 fun Modifier.withBorder(
@@ -35,3 +38,15 @@ fun Modifier.withBorder(
         padding(width)
     }
     .clip(shape)
+
+/**
+ * Set a random background color - useful for figuring out animations.
+ */
+@Composable
+fun Modifier.colorize(tag: String? = null, alpha: Float = 0.6F): Modifier {
+    val color = remember { anyMaterialColor.copy(alpha = alpha) }
+    if (tag != null) {
+        println("$tag: ${color.toPrettyHexString()}")
+    }
+    return this.then(background(color))
+}
