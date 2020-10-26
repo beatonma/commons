f7033dcf6beedef9ee7d3f7c6e1e7bb0dee59bce
Michael Beaton <beatonma@gmail.com>
2020-10-26 19:27:12 +0000

    Global constants for layout whitespace.

--- theme/src/main/java/org/beatonma/commons/theme/compose/Whitespace.kt
+++ theme/src/main/java/org/beatonma/commons/theme/compose/Whitespace.kt
@@ -1,29 +1,45 @@
 package org.beatonma.commons.theme.compose
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.ui.unit.dp
 
 object Whitespace {
     object List {
         object Vertical {
-
             val between = 8.dp
             val around = 16.dp
         }
 
         object Horizontal {
-
             val between = 12.dp
             val around = 16.dp
         }
     }
 
     object Image {
-
         val around = 8.dp
     }
 
-    object TextList {
+    object Icon {
+        object Small {
+            val around = 8.dp
+        }
 
+        object Large {
+            val around = 16.dp
+        }
+    }
+
+    object TextList {
         val between = 8.dp
     }
+
+    object WindowContent {
+        val bottom = 160.dp
+    }
+}
+
+object Padding {
+    val VerticalList = PaddingValues(bottom = Whitespace.List.Vertical.between)
+    val Card = PaddingValues(start = 12.dp, top = 12.dp, end = 12.dp, bottom = 16.dp)
 }
