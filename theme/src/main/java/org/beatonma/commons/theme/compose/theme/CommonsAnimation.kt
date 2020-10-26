4c0621d64abe9fd88e9a4f2ac98865be2c183d4b
Michael Beaton <beatonma@gmail.com>
2020-10-26 19:25:46 +0000

    App-standard animation stuff

---
+++ theme/src/main/java/org/beatonma/commons/theme/compose/theme/CommonsAnimation.kt
@@ -0,0 +1,21 @@
+package org.beatonma.commons.theme.compose.theme
+
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+
+private const val DEFAULT_DURATION = 300
+
+fun <T> CommonsSpring() = spring<T>(Spring.DampingRatioLowBouncy, Spring.StiffnessLow)
+fun <T> CommonsTween(
+    duration: Int = DEFAULT_DURATION,
+    easing: CubicBezierEasing = FastOutSlowInEasing,
+) =
+    tween<T>(duration, easing = easing)
+
+fun cubicBezier(a: Number, b: Number, c: Number, d: Number) =
+    CubicBezierEasing(a.toFloat(), b.toFloat(), c.toFloat(), d.toFloat())
+
+val ExpCubicEasing: CubicBezierEasing = cubicBezier(0.55, 0, 1, 0.45)
