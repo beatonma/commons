6248d4ab7c9c32e643f34dd2f7bcab5da98e410d
Michael Beaton <beatonma@gmail.com>
2020-10-30 20:41:52 +0000

    Added OptionalText variation to accept AnnotatedString

--- compose/src/main/java/org/beatonma/commons/compose/components/Text.kt
+++ compose/src/main/java/org/beatonma/commons/compose/components/Text.kt
@@ -2,9 +2,11 @@ package org.beatonma.commons.compose.components
 
 import androidx.compose.foundation.AmbientTextStyle
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -53,6 +55,52 @@ fun OptionalText(
             overflow,
             softWrap,
             maxLines,
+            onTextLayout,
+            style
+        )
+    }
+}
+
+/**
+ * Display nothing if text is null
+ */
+@Composable
+fun OptionalText(
+    text: AnnotatedString?,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Inherit,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Inherit,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Inherit,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = AmbientTextStyle.current,
+) {
+    if (!text?.toString().isNullOrBlank()) {
+        Text(
+            text!!,
+            modifier,
+            color,
+            fontSize,
+            fontStyle,
+            fontWeight,
+            fontFamily,
+            letterSpacing,
+            textDecoration,
+            textAlign,
+            lineHeight,
+            overflow,
+            softWrap,
+            maxLines,
+            inlineContent,
             onTextLayout,
             style
         )
