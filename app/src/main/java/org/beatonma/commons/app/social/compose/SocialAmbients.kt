6eadec20d2c6e9c1fab72e742c5311039c8ae256
Michael Beaton <beatonma@gmail.com>
2020-11-02 18:27:33 +0000

    Added AmbientSocialTheme for applying contextual coloring to social ui.

--- app/src/main/java/org/beatonma/commons/app/social/compose/SocialAmbients.kt
+++ app/src/main/java/org/beatonma/commons/app/social/compose/SocialAmbients.kt
@@ -1,20 +1,38 @@
 package org.beatonma.commons.app.social.compose
 
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidableAmbient
 import androidx.compose.runtime.ambientOf
+import androidx.compose.ui.graphics.Color
 import org.beatonma.commons.ActionBlock
-import org.beatonma.commons.app.social.SocialViewModel
+import org.beatonma.commons.app.ui.colors.ComposePartyColors
 import org.beatonma.commons.snommoc.models.social.EmptySocialContent
 import org.beatonma.commons.snommoc.models.social.SocialComment
 
-val SocialAmbient = ambientOf { EmptySocialContent }
-val SocialViewModelAmbient =
-    ambientOf<SocialViewModel> { error("SocialViewModel has not been provided") }
-
-val SocialActionsAmbient = ambientOf { SocialActions() }
+val AmbientSocialContent = ambientOf { EmptySocialContent }
+val AmbientSocialTheme: ProvidableAmbient<SocialTheme> =
+    ambientOf { error("Social theme has not been provided") }
+val AmbientSocialActions = ambientOf { SocialActions() }
 
 class SocialActions(
     val onVoteUpClick: ActionBlock = {},
     val onVoteDownClick: ActionBlock = {},
     val onExpandedCommentIconClick: ActionBlock = {},
     val onCommentClick: (SocialComment) -> Unit = {},
+)
+
+class SocialTheme(
+    val collapsedBackground: Color = Color.Transparent,
+    val collapsedOnBackground: Color = Color.Transparent,
+    val expandedBackground: Color = Color.Transparent,
+    val expandedOnBackground: Color = Color.Transparent,
+)
+
+@Composable
+fun ComposePartyColors.asSocialTheme() = SocialTheme(
+    primary,
+    onPrimary,
+    colors.background,
+    colors.onBackground
 )
