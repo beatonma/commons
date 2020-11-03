c93b12f896a31d72b19f36c0dd71a743a05e1027
Michael Beaton <beatonma@gmail.com>
2020-11-03 12:25:04 +0000

    Constant values for zIndex rendering organisation.

---
+++ theme/src/main/java/org/beatonma/commons/theme/compose/Layer.kt
@@ -0,0 +1,12 @@
+package org.beatonma.commons.theme.compose
+
+/**
+ * Standard values for zIndex modifier to ensure consistency when rendering views that may overlap.
+ */
+object Layer {
+    const val Bottom = 0F
+    const val Low = 4F
+    const val Middle = 8F
+    const val High = 16F
+    const val Top = 128F
+}
