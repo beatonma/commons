8960fe3ae825e5d21aa6321d8987a5e32e216034
Michael Beaton <beatonma@gmail.com>
2020-11-09 17:48:14 +0000

    Added socialTransitionState()

--- app/src/main/java/org/beatonma/commons/app/social/SocialUiState.kt
+++ app/src/main/java/org/beatonma/commons/app/social/SocialUiState.kt
@@ -1,8 +1,11 @@
 package org.beatonma.commons.app.social
 
+import androidx.compose.animation.core.TransitionState
+import androidx.compose.animation.transition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import org.beatonma.commons.app.social.compose.rememberSocialScaffoldTransition
 
 enum class SocialUiState {
     Collapsed,
@@ -13,3 +16,9 @@ enum class SocialUiState {
 @Composable
 fun rememberSocialUiState(default: SocialUiState = SocialUiState.Collapsed) =
     remember { mutableStateOf(default) }
+
+@Composable
+fun socialTransitionState(toState: SocialUiState): TransitionState {
+    val transitionDef = rememberSocialScaffoldTransition()
+    return transition(definition = transitionDef, toState = toState)
+}
