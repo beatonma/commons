11b026feb88e751126dbf397f751536b287003d2
Michael Beaton <beatonma@gmail.com>
2020-11-09 13:15:31 +0000

    Added Fragment.withSystemUi() for composable blocks that need to know about window insets.

--- app/src/main/java/org/beatonma/commons/app/ui/compose/Fragment.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/compose/Fragment.kt
@@ -1,9 +1,13 @@
 package org.beatonma.commons.app.ui.compose
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import org.beatonma.commons.theme.compose.theme.CommonsTheme
+import org.beatonma.commons.theme.compose.theme.SystemBars
+import org.beatonma.commons.theme.compose.theme.systemui.withSystemUi
 
 fun Fragment.composeView(
     content: @Composable () -> Unit,
@@ -14,3 +18,13 @@ fun Fragment.composeView(
         }
     }
 }
+
+@Composable
+fun Fragment.withSystemUi(
+    systemBarColor: Color = MaterialTheme.colors.SystemBars,
+    content: @Composable () -> Unit,
+) = withSystemUi(
+    requireActivity().window,
+    systemBarColor,
+    content
+)
