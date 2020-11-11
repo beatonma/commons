bbd0b04531055fd7e14140f739068bcb8ed7a4a3
Michael Beaton <beatonma@gmail.com>
2020-11-11 21:50:03 +0000

    Now extends BaseUserAccountViewModel so that active user can be retrieved.

--- app/src/main/java/org/beatonma/commons/app/memberprofile/compose/ComposeMemberProfileViewModel.kt
+++ app/src/main/java/org/beatonma/commons/app/memberprofile/compose/ComposeMemberProfileViewModel.kt
@@ -1,19 +1,87 @@
 package org.beatonma.commons.app.memberprofile.compose
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.AndroidViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import org.beatonma.commons.app
+import org.beatonma.commons.app.signin.BaseUserAccountViewModel
 import org.beatonma.commons.core.ParliamentID
+import org.beatonma.commons.core.extensions.withNotNull
+import org.beatonma.commons.data.core.CompleteMember
+import org.beatonma.commons.data.core.interfaces.Temporal
+import org.beatonma.commons.data.core.interfaces.compressConsecutiveItems
+import org.beatonma.commons.data.core.room.entities.member.HistoricalConstituencyWithElection
+import org.beatonma.commons.data.core.room.entities.member.PartyAssociationWithParty
 import org.beatonma.commons.repo.repository.MemberRepository
+import org.beatonma.commons.repo.repository.UserRepository
 
 class ComposeMemberProfileViewModel
 @ViewModelInject constructor(
-    private val repository: MemberRepository,
+    private val memberRepository: MemberRepository,
+    userRepository: UserRepository,
     @ApplicationContext context: Context,
-): AndroidViewModel(context.app) {
+) : BaseUserAccountViewModel(userRepository, context.app) {
 
     fun forMember(parliamentID: ParliamentID) =
-        repository.getMember(parliamentID)
+        memberRepository.getMember(parliamentID)
+
+    suspend fun constructHistoryOf(member: CompleteMember): List<Temporal> {
+        fun <T : Temporal> MutableList<Temporal>.addEvents(items: List<T>?) {
+            withNotNull(items) { safeItems ->
+                addAll(
+                    safeItems.mapNotNull {
+                        try {
+                            it
+                        }
+                        catch (e: Exception) {
+                            null
+                        }
+                    }
+                )
+            }
+        }
+
+        return mutableListOf<Temporal>().apply {
+            addEvents(member.posts)
+            addEvents(member.houses)
+            addEvents(member.experiences)
+            addEvents(compressConstituencies(member.historicConstituencies))
+            addEvents(compressParties(member.parties))
+            addEvents(member.committees)
+            addEvents(member.financialInterests)
+
+            sortBy { it.startOf() }
+        }
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    suspend fun compressParties(parties: List<PartyAssociationWithParty>?) =
+        compressConsecutiveItems(
+            parties,
+            areItemsTheSame = { a, b -> a.party == b.party },
+            combineFunc = { a, b ->
+                a.copy(
+                    partyAssocation = a.partyAssocation.copy(
+                        end = b.end
+                    )
+                )
+            }
+        )
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    suspend fun compressConstituencies(constituencies: List<HistoricalConstituencyWithElection>?) =
+        compressConsecutiveItems(
+            constituencies,
+            areItemsTheSame = { a, b ->
+                a.constituency == b.constituency
+            },
+            combineFunc = { a, b ->
+                a.copy(
+                    historicalConstituency = a.historicalConstituency.copy(
+                        end = b.historicalConstituency.end
+                    )
+                )
+            }
+        )
 }
