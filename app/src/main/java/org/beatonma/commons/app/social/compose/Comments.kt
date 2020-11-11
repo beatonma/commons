839fe58019d75186a7006f57df12e05a9a24085b
Michael Beaton <beatonma@gmail.com>
2020-11-11 21:45:45 +0000

    Implemented bottomsheet swiping behaviour to dismiss comment creation UI. Comment TextField now requests focus when opened. Active account username is now displayed when creating a new comment.

--- app/src/main/java/org/beatonma/commons/app/social/compose/Comments.kt
+++ app/src/main/java/org/beatonma/commons/app/social/compose/Comments.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -24,12 +27,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawOpacity
 import androidx.compose.ui.draw.drawShadow
+import androidx.compose.ui.focus.ExperimentalFocus
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focusRequester
+import androidx.compose.ui.gesture.scrollorientationlocking.Orientation
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.beatonma.commons.BuildConfig
 import org.beatonma.commons.R
+import org.beatonma.commons.app.signin.AmbientUserToken
+import org.beatonma.commons.app.signin.NullUserToken
 import org.beatonma.commons.app.social.SocialUiState
 import org.beatonma.commons.compose.ambient.colors
 import org.beatonma.commons.compose.ambient.typography
@@ -37,17 +46,21 @@ import org.beatonma.commons.compose.ambient.withEmphasisHigh
 import org.beatonma.commons.compose.ambient.withEmphasisMedium
 import org.beatonma.commons.compose.animation.lerpBetween
 import org.beatonma.commons.compose.components.CardText
+import org.beatonma.commons.compose.components.Hint
 import org.beatonma.commons.compose.components.LengthTextFieldValidator
 import org.beatonma.commons.compose.components.ModalScrim
 import org.beatonma.commons.compose.components.ValidatedLengthTextField
 import org.beatonma.commons.compose.modifiers.either
+import org.beatonma.commons.compose.modifiers.onlyWhen
 import org.beatonma.commons.compose.modifiers.wrapContentSize
 import org.beatonma.commons.compose.util.update
 import org.beatonma.commons.core.extensions.lerp
 import org.beatonma.commons.core.extensions.lerpBetween
+import org.beatonma.commons.core.extensions.map
 import org.beatonma.commons.core.extensions.progressIn
 import org.beatonma.commons.core.extensions.reversed
 import org.beatonma.commons.core.extensions.withEasing
+import org.beatonma.commons.data.core.room.entities.user.UserToken
 import org.beatonma.commons.kotlin.extensions.formatted
 import org.beatonma.commons.snommoc.models.social.SocialComment
 import org.beatonma.commons.theme.compose.Elevation
@@ -55,6 +68,7 @@ import org.beatonma.commons.theme.compose.Padding
 import org.beatonma.commons.theme.compose.pdp
 import org.beatonma.commons.theme.compose.plus
 import org.beatonma.commons.theme.compose.theme.CORNER_SMALL
+import org.beatonma.commons.theme.compose.theme.CommonsSpring
 import org.beatonma.commons.theme.compose.theme.systemui.imeOrNavigationBarsPadding
 import org.beatonma.commons.theme.compose.theme.systemui.navigationBarsPadding
 
@@ -69,8 +83,9 @@ internal fun CommentList(
         NoComments(modifier)
     }
     else {
-        val progress = AmbientCollapseExpandProgress.current.progressIn(0.75F, 1F).withEasing(
-            FastOutSlowInEasing)
+        val progress = AmbientCollapseExpandProgress.current
+            .progressIn(0.75F, 1F)
+            .withEasing(FastOutSlowInEasing)
 
         LazyColumnForIndexed(comments, modifier) { i, comment ->
             Comment(
@@ -88,7 +103,8 @@ private fun NoComments(modifier: Modifier = Modifier) {
         Text(
             stringResource(R.string.social_comment_no_comments),
             modifier.fillMaxWidth(),
-            style = typography.h6, textAlign = TextAlign.Center)
+            style = typography.h6, textAlign = TextAlign.Center
+        )
     }
 }
 
@@ -110,9 +126,9 @@ private fun Comment(
 
         withEmphasisMedium {
             Row {
-                Text(comment.username,
+                Username(
+                    comment.username,
                     style = typography.caption,
-                    color = colors.secondary,
                     modifier = Modifier.padding(Padding.HorizontalListItem)
                 )
                 Text(comment.created.formatted(), style = typography.caption)
@@ -127,25 +143,62 @@ private fun Comment(
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CreateCommentUi(
+    userToken: UserToken = AmbientUserToken.current,
     uiState: MutableState<SocialUiState> = AmbientSocialUiState.current,
-    progress: Float = AmbientSocialUiTransition.current[ExpandedComposeCommentProgress],
+//    progress: Float = AmbientSocialUiTransition.current[ExpandedComposeCommentProgress],
 ) {
-    val surfaceShape = getCommentUiSurfaceShape(progress)
+    if (userToken == NullUserToken) {
+        // Show sign-in instead of add comment fab
+        // TODO
+    }
+
+    val swipeState = rememberSwipeableState(
+        initialValue = uiState.value,
+        animationSpec = CommonsSpring()
+    )
+    val swipeAnchors = mapOf(
+        0F to SocialUiState.Expanded,
+        500F to SocialUiState.ComposeComment
+    )
+
+    val targetState = uiState.value
+
+    val swipeable = Modifier.onlyWhen(targetState != SocialUiState.Collapsed) {
+        if (targetState != swipeState.value) {
+            swipeState.animateTo(targetState, onEnd = { _, finalState ->
+                uiState.update(finalState)
+            })
+        }
+
+        swipeable(
+            state = swipeState,
+            enabled = swipeState.value == SocialUiState.ComposeComment,
+
+            anchors = swipeAnchors,
+            orientation = Orientation.Vertical,
+            reverseDirection = true,
+        )
+    }
+
+    val offset = swipeState.offset.value
+    val progress = (if (offset.isNaN()) 0F else offset).map(0F, 500F, 0F, 1F)
 
     ModalScrim(
         alignment = Alignment.BottomEnd,
         alpha = progress,
         onClickAction = {
             uiState.update(SocialUiState.Expanded)
-
         }
     ) {
+        val surfaceShape = getCommentUiSurfaceShape(progress)
+
         Surface(
             Modifier
                 .padding(progress.lerpBetween(Padding.Fab, Padding.Zero))
                 .navigationBarsPadding(scale = progress.reversed()) // The bottom sheet should extend behind navigation bar as it expands.
+                .then(swipeable)
                 .either(
-                    condition = progress == 0F,
+                    condition = targetState == SocialUiState.Expanded,
                     whenTrue = {
                         clickable(onClick = AmbientSocialActions.current.onCreateCommentClick)
                     },
@@ -183,6 +236,7 @@ private fun CreateCommentButtonContent(
     )
 }
 
+@OptIn(ExperimentalFocus::class)
 @Composable
 private fun CreateCommentSheetContent(
     progress: Float,
@@ -192,19 +246,38 @@ private fun CreateCommentSheetContent(
         return
     }
 
+    val user = AmbientUserToken.current
+    val focusRequester = remember { FocusRequester() }
+
+    if (progress == 1F) {
+        // Focus on text field and bring up the IME.
+        focusRequester.requestFocus()
+    }
+
     CardText(
         Modifier
-            .wrapContentSize(horizontalProgress = progress.progressIn(0F, 0.4F),
-                verticalProgress = progress.progressIn(0F, 0.8F))
+            .wrapContentSize(
+                horizontalProgress = progress.progressIn(0F, 0.4F),
+                verticalProgress = progress.progressIn(0F, 0.8F)
+            )
             .drawOpacity(progress.progressIn(0.8F, 1F))
             .imeOrNavigationBarsPadding(scale = progress)
     ) {
 
         Column {
-            Text(stringResource(R.string.social_compose_comment),
-                modifier = Modifier.padding(Padding.VerticalListItem),
-                style = typography.h6
-            )
+            Row(
+                Modifier.fillMaxWidth()
+                    .padding(Padding.VerticalListItem),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    stringResource(R.string.social_compose_comment),
+                    style = typography.h6
+                )
+
+                Username(user.username)
+            }
 
             ValidatedLengthTextField(
                 value = commentText.value,
@@ -212,6 +285,9 @@ private fun CreateCommentSheetContent(
                     BuildConfig.SOCIAL_COMMENT_MIN_LENGTH,
                     BuildConfig.SOCIAL_COMMENT_MAX_LENGTH
                 ),
+                placeholder = {
+                    Hint(stringResource(R.string.social_compose_comment_hint))
+                },
                 onValueChange = { value, isValid ->
                     commentText.update(value)
 
@@ -223,10 +299,12 @@ private fun CreateCommentSheetContent(
                     .fillMaxWidth()
                     .heightIn(64.dp, 220.dp)
                     .animateContentSize()
+                    .focusRequester(focusRequester)
             )
 
+            val socialActions = AmbientSocialActions.current
             OutlinedButton(
-                onClick = AmbientSocialActions.current.onCommentSubmitClick,
+                onClick = { socialActions.onCommentSubmitClick(commentText.value) },
                 modifier = Modifier.align(Alignment.End).padding(Padding.CardButton),
             ) {
                 Text(stringResource(R.string.social_comment_post))
