4c13bace37b2d0a678152f60ee368ba38e917da7
Michael Beaton <beatonma@gmail.com>
2020-11-13 11:22:50 +0000

    composeView now provides onBackground as default content color

--- app/src/main/java/org/beatonma/commons/app/ui/compose/Fragment.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/compose/Fragment.kt
@@ -1,10 +1,13 @@
 package org.beatonma.commons.app.ui.compose
 
+import androidx.compose.foundation.AmbientContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import org.beatonma.commons.compose.ambient.colors
 import org.beatonma.commons.theme.compose.theme.CommonsTheme
 import org.beatonma.commons.theme.compose.theme.SystemBars
 import org.beatonma.commons.theme.compose.theme.systemui.withSystemUi
@@ -14,7 +17,11 @@ fun Fragment.composeView(
 ) = ComposeView(requireContext()).apply {
     setContent {
         CommonsTheme {
-            content()
+            Providers(
+                AmbientContentColor provides colors.onBackground,
+            ) {
+                content()
+            }
         }
     }
 }
