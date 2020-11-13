fac872722df8767d6a7e19612e839602a60c4bb6
Michael Beaton <beatonma@gmail.com>
2020-11-13 16:20:40 +0000

    Added CommonsSpanStyle with standard italic, bold and quote styles

--- theme/src/main/java/org/beatonma/commons/theme/compose/theme/CommonsTypography.kt
+++ theme/src/main/java/org/beatonma/commons/theme/compose/theme/CommonsTypography.kt
@@ -1,6 +1,11 @@
 package org.beatonma.commons.theme.compose.theme
 
+import androidx.compose.foundation.AmbientContentColor
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -11,3 +16,15 @@ val CommonsTypography = Typography(
     h2 = default.h2.copy(fontSize = 48.sp),
     h3 = default.h3.copy(fontSize = 42.sp)
 )
+
+object CommonsSpanStyle {
+    val italic: SpanStyle = SpanStyle(fontStyle = FontStyle.Italic)
+    val bold: SpanStyle = SpanStyle(fontWeight = FontWeight.Bold)
+    val quote: SpanStyle = SpanStyle(fontFamily = FontFamily.Monospace)
+
+    @Composable
+    fun quote(): SpanStyle = SpanStyle(
+        background = AmbientContentColor.current.copy(alpha = 0.2F),
+        fontFamily = FontFamily.Monospace,
+    )
+}
