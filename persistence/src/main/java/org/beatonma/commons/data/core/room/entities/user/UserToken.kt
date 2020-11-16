d8dcc8fb55d47b9acbfaf36916b490cc871db05a
Michael Beaton <beatonma@gmail.com>
2020-11-16 11:56:04 +0000

    Removed old commented code

--- persistence/src/main/java/org/beatonma/commons/data/core/room/entities/user/UserToken.kt
+++ persistence/src/main/java/org/beatonma/commons/data/core/room/entities/user/UserToken.kt
@@ -19,31 +19,3 @@ data class UserToken(
     @ColumnInfo(name = "google_id") val googleId: String,
     @ColumnInfo(name = "username") val username: String,
 )
-
-//data class ApiUserToken(
-//    @field:Json(name = Contract.SNOMMOC_TOKEN) val snommocToken: SnommocToken,
-//    @field:Json(name = Contract.GOOGLE_TOKEN) val googleTokenStub: String,
-//    @field:Json(name = Contract.USERNAME) val username: String,
-//) {
-//    fun composeToUserToken(account: UserAccount) = UserToken(
-//        name = account.name,
-//        photoUrl = account.photoUrl,
-//        email = account.email,
-//        snommocToken = this.snommocToken,
-//        googleId = account.googleId,
-//        username = this.username,
-//    )
-//}
-
-//
-//sealed class AccountAction(@field:Json(name = Contract.ACCOUNT_ACTION) val action: String)
-//data class RenameAccountRequest(
-//    @field:Json(name = Contract.USERNAME) val currentUsername: String,
-//    @field:Json(name = Contract.ACCOUNT_NEW_USERNAME) val newUsername: String,
-//    @field:Json(name = Contract.SNOMMOC_TOKEN) val token: SnommocToken,
-//): AccountAction(action=Contract.ACCOUNT_ACTION_CHANGE_USERNAME)
-//
-//data class DeleteUserRequest(
-//    @field:Json(name = Contract.GOOGLE_TOKEN) val gtoken: String,
-//    @field:Json(name = Contract.SNOMMOC_TOKEN) val token: SnommocToken,
-//): AccountAction("delete")
