8d4d8ac37719a1c0d18b7598e126483fbec54687
Michael Beaton <beatonma@gmail.com>
2020-11-20 19:23:11 +0000

    A small colored indicator.

---
+++ app/src/main/java/org/beatonma/commons/app/ui/compose/components/Dot.kt
@@ -0,0 +1,26 @@
+package org.beatonma.commons.app.ui.compose.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Dot(
+    color: Color,
+    size: Dp = 8.dp,
+    shape: Shape = CircleShape,
+    modifier: Modifier = Modifier,
+) {
+    Spacer(
+        modifier
+            .size(size)
+            .background(color, shape)
+    )
+}
