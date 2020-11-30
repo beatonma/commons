20b60519b9a666602063e00fbb2e46ab27a10b4b
Michael Beaton <beatonma@gmail.com>
2020-11-30 12:22:09 +0000

    Removed unused "Featured" content - replaced by Zeitgeist.

--- app/src/main/java/org/beatonma/commons/app/featured/FeaturedContentFragment.kt
+++
@@ -1,7 +0,0 @@
-package org.beatonma.commons.app.featured
-
-import androidx.fragment.app.Fragment
-
-class FeaturedContentFragment : Fragment() {
-
-}
--- repo/src/main/java/org/beatonma/commons/repo/repository/ZeitgeistRepository.kt
+++ repo/src/main/java/org/beatonma/commons/repo/repository/ZeitgeistRepository.kt
@@ -1,5 +1,6 @@
 package org.beatonma.commons.repo.repository
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
@@ -7,26 +8,16 @@ import kotlinx.coroutines.launch
 import org.beatonma.commons.data.core.room.dao.BillDao
 import org.beatonma.commons.data.core.room.dao.DivisionDao
 import org.beatonma.commons.data.core.room.dao.MemberDao
-import org.beatonma.commons.data.core.room.entities.bill.FeaturedBill
-import org.beatonma.commons.data.core.room.entities.bill.FeaturedBillWithBill
 import org.beatonma.commons.data.core.room.entities.bill.ZeitgeistBill
-import org.beatonma.commons.data.core.room.entities.division.FeaturedDivision
-import org.beatonma.commons.data.core.room.entities.division.FeaturedDivisionWithDivision
 import org.beatonma.commons.data.core.room.entities.division.ZeitgeistDivision
-import org.beatonma.commons.data.core.room.entities.member.FeaturedMemberProfile
 import org.beatonma.commons.data.core.room.entities.member.ZeitgeistMember
 import org.beatonma.commons.repo.CommonsApi
 import org.beatonma.commons.repo.FlowIoResult
-import org.beatonma.commons.repo.FlowIoResultList
-import org.beatonma.commons.repo.IoResultList
 import org.beatonma.commons.repo.converters.toBill
 import org.beatonma.commons.repo.converters.toDivision
 import org.beatonma.commons.repo.converters.toMemberProfile
 import org.beatonma.commons.repo.models.Zeitgeist
 import org.beatonma.commons.repo.result.cachedResultFlow
-import org.beatonma.commons.snommoc.models.ApiBill
-import org.beatonma.commons.snommoc.models.ApiDivision
-import org.beatonma.commons.snommoc.models.ApiMemberProfile
 import org.beatonma.commons.snommoc.models.ApiZeitgeist
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -44,40 +35,6 @@ class ZeitgeistRepository @Inject constructor(
         databaseQuery = ::loadZeitgeist,
         networkCall = remoteSource::getZeitgeist,
         saveCallResult = ::saveZeitgeist
-    )
-
-    @Deprecated("Use zeitgeist")
-    fun getFeaturedPeople(): FlowIoResultList<FeaturedMemberProfile> = cachedResultFlow(
-        databaseQuery = memberDao::getFeaturedProfiles,
-        networkCall = remoteSource::getFeaturedPeople,
-        saveCallResult = { profiles ->
-            memberDao.saveFeaturedPeople(profiles.map(ApiMemberProfile::toMemberProfile))
-        }
-    )
-
-    @Deprecated("Use zeitgeist")
-    fun getFeaturedBills(): FlowIoResultList<FeaturedBillWithBill> = cachedResultFlow(
-        databaseQuery = billDao::getFeaturedBills,
-        networkCall = remoteSource::getFeaturedBills,
-        saveCallResult = { bills ->
-            billDao.insertBills(bills.map(ApiBill::toBill))
-
-            billDao.insertFeaturedBills(
-                bills.map { FeaturedBill(it.parliamentdotuk) }
-            )
-        }
-    )
-
-    @Deprecated("Use zeitgeist")
-    fun getFeaturedDivisions(): Flow<IoResultList<FeaturedDivisionWithDivision>> = cachedResultFlow(
-        databaseQuery = divisionDao::getFeaturedDivisionsFlow,
-        networkCall = remoteSource::getFeaturedDivisions,
-        saveCallResult = { divisions ->
-            divisionDao.insertDivisions(divisions.map(ApiDivision::toDivision))
-            divisionDao.insertFeaturedDivisions(
-                divisions.map { FeaturedDivision(it.parliamentdotuk) }
-            )
-        }
     )
 
     suspend fun saveZeitgeist(zeitgeist: ApiZeitgeist) {
@@ -106,6 +63,7 @@ class ZeitgeistRepository @Inject constructor(
         }
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     fun loadZeitgeist(): Flow<Zeitgeist> = channelFlow {
         val zeitgeist = Zeitgeist()
 
