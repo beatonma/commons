8cf637b591e79a8e9ab5948419a95eec9823acc1
Michael Beaton <beatonma@gmail.com>
2020-12-03 20:37:16 +0000

    Updated imports for compose alpha07.

--- theme/src/main/java/org/beatonma/commons/theme/compose/theme/systemui/Insets.kt
+++ theme/src/main/java/org/beatonma/commons/theme/compose/theme/systemui/Insets.kt
@@ -34,13 +34,14 @@ import androidx.compose.runtime.onCommit
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticAmbientOf
-import androidx.compose.ui.LayoutModifier
-import androidx.compose.ui.Measurable
-import androidx.compose.ui.MeasureScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.layout.IntrinsicMeasurable
 import androidx.compose.ui.layout.IntrinsicMeasureScope
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
 import androidx.compose.ui.platform.DensityAmbient
 import androidx.compose.ui.platform.LayoutDirectionAmbient
 import androidx.compose.ui.platform.ViewAmbient
@@ -131,7 +132,7 @@ class Insets {
         internal set
 }
 
-val InsetsAmbient = staticAmbientOf<DisplayInsets>()
+val InsetsAmbient = staticAmbientOf { DisplayInsets() }
 
 /**
  * Applies any [WindowInsetsCompat] values to [InsetsAmbient], which are then available
@@ -566,8 +567,8 @@ private data class InsetsPaddingModifier(
 
     override fun MeasureScope.measure(
         measurable: Measurable,
-        constraints: Constraints
-    ): MeasureScope.MeasureResult {
+        constraints: Constraints,
+    ): MeasureResult {
         val left = if (applyLeft) (insets.left * scale).toInt() else 0
         val top = if (applyTop) (insets.top * scale).toInt() else 0
         val right = if (applyRight) (insets.right * scale).toInt() else 0
@@ -625,8 +626,8 @@ private data class InsetsSizeModifier(
 
     override fun MeasureScope.measure(
         measurable: Measurable,
-        constraints: Constraints
-    ): MeasureScope.MeasureResult {
+        constraints: Constraints,
+    ): MeasureResult {
         val wrappedConstraints = targetConstraints.let { targetConstraints ->
             val resolvedMinWidth = if (widthSide != null) {
                 targetConstraints.minWidth
