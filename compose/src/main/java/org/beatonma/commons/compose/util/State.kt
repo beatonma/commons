7bdc6255b66ac137f3b72e6626df6ebf3a231b87
Michael Beaton <beatonma@gmail.com>
2020-12-03 20:35:50 +0000

    Added rememberText() helper.

--- compose/src/main/java/org/beatonma/commons/compose/util/State.kt
+++ compose/src/main/java/org/beatonma/commons/compose/util/State.kt
@@ -1,5 +1,11 @@
 package org.beatonma.commons.compose.util
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 fun <T> MutableState<T>.update(newValue: T) = component2()(newValue)
+
+@Composable
+fun rememberText(default: String = ""): MutableState<String> = remember { mutableStateOf(default) }
