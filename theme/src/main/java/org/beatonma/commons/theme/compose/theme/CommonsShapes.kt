78964e55126358c520b07f17e6c9c9036c57c3ef
Michael Beaton <beatonma@gmail.com>
2020-12-04 12:03:07 +0000

    Extracted corner sizes to const vals for reuse.

--- theme/src/main/java/org/beatonma/commons/theme/compose/theme/CommonsShapes.kt
+++ theme/src/main/java/org/beatonma/commons/theme/compose/theme/CommonsShapes.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
+const val CORNER_SMALL = 8
+const val CORNER_MEDIUM = 16
+const val CORNER_LARGE = 24
+
 val CommonsShapes = Shapes(
-    large = RoundedCornerShape(24.dp),
-    medium = RoundedCornerShape(16.dp),
-    small = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(CORNER_LARGE.dp),
+    medium = RoundedCornerShape(CORNER_MEDIUM.dp),
+    small = RoundedCornerShape(CORNER_SMALL.dp),
 )
 
 fun CornerBasedShape.withSquareTop() = copy(topLeft = ZeroCornerSize, topRight = ZeroCornerSize)
