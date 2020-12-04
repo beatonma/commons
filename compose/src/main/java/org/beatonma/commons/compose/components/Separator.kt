a8029b9ac9a6cad26c4a57a2060cdca76601a284
Michael Beaton <beatonma@gmail.com>
2020-12-04 12:09:13 +0000

    Updated imports for compose alpha07

--- compose/src/main/java/org/beatonma/commons/compose/components/Separator.kt
+++ compose/src/main/java/org/beatonma/commons/compose/components/Separator.kt
@@ -1,6 +1,5 @@
 package org.beatonma.commons.compose.components
 
-import androidx.compose.foundation.AmbientContentColor
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredHeight
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.AmbientContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
