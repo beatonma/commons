d014556df9691502e9b28ae1d08c19c5dae1133d
Michael Beaton <beatonma@gmail.com>
2020-12-04 11:44:53 +0000

    Converted to simple ViewModel and removed UserRepository dependency.

--- app/src/main/java/org/beatonma/commons/app/featured/FeaturedContentViewModel.kt
+++ app/src/main/java/org/beatonma/commons/app/featured/FeaturedContentViewModel.kt
@@ -1,14 +1,10 @@
 package org.beatonma.commons.app.featured
 
-import android.content.Context
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import dagger.hilt.android.qualifiers.ApplicationContext
-import org.beatonma.commons.app
-import org.beatonma.commons.app.signin.BaseUserAccountViewModel
 import org.beatonma.commons.repo.repository.SnommocRepository
-import org.beatonma.commons.repo.repository.UserRepository
 import org.beatonma.commons.repo.repository.ZeitgeistRepository
 import org.beatonma.commons.repo.result.IoResult
 import org.beatonma.commons.snommoc.models.MessageOfTheDay
@@ -16,10 +12,7 @@ import org.beatonma.commons.snommoc.models.MessageOfTheDay
 class FeaturedContentViewModel @ViewModelInject constructor(
     snommocRepository: SnommocRepository,
     zeitgeistRepository: ZeitgeistRepository,
-    userRepository: UserRepository,
-    @ApplicationContext application: Context,
-) : BaseUserAccountViewModel(userRepository, application.app) {
-
+) : ViewModel() {
     val zeitgeist = zeitgeistRepository.getZeitgeist()
     val motd: LiveData<IoResult<List<MessageOfTheDay>>> = snommocRepository.getMotd().asLiveData()
 }
