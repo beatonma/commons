5bf77dae2c94b9212c0110ac5dd36e887efaf152
Michael Beaton <beatonma@gmail.com>
2020-12-04 11:38:23 +0000

    Refactored so FabBottomSheet and SwipeableFabBottomSheet share the same underlying layout code. Added BottomSheetContent and BottomSheetText helpers to standardise bottomsheet content animation.

--- app/src/main/java/org/beatonma/commons/app/ui/compose/components/FabBottomSheet.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/compose/components/FabBottomSheet.kt
@@ -5,7 +5,6 @@ import androidx.compose.animation.core.TransitionDefinition
 import androidx.compose.animation.core.TransitionState
 import androidx.compose.animation.core.transitionDefinition
 import androidx.compose.animation.transition
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
@@ -13,6 +12,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.material.SwipeableState
+import androidx.compose.material.Text
 import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
@@ -30,8 +30,10 @@ import org.beatonma.commons.compose.ambient.colors
 import org.beatonma.commons.compose.ambient.typography
 import org.beatonma.commons.compose.animation.lerpBetween
 import org.beatonma.commons.compose.animation.progressKey
+import org.beatonma.commons.compose.components.CardText
 import org.beatonma.commons.compose.components.ModalScrim
 import org.beatonma.commons.compose.modifiers.either
+import org.beatonma.commons.compose.modifiers.wrapContentSize
 import org.beatonma.commons.compose.util.update
 import org.beatonma.commons.core.extensions.lerpBetween
 import org.beatonma.commons.core.extensions.map
@@ -42,12 +44,16 @@ import org.beatonma.commons.theme.compose.Elevation
 import org.beatonma.commons.theme.compose.Padding
 import org.beatonma.commons.theme.compose.theme.CORNER_SMALL
 import org.beatonma.commons.theme.compose.theme.CommonsSpring
+import org.beatonma.commons.theme.compose.theme.systemui.imeOrNavigationBarsPadding
 import org.beatonma.commons.theme.compose.theme.systemui.navigationBarsPadding
 
 enum class FabBottomSheetState {
     Fab,
     BottomSheet,
 }
+
+@Composable
+fun rememberFabBottomSheetState() = remember { mutableStateOf(FabBottomSheetState.Fab) }
 
 /**
  * Displays a FAB which expands into a bottom sheet dialog for comment authoring.
@@ -60,44 +66,15 @@ fun FabBottomSheet(
     transitionState: TransitionState = transition(transition, toState = uiState.value),
     fabContent: @Composable (progress: Float) -> Unit,
     bottomSheetContent: @Composable (progress: Float) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    val progress = transitionState[progressKey]
-
-    val targetState = uiState.value
-
-    ModalScrim(
-        alignment = Alignment.BottomEnd,
-        alpha = progress,
-        onClickAction = {
-            uiState.update(FabBottomSheetState.Fab)
-        }
-    ) {
-        val surfaceShape = getFabBottomsheetSurfaceShape(progress)
-
-        Surface(
-            Modifier
-                .padding(progress.lerpBetween(Padding.Fab, Padding.Zero))
-                .navigationBarsPadding(scale = progress.reversed()) // The bottom sheet should extend behind navigation bar as it expands.
-                .either(
-                    condition = targetState == FabBottomSheetState.Fab,
-                    whenTrue = {
-                        clickable { uiState.update(FabBottomSheetState.BottomSheet) }
-                    },
-                    whenFalse = {
-                        clickable { }
-                    }
-                )
-                .drawShadow(Elevation.ModalSurface, surfaceShape),
-            shape = surfaceShape,
-            color = progress.progressIn(0F, 0.4F)
-                .lerpBetween(colors.primary, colors.surface),
-        ) {
-            Box {
-                fabContent(progress)
-                bottomSheetContent(progress)
-            }
-        }
-    }
+    FabBottomSheetLayout(
+        uiState,
+        transitionState[progressKey],
+        fabContent,
+        bottomSheetContent,
+        modifier,
+    )
 }
 
 /**
@@ -113,10 +90,11 @@ fun SwipeableFabBottomSheet(
     ),
     swipeAnchors: Map<Float, FabBottomSheetState> = mapOf(
         0F to FabBottomSheetState.Fab,
-        500F to FabBottomSheetState.BottomSheet
+        500F to FabBottomSheetState.BottomSheet,
     ),
     fabContent: @Composable (progress: Float) -> Unit,
     bottomSheetContent: @Composable (progress: Float) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     val offset = swipeState.offset.value
     val progress = (if (offset.isNaN()) 0F else offset).map(0F, 500F, 0F, 1F)
@@ -127,13 +105,79 @@ fun SwipeableFabBottomSheet(
             uiState.update(finalState)
         })
     }
-    val swipeable = Modifier.swipeable(
-        state = swipeState,
-//            enabled = swipeState.value == FabBottomSheetState.BottomSheet,
-        anchors = swipeAnchors,
-        orientation = Orientation.Vertical,
-        reverseDirection = true,
+
+    FabBottomSheetLayout(
+        uiState,
+        progress,
+        fabContent,
+        bottomSheetContent,
+        modifier.swipeable(
+            state = swipeState,
+            anchors = swipeAnchors,
+            orientation = Orientation.Vertical,
+            reverseDirection = true,
+        )
     )
+}
+
+@Composable
+fun FabText(
+    text: String,
+    progress: Float,
+) {
+    Text(
+        text,
+        Modifier
+            .padding(Padding.ExtendedFabContent)
+            .drawOpacity(progress.reversed().progressIn(0.8F, 1.0F)),
+        color = colors.onSecondary,
+        style = typography.button,
+    )
+}
+
+@Composable
+fun BottomSheetContent(
+    progress: Float,
+    handleImeInsets: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    Box(
+        Modifier
+            .either(
+                condition = handleImeInsets,
+                whenTrue = { imeOrNavigationBarsPadding(scale = progress) },
+                whenFalse = { navigationBarsPadding(scale = progress) },
+            )
+            .wrapContentSize(
+                horizontalProgress = progress.progressIn(0F, 0.4F),
+                verticalProgress = progress.progressIn(0F, 0.8F)
+            )
+            .drawOpacity(progress.progressIn(0.8F, 1F)),
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun BottomSheetText(
+    progress: Float,
+    handleImeInsets: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    BottomSheetContent(progress, handleImeInsets) {
+        CardText(content = content)
+    }
+}
+
+@Composable
+private fun FabBottomSheetLayout(
+    uiState: MutableState<FabBottomSheetState>,
+    progress: Float,
+    fabContent: @Composable (progress: Float) -> Unit,
+    bottomSheetContent: @Composable (progress: Float) -> Unit,
+    modifier: Modifier,
+) {
+    val targetState = uiState.value
 
     ModalScrim(
         alignment = Alignment.BottomEnd,
@@ -145,59 +189,29 @@ fun SwipeableFabBottomSheet(
         val surfaceShape = getFabBottomsheetSurfaceShape(progress)
 
         Surface(
-            Modifier
+            modifier
                 .padding(progress.lerpBetween(Padding.Fab, Padding.Zero))
                 .navigationBarsPadding(scale = progress.reversed()) // The bottom sheet should extend behind navigation bar as it expands.
-                .then(swipeable)
-                .either(
-                    condition = targetState == FabBottomSheetState.Fab,
-                    whenTrue = {
-                        clickable { uiState.update(FabBottomSheetState.BottomSheet) }
-                    },
-                    whenFalse = {
-                        clickable { }
+                .clickable {
+                    if (targetState == FabBottomSheetState.Fab) {
+                        uiState.update(FabBottomSheetState.BottomSheet)
                     }
-                )
+                }
                 .drawShadow(Elevation.ModalSurface, surfaceShape),
             shape = surfaceShape,
             color = progress.progressIn(0F, 0.4F).lerpBetween(colors.primary, colors.surface),
         ) {
             Box {
-                fabContent(progress)
-                bottomSheetContent(progress)
+                if (progress != 1F) {
+                    fabContent(progress)
+                }
+                if (progress != 0F) {
+                    bottomSheetContent(progress)
+                }
             }
         }
     }
 }
-
-@Composable
-fun FabText(
-    text: String,
-    progress: Float,
-) {
-    if (progress == 1F) {
-        return
-    }
-
-    Text(
-        text,
-        Modifier
-            .padding(Padding.ExtendedFabContent)
-            .drawOpacity(progress.reversed().progressIn(0.8F, 1.0F)),
-        color = colors.onSecondary,
-        style = typography.button,
-    )
-}
-
-private fun getFabBottomsheetSurfaceShape(progress: Float): Shape {
-    val eased = progress.withEasing(LinearOutSlowInEasing)
-    val upperCornerSize = eased.lerpBetween(24, CORNER_SMALL).dp
-    val lowerCornerSize = eased.lerpBetween(24, 0).dp
-    return RoundedCornerShape(upperCornerSize, upperCornerSize, lowerCornerSize, lowerCornerSize)
-}
-
-@Composable
-fun rememberFabBottomSheetState() = remember { mutableStateOf(FabBottomSheetState.Fab) }
 
 @Composable
 private fun rememberFabBottomSheetTransition() = remember {
@@ -216,4 +230,11 @@ private fun rememberFabBottomSheetTransition() = remember {
             progressKey using CommonsSpring()
         }
     }
+}
+
+private fun getFabBottomsheetSurfaceShape(progress: Float): Shape {
+    val eased = progress.withEasing(LinearOutSlowInEasing)
+    val upperCornerSize = eased.lerpBetween(24, CORNER_SMALL).dp
+    val lowerCornerSize = eased.lerpBetween(24, 0).dp
+    return RoundedCornerShape(upperCornerSize, upperCornerSize, lowerCornerSize, lowerCornerSize)
 }
