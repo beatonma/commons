7f9d02104067f2c38d7937d30241ba3873a591bf
Michael Beaton <beatonma@gmail.com>
2020-12-04 11:49:18 +0000

    Added AmbientSocialUiState, AmbientSocialUiTransition, AmbientSocialCommentValidator

--- app/src/main/java/org/beatonma/commons/app/social/compose/SocialAmbients.kt
+++ app/src/main/java/org/beatonma/commons/app/social/compose/SocialAmbients.kt
@@ -1,25 +1,43 @@
 package org.beatonma.commons.app.social.compose
 
+import androidx.compose.animation.core.TransitionState
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.ProvidableAmbient
 import androidx.compose.runtime.ambientOf
+import androidx.compose.runtime.staticAmbientOf
 import androidx.compose.ui.graphics.Color
 import org.beatonma.commons.ActionBlock
+import org.beatonma.commons.BuildConfig
+import org.beatonma.commons.app.social.SocialUiState
 import org.beatonma.commons.app.ui.colors.ComposePartyColors
+import org.beatonma.commons.compose.components.TextValidationRules
 import org.beatonma.commons.snommoc.models.social.EmptySocialContent
 import org.beatonma.commons.snommoc.models.social.SocialComment
+import org.beatonma.commons.snommoc.models.social.SocialContent
 
-val AmbientSocialContent = ambientOf { EmptySocialContent }
+val AmbientSocialContent: ProvidableAmbient<SocialContent> = ambientOf { EmptySocialContent }
 val AmbientSocialTheme: ProvidableAmbient<SocialTheme> =
     ambientOf { error("Social theme has not been provided") }
-val AmbientSocialActions = ambientOf { SocialActions() }
+val AmbientSocialActions: ProvidableAmbient<SocialActions> = ambientOf { SocialActions() }
+val AmbientSocialUiState: ProvidableAmbient<MutableState<SocialUiState>> =
+    ambientOf { error("SocialUiState has not been set") }
+val AmbientSocialUiTransition: ProvidableAmbient<TransitionState> =
+    ambientOf { error("TransitionState has not been set") }
+val AmbientSocialCommentValidator: ProvidableAmbient<TextValidationRules> = staticAmbientOf {
+    TextValidationRules(
+        minLength = BuildConfig.SOCIAL_COMMENT_MIN_LENGTH,
+        maxLength = BuildConfig.SOCIAL_COMMENT_MAX_LENGTH,
+    )
+}
 
 class SocialActions(
     val onVoteUpClick: ActionBlock = {},
     val onVoteDownClick: ActionBlock = {},
     val onExpandedCommentIconClick: ActionBlock = {},
     val onCommentClick: (SocialComment) -> Unit = {},
+    val onCommentSubmitClick: (String) -> Unit = {},
 )
 
 class SocialTheme(
