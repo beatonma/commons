99f7a14ee1a582a71c02afcb3ce27f0d52a9da68
Michael Beaton <beatonma@gmail.com>
2020-12-04 12:01:29 +0000

    Added hyperlink style

--- theme/src/main/java/org/beatonma/commons/theme/compose/theme/CommonsTypography.kt
+++ theme/src/main/java/org/beatonma/commons/theme/compose/theme/CommonsTypography.kt
@@ -1,12 +1,14 @@
 package org.beatonma.commons.theme.compose.theme
 
-import androidx.compose.foundation.AmbientContentColor
+import androidx.compose.material.AmbientContentColor
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.sp
 
 private val default = Typography()
@@ -21,6 +23,12 @@ object CommonsSpanStyle {
     val italic: SpanStyle = SpanStyle(fontStyle = FontStyle.Italic)
     val bold: SpanStyle = SpanStyle(fontWeight = FontWeight.Bold)
     val quote: SpanStyle = SpanStyle(fontFamily = FontFamily.Monospace)
+
+    @Composable
+    val hyperlink: SpanStyle
+        get() =
+            SpanStyle(color = MaterialTheme.colors.primary,
+                textDecoration = TextDecoration.Underline)
 
     @Composable
     fun quote(): SpanStyle = SpanStyle(
