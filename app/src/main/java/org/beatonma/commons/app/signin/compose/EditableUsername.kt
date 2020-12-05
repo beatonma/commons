03dd3b1a92dc60b92621a4ded44c32312184de79
Michael Beaton <beatonma@gmail.com>
2020-12-05 18:28:41 +0000

    Implemented simple UI state for when we are waiting for a server response. Added simple alpha animation when changing states.

--- app/src/main/java/org/beatonma/commons/app/signin/compose/EditableUsername.kt
+++ app/src/main/java/org/beatonma/commons/app/signin/compose/EditableUsername.kt
@@ -1,13 +1,19 @@
 package org.beatonma.commons.app.signin.compose
 
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.FloatPropKey
+import androidx.compose.animation.core.TransitionDefinition
+import androidx.compose.animation.core.TransitionState
+import androidx.compose.animation.core.transitionDefinition
+import androidx.compose.animation.transition
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.AmbientContentColor
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
@@ -18,51 +24,65 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import org.beatonma.commons.BuildConfig
 import org.beatonma.commons.R
 import org.beatonma.commons.app.social.compose.Username
+import org.beatonma.commons.app.ui.compose.components.LoadingIcon
 import org.beatonma.commons.compose.ambient.typography
+import org.beatonma.commons.compose.animation.progressKey
 import org.beatonma.commons.compose.components.TextValidationResult
 import org.beatonma.commons.compose.components.TextValidationRules
 import org.beatonma.commons.compose.components.ValidatedTextField
 import org.beatonma.commons.compose.util.rememberText
 import org.beatonma.commons.compose.util.update
 import org.beatonma.commons.data.core.room.entities.user.UserToken
+import org.beatonma.commons.theme.compose.theme.CommonsSpring
 
-internal enum class State {
+internal enum class EditableState {
     ReadOnly,
     Editable,
     AwaitingResult,
     ;
 }
 
+private val readOnlyAlpha = FloatPropKey()
+private val editableAlpha = FloatPropKey()
+private val awaitingResultAlpha = FloatPropKey()
+
 @Composable
 internal fun EditableUsername(
     userToken: UserToken,
-    state: MutableState<State> = remember { mutableStateOf(State.ReadOnly) },
+    state: MutableState<EditableState> = remember { mutableStateOf(EditableState.ReadOnly) },
+    transitionDef: TransitionDefinition<EditableState> = rememberEditableStateTransition(),
+    transition: TransitionState = transition(transitionDef, state.value),
 ) {
     when (state.value) {
-        State.ReadOnly -> ReadOnlyUsernameLayout(userToken = userToken, state = state)
-        State.Editable -> EditableUsernameLayout(userToken = userToken, state = state)
-        State.AwaitingResult -> TODO()
+        EditableState.ReadOnly -> ReadOnlyUsernameLayout(userToken,
+            state,
+            Modifier.alpha(transition[readOnlyAlpha]))
+        EditableState.Editable -> EditableUsernameLayout(userToken,
+            state,
+            Modifier.alpha(transition[editableAlpha]))
+        EditableState.AwaitingResult -> AwaitingResultLayout(userToken,
+            Modifier.alpha(transition[awaitingResultAlpha]))
     }
-
-    Text(state.value.name, style = typography.h6)
 }
 
 @Composable
 private fun ReadOnlyUsernameLayout(
     userToken: UserToken,
-    state: MutableState<State>,
+    state: MutableState<EditableState>,
+    modifier: Modifier = Modifier,
 ) {
-    val makeEditable = { state.update(State.Editable) }
+    val makeEditable = { state.update(EditableState.Editable) }
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.clickable(onClick = makeEditable),
+        modifier = modifier.clickable(onClick = makeEditable),
     ) {
         Username(userToken.username, style = typography.h4)
 
@@ -73,9 +93,27 @@ private fun ReadOnlyUsernameLayout(
 }
 
 @Composable
+private fun AwaitingResultLayout(
+    userToken: UserToken,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Username(userToken.username, style = typography.h4,
+            modifier = Modifier.alpha(ContentAlpha.disabled)
+        )
+
+        LoadingIcon()
+    }
+}
+
+@Composable
 private fun EditableUsernameLayout(
     userToken: UserToken,
-    state: MutableState<State>,
+    state: MutableState<EditableState>,
+    modifier: Modifier = Modifier,
     validationMessages: ValidationMessages = rememberValidationMessages(),
     actions: UserProfileActions = AmbientUserProfileActions.current,
 ) {
@@ -88,7 +126,11 @@ private fun EditableUsernameLayout(
     val validationRules = rememberUsernameValidator()
     val validationResultMessage = rememberText()
 
-    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End,
+    ) {
         ValidatedTextField(
             text, validationRules,
             onValueChange = { _, validationResult ->
@@ -101,11 +143,16 @@ private fun EditableUsernameLayout(
             textStyle = TextStyle(color = AmbientContentColor.current),
         )
 
-        IconButton(onClick = { actions.signInActions.renameAccount(text.value) }) {
+        IconButton(
+            onClick = {
+                state.update(EditableState.AwaitingResult)
+                actions.signInActions.renameAccount(text.value)
+            }
+        ) {
             Icon(Icons.Default.Done)
         }
 
-        IconButton(onClick = { state.update(State.ReadOnly) }) {
+        IconButton(onClick = { state.update(EditableState.ReadOnly) }) {
             Icon(Icons.Default.Undo)
         }
     }
@@ -161,3 +208,39 @@ private class ValidationMessages(
         TextValidationResult.OK -> ok
     }
 }
+
+@Composable
+private fun rememberEditableStateTransition(
+    animSpec: AnimationSpec<Float> = CommonsSpring(),
+): TransitionDefinition<EditableState> =
+    remember {
+        transitionDefinition {
+            state(EditableState.ReadOnly) {
+                this[readOnlyAlpha] = 1F
+                this[editableAlpha] = 0F
+                this[awaitingResultAlpha] = 0F
+            }
+            state(EditableState.Editable) {
+                this[readOnlyAlpha] = 0F
+                this[editableAlpha] = 1F
+                this[awaitingResultAlpha] = 0F
+            }
+            state(EditableState.AwaitingResult) {
+                this[readOnlyAlpha] = 0F
+                this[editableAlpha] = 0F
+                this[awaitingResultAlpha] = 1F
+            }
+
+            transition(
+                EditableState.ReadOnly to EditableState.Editable,
+
+                EditableState.Editable to EditableState.ReadOnly,
+                EditableState.Editable to EditableState.AwaitingResult,
+
+                EditableState.AwaitingResult to EditableState.Editable,
+                EditableState.AwaitingResult to EditableState.ReadOnly,
+            ) {
+                progressKey using animSpec
+            }
+        }
+    }
