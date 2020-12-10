19252f8fc24102cadc27e23e1c96b41bcd3880a1
Michael Beaton <beatonma@gmail.com>
2020-12-10 20:53:43 +0000

    Renamed SignInActions -> UserAccountActions Updated UserAccountActions.renameAccount signature.

--- app/src/main/java/org/beatonma/commons/app/signin/compose/AmbientUser.kt
+++ app/src/main/java/org/beatonma/commons/app/signin/compose/AmbientUser.kt
@@ -3,6 +3,7 @@ package org.beatonma.commons.app.signin.compose
 import androidx.compose.runtime.ProvidableAmbient
 import androidx.compose.runtime.ambientOf
 import org.beatonma.commons.ActionBlock
+import org.beatonma.commons.app.signin.RenameResult
 import org.beatonma.commons.data.core.room.entities.user.UserToken
 
 val AmbientUserToken: ProvidableAmbient<UserToken> = ambientOf { NullUserToken }
@@ -13,9 +14,11 @@ val NullUserToken = UserToken(
     snommocToken = "null", googleId = "null", username = "NoUser"
 )
 
-class SignInActions(
+class UserAccountActions(
     val signIn: ActionBlock = {},
     val signOut: ActionBlock = {},
-    val renameAccount: (newName: String) -> Unit = {},
-    val deleteAccount: ActionBlock = {},
+    val renameAccount: suspend (userToken: UserToken, newName: String) -> RenameResult =
+        { _, _ -> TODO("renameAccount is not implemented") },
+    val deleteAccount: suspend (userToken: UserToken) -> Unit =
+        { TODO("deleteAccount is not implemented") },
 )
