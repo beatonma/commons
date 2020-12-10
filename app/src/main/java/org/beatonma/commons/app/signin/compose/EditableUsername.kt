d15d3e9beb76af7c574cc1fcdef37416035066bc
Michael Beaton <beatonma@gmail.com>
2020-12-10 20:52:48 +0000

    Added RenameResult handling to ValidationMessages so server response can be handled with appropriate error messages if needed. UI now updates to suitable state depending on success of rename request.

--- app/src/main/java/org/beatonma/commons/app/signin/compose/EditableUsername.kt
+++ app/src/main/java/org/beatonma/commons/app/signin/compose/EditableUsername.kt
@@ -20,23 +20,33 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Undo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.ProvidableAmbient
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.ExperimentalFocus
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.beatonma.commons.BuildConfig
 import org.beatonma.commons.R
+import org.beatonma.commons.app.signin.RenameResult
 import org.beatonma.commons.app.social.compose.Username
 import org.beatonma.commons.app.ui.compose.components.LoadingIcon
 import org.beatonma.commons.compose.ambient.typography
 import org.beatonma.commons.compose.animation.progressKey
+import org.beatonma.commons.compose.components.AmbientFeedbackMessage
 import org.beatonma.commons.compose.components.Hint
 import org.beatonma.commons.compose.components.TextValidationResult
 import org.beatonma.commons.compose.components.TextValidationRules
@@ -53,9 +63,9 @@ internal enum class EditableState {
     ;
 }
 
-private val readOnlyAlpha = FloatPropKey()
-private val editableAlpha = FloatPropKey()
-private val awaitingResultAlpha = FloatPropKey()
+internal val readOnlyVisibility = FloatPropKey()
+internal val editableVisibility = FloatPropKey()
+internal val awaitingResultVisibility = FloatPropKey()
 
 @OptIn(ExperimentalFocus::class)
 @Composable
@@ -66,18 +76,22 @@ internal fun EditableUsername(
     transition: TransitionState = transition(transitionDef, state.value),
     focusRequester: FocusRequester = remember(::FocusRequester),
 ) {
+    val coroutineScope = rememberCoroutineScope()
     when (state.value) {
         EditableState.ReadOnly -> ReadOnlyUsernameLayout(userToken,
             state,
-            Modifier.alpha(transition[readOnlyAlpha]))
+            Modifier.alpha(transition[readOnlyVisibility]))
 
-        EditableState.Editable -> EditableUsernameLayout(userToken,
+        EditableState.Editable -> EditableUsernameLayout(
+            userToken,
             state,
-            Modifier.alpha(transition[editableAlpha]),
-            focusRequester = focusRequester)
+            coroutineScope = coroutineScope,
+            modifier = Modifier.alpha(transition[editableVisibility]),
+            focusRequester = focusRequester,
+        )
 
         EditableState.AwaitingResult -> AwaitingResultLayout(userToken,
-            Modifier.alpha(transition[awaitingResultAlpha]))
+            Modifier.alpha(transition[awaitingResultVisibility]))
     }
 }
 
@@ -118,25 +132,71 @@ private fun AwaitingResultLayout(
     }
 }
 
-@OptIn(ExperimentalFocus::class)
+@OptIn(ExperimentalFocus::class, InternalCoroutinesApi::class)
 @Composable
 private fun EditableUsernameLayout(
     userToken: UserToken,
     state: MutableState<EditableState>,
+    coroutineScope: CoroutineScope,
     modifier: Modifier = Modifier,
     validationMessages: ValidationMessages = rememberValidationMessages(),
-    actions: UserProfileActions = AmbientUserProfileActions.current,
+    submitAction: suspend (UserToken, String) -> RenameResult = AmbientUserProfileActions.current.userAccountActions.renameAccount,
     focusRequester: FocusRequester = remember(::FocusRequester),
 ) {
     val text = rememberText(userToken.username)
-    val keyboardOptions = remember {
-        KeyboardOptions(
-            keyboardType = KeyboardType.Text,
-        )
-    }
+    val keyboardOptions = remember { KeyboardOptions(keyboardType = KeyboardType.Text) }
     val validationRules = rememberUsernameValidator()
-    val validationResultMessage = rememberText()
+    val validationResultMessage = AmbientFeedbackMessage.current
 
+    fun renameAction() {
+        state.update(EditableState.AwaitingResult)
+
+        coroutineScope.launch {
+            val result = submitAction(userToken, text.value)
+            val pendingState = when (result) {
+                RenameResult.ACCEPTED,
+                RenameResult.NO_CHANGE,
+                -> EditableState.ReadOnly
+
+                else -> EditableState.Editable
+            }
+
+            val message = validationMessages.getMessage(result)
+
+            withContext(Dispatchers.Main) {
+                state.update(pendingState)
+                validationResultMessage.update(message)
+            }
+        }
+    }
+
+    EditableUsernameLayout(
+        text,
+        validationRules,
+        validationResultMessage,
+        validationMessages,
+        modifier,
+        focusRequester,
+        keyboardOptions,
+        AmbientContentColor,
+        state,
+        ::renameAction)
+}
+
+@OptIn(ExperimentalFocus::class)
+@Composable
+private fun EditableUsernameLayout(
+    text: MutableState<String>,
+    validationRules: TextValidationRules,
+    validationResultMessage: MutableState<String>,
+    validationMessages: ValidationMessages,
+    modifier: Modifier,
+    focusRequester: FocusRequester,
+    keyboardOptions: KeyboardOptions,
+    AmbientContentColor: ProvidableAmbient<Color>,
+    state: MutableState<EditableState>,
+    onSubmitName: () -> Unit,
+) {
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
@@ -145,7 +205,9 @@ private fun EditableUsernameLayout(
         ValidatedTextField(
             text, validationRules,
             onValueChange = { _, validationResult ->
-                validationResultMessage.update(validationMessages.getMessage(validationResult))
+                validationResultMessage.update(
+                    validationMessages.getMessage(validationResult)
+                )
             },
             validationResultMessage = validationResultMessage,
             placeholder = {
@@ -161,12 +223,7 @@ private fun EditableUsernameLayout(
             textStyle = TextStyle(color = AmbientContentColor.current),
         )
 
-        IconButton(
-            onClick = {
-                state.update(EditableState.AwaitingResult)
-                actions.signInActions.renameAccount(text.value)
-            }
-        ) {
+        IconButton(onClick = onSubmitName) {
             Icon(Icons.Default.Done)
         }
 
@@ -206,6 +263,8 @@ private fun rememberValidationMessages(): ValidationMessages {
         BuildConfig.ACCOUNT_USERNAME_MIN_LENGTH
     )
     val formatError = stringResource(R.string.account_username_validation_format_error)
+    val error = stringResource(R.string.account_username_validation_try_again)
+    val deniedByServer = stringResource(R.string.account_username_validation_denied_by_server)
 
     return remember {
         ValidationMessages(
@@ -213,15 +272,28 @@ private fun rememberValidationMessages(): ValidationMessages {
             tooShort = tooShort,
             formatError = formatError,
             ok = "",
+            error = error,
+            deniedByServer = deniedByServer,
         )
     }
 }
 
+/**
+ * @param formatError       The username contains invalid characters or has valid characters
+ *                          in illegal positions
+ *
+ * @param deniedByServer    The server processed the request but refused to change the username
+ *                          because it is reserved, blocked, or already taken.
+ *
+ * @param error             Server error or network error - trying again may work.
+ */
 private class ValidationMessages(
     val tooLong: String,
     val tooShort: String,
     val formatError: String,
     val ok: String,
+    val error: String,
+    val deniedByServer: String,
 ) {
     fun getMessage(result: TextValidationResult) = when (result) {
         TextValidationResult.TOO_LONG -> tooLong
@@ -229,28 +301,44 @@ private class ValidationMessages(
         TextValidationResult.FORMAT_ERROR -> formatError
         TextValidationResult.OK -> ok
     }
+
+    fun getMessage(result: RenameResult) = when (result) {
+        RenameResult.ACCEPTED, RenameResult.NO_CHANGE -> ok
+        RenameResult.TOO_LONG -> tooLong
+        RenameResult.TOO_SHORT -> tooShort
+        RenameResult.BAD_START_OR_END -> formatError
+
+        RenameResult.SERVER_DENIED -> deniedByServer
+
+        RenameResult.SERVER_ERROR,
+        RenameResult.SERVER_BAD_REQUEST,
+        RenameResult.ERROR,
+        -> error
+
+        else -> error
+    }
 }
 
 @Composable
-private fun rememberEditableStateTransition(
+internal fun rememberEditableStateTransition(
     animSpec: AnimationSpec<Float> = CommonsSpring(),
 ): TransitionDefinition<EditableState> =
     remember {
         transitionDefinition {
             state(EditableState.ReadOnly) {
-                this[readOnlyAlpha] = 1F
-                this[editableAlpha] = 0F
-                this[awaitingResultAlpha] = 0F
+                this[readOnlyVisibility] = 1F
+                this[editableVisibility] = 0F
+                this[awaitingResultVisibility] = 0F
             }
             state(EditableState.Editable) {
-                this[readOnlyAlpha] = 0F
-                this[editableAlpha] = 1F
-                this[awaitingResultAlpha] = 0F
+                this[readOnlyVisibility] = 0F
+                this[editableVisibility] = 1F
+                this[awaitingResultVisibility] = 0F
             }
             state(EditableState.AwaitingResult) {
-                this[readOnlyAlpha] = 0F
-                this[editableAlpha] = 0F
-                this[awaitingResultAlpha] = 1F
+                this[readOnlyVisibility] = 0F
+                this[editableVisibility] = 0F
+                this[awaitingResultVisibility] = 1F
             }
 
             transition(
