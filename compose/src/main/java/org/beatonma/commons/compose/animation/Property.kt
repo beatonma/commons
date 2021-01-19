4e779acc879c5e1539a08d96789cd050ebca99e7
Michael Beaton <beatonma@gmail.com>
2021-01-19 17:09:53 +0000

    Added FloatPropKey label

--- compose/src/main/java/org/beatonma/commons/compose/animation/Property.kt
+++ compose/src/main/java/org/beatonma/commons/compose/animation/Property.kt
@@ -1,7 +1,5 @@
 package org.beatonma.commons.compose.animation
 
 import androidx.compose.animation.core.FloatPropKey
-import androidx.compose.animation.core.TransitionState
 
-val progressKey = FloatPropKey()
-val TransitionState.progress get() = this[progressKey]
+val progressKey = FloatPropKey("Default key for simple 0F..1F animation progress")
