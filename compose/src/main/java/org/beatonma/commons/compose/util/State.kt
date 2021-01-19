a7896d9bb769eb59831f8ea49f98cf9b19472686
Michael Beaton <beatonma@gmail.com>
2021-01-19 16:19:32 +0000

    Added MutableState<T>.mapUpdate() helper for updating value based on previous value. Added MutableState<Boolean>.toggle() helper. Added rememberListOf(), rememberSetOf() helpers.

--- compose/src/main/java/org/beatonma/commons/compose/util/State.kt
+++ compose/src/main/java/org/beatonma/commons/compose/util/State.kt
@@ -5,10 +5,26 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
-fun <T> MutableState<T>.update(newValue: T) = component2()(newValue)
+/**
+ * Set a new value.
+ */
+fun <T> MutableState<T>.update(newValue: T) { value = newValue }
+
+/**
+ * Update the current value using the previous value.
+ */
+inline fun <T> MutableState<T>.mapUpdate(map: (T) -> T) { value = map(value) }
+
+fun MutableState<Boolean>.toggle() = update(!value)
 
 @Composable
 fun rememberText(default: String = ""): MutableState<String> = remember { mutableStateOf(default) }
 
 @Composable
 fun rememberBoolean(default: Boolean = false) = remember { mutableStateOf(default) }
+
+@Composable
+fun <T> rememberListOf(default: List<T> = listOf()) = remember { mutableStateOf(default) }
+
+@Composable
+fun <T> rememberSetOf(default: Set<T> = setOf()) = remember { mutableStateOf(default) }
