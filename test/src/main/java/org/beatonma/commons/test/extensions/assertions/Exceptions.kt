88cbd31b9014820c21cb1dd94dc1d013d1107e57
Michael Beaton <beatonma@gmail.com>
2021-02-04 11:40:18 +0000

    Renamed file, added assertThrows.

--- test/src/main/java/org/beatonma/commons/test/extensions/assertions/Exceptions.kt
+++ test/src/main/java/org/beatonma/commons/test/extensions/assertions/Exceptions.kt
@@ -1,5 +1,7 @@
 package org.beatonma.commons.test.extensions.assertions
 
+import kotlin.reflect.KClass
+
 
 /**
  * Handy for testing tests!
@@ -18,5 +20,27 @@ inline fun assertThrowsAssertionError(
     }
     if (!throwsError) {
         throw AssertionError("Expected an AssertionError but none was thrown: $message")
+    }
+}
+
+
+/**
+ * Ensure that the block throws an exception of the expected type.
+ */
+inline fun <E: Throwable> assertThrows(
+    errorClass: KClass<E>,
+    block: () -> Unit
+) {
+    var throwsError = false
+    try {
+        block.invoke()
+    } catch (e: Throwable) {
+        e.assertInstanceOf(errorClass)
+
+        throwsError = true
+        println("Expected AssertionError [${e.message ?: "no message"}] was thrown correctly")
+    }
+    if (!throwsError) {
+        throw AssertionError("Expected a Throwable[$errorClass] but none was thrown!")
     }
 }
