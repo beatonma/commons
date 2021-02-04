c5d72ce569f2d3ee6cfef7ecd26a33b805a1d5df
Michael Beaton <beatonma@gmail.com>
2021-02-04 14:37:41 +0000

    Updated import paths. Added result.onErrorCode handler to WithResultData Added WithResponseCode implementation.

--- app/src/main/java/org/beatonma/commons/app/ui/compose/Result.kt
+++ app/src/main/java/org/beatonma/commons/app/ui/compose/Result.kt
@@ -1,23 +1,69 @@
 package org.beatonma.commons.app.ui.compose
 
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import org.beatonma.commons.app.ui.compose.components.Loading
-import org.beatonma.commons.app.ui.compose.components.Warning
+import androidx.compose.ui.Modifier
+import org.beatonma.commons.app.ui.compose.components.ErrorUi
+import org.beatonma.commons.app.ui.compose.components.LoadingIcon
 import org.beatonma.commons.repo.result.IoResult
+import org.beatonma.commons.repo.result.ResponseCode
 import org.beatonma.commons.repo.result.onError
+import org.beatonma.commons.repo.result.onErrorCode
 import org.beatonma.commons.repo.result.onLoading
 import org.beatonma.commons.repo.result.onSuccess
+import org.beatonma.commons.repo.result.onSuccessCode
 
+private const val TAG = "Compose.Result"
 
 @Composable
 fun <T> WithResultData(
     result: IoResult<T>,
-    onLoading: @Composable () -> Unit = { Loading() },
-    onError: @Composable (error: Throwable) -> Unit = { Warning(error = it) },
+    onLoading: @Composable () -> Unit = { LoadingIcon(Modifier.fillMaxWidth()) },
+    onError: @Composable (error: Throwable?, code: ResponseCode?) -> Unit =
+        { error, code ->
+            when {
+                error != null -> ErrorUi(error = error)
+                code != null -> ErrorUi(error = code)
+            }
+        },
     onSuccess: @Composable (data: T) -> Unit,
 ) {
     result
-        .onError { onError(it) }
+        .onError { error ->
+            Log.w(TAG, error)
+            onError(error, ResponseCode(418))
+        }
+        .onErrorCode { code ->
+            Log.w(TAG, "$code")
+            onError(null, code)
+        }
         .onLoading { onLoading() }
         .onSuccess { onSuccess(it) }
+}
+
+@Composable
+fun <T> WithResponseCode(
+    result: IoResult<T>,
+    onLoading: @Composable () -> Unit = { LoadingIcon(Modifier.fillMaxWidth()) },
+    onError: @Composable (error: Throwable?, code: ResponseCode?) -> Unit =
+        { error, code ->
+            when {
+                error != null -> ErrorUi(error = error)
+                code != null -> ErrorUi(error = code)
+            }
+        },
+    onSuccess: @Composable (ResponseCode) -> Unit,
+) {
+    result
+        .onError { error ->
+            Log.w(TAG, error)
+            onError(error, ResponseCode(418))
+        }
+        .onErrorCode { code ->
+            Log.w(TAG, "$code")
+            onError(null, code)
+        }
+        .onLoading { onLoading() }
+        .onSuccessCode { onSuccess(it) }
 }
