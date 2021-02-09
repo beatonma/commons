8121d076b03664f276ea7abb6a4cf3b6a8dd00a9
Michael Beaton <beatonma@gmail.com>
2021-02-09 18:33:38 +0000

    Added additional layers Zero and Max at the absolute min and max possible values for ensuring a composable is guaranteed to be at the front or back layer.

--- theme/src/main/java/org/beatonma/commons/theme/compose/Layer.kt
+++ theme/src/main/java/org/beatonma/commons/theme/compose/Layer.kt
@@ -6,8 +6,9 @@ private const val LAYER_STEP = 16F
  * Standard values for zIndex modifier to ensure consistency when rendering views that may overlap.
  */
 object Layer {
-    const val Bottom = 0F
-    const val Low = LAYER_STEP
+    const val Zero = 0F
+    const val Bottom = Zero + LAYER_STEP
+    const val Low = Bottom + LAYER_STEP
     const val Middle = Low + LAYER_STEP
     const val High = Middle + LAYER_STEP
     const val Top = High + LAYER_STEP
@@ -19,4 +20,6 @@ object Layer {
 
     const val AlwaysOnTopSurface = ModalContent + LAYER_STEP
     const val AlwaysOnTopContent = AlwaysOnTopSurface + LAYER_STEP
+
+    const val Max = Float.MAX_VALUE
 }
