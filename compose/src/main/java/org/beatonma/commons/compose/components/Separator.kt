eca7028d9fdb67b202ab1bd6d455ae444f917500
Michael Beaton <beatonma@gmail.com>
2021-02-09 15:51:56 +0000

    HorizontalSeparator now accepts a content block which will be displayed as part of the separator --- { like this } ---.

--- compose/src/main/java/org/beatonma/commons/compose/components/Separator.kt
+++ compose/src/main/java/org/beatonma/commons/compose/components/Separator.kt
@@ -9,12 +9,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.AmbientContentColor
+import androidx.compose.material.AmbientTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.AmbientLayoutDirection
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import org.beatonma.commons.compose.ambient.typography
+import org.beatonma.commons.compose.util.ComposableBlock
+import org.beatonma.commons.compose.util.size
 import org.beatonma.commons.theme.compose.Padding
+import org.beatonma.commons.theme.compose.plus
 
 private const val ALPHA = 0.1F
 
@@ -22,20 +33,25 @@ private const val ALPHA = 0.1F
 fun HorizontalSeparator(
     modifier: Modifier = Modifier,
     color: Color = AmbientContentColor.current.copy(alpha = ALPHA),
+    text: ComposableBlock? = null,
 ) {
-
-    Box(
-        Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Separator(
-            color = color,
-            modifier = Modifier
-                .padding(Padding.HorizontalSeparator)
-                .preferredHeight(1.dp)
-                .fillMaxWidth(0.25F)
-                .then(modifier)
-        )
+    if (text == null) {
+        Box(
+            Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Separator(
+                color = color,
+                modifier = Modifier
+                    .padding(Padding.HorizontalSeparator)
+                    .preferredHeight(1.dp)
+                    .fillMaxWidth(0.25F)
+                    .then(modifier),
+            )
+        }
+    }
+    else {
+        HorizontalSeparatorWithContent(color, modifier, content = text)
     }
 }
 
@@ -60,6 +76,61 @@ fun VerticalSeparator(
 }
 
 @Composable
-private fun Separator(color: Color, modifier: Modifier = Modifier) {
+private fun Separator(color: Color, modifier: Modifier) {
     Spacer(modifier.background(color))
+}
+
+@Composable
+private fun HorizontalSeparatorWithContent(
+    color: Color,
+    modifier: Modifier,
+    layoutDirection: LayoutDirection = AmbientLayoutDirection.current,
+    content: @Composable () -> Unit,
+) {
+    val alignment = Alignment.Center
+    val lineModifier = Modifier
+        .padding(Padding.HorizontalSeparator + Padding.VerticalSeparator)
+        .preferredHeight(1.dp)
+
+    Providers(
+        AmbientTextStyle provides typography.caption,
+        AmbientContentColor provides color,
+    ) {
+        Layout(
+            modifier = modifier,
+            content = {
+                Separator(color, lineModifier)
+                content()
+                Separator(color, lineModifier)
+            }
+        ) { measurables, constraints ->
+            check(measurables.size == 3)
+
+            val contentConstraints = constraints.copy(maxWidth = (constraints.maxWidth.toFloat() * .7F).toInt())
+            val contentPlaceable = measurables[1].measure(contentConstraints)
+
+            val lineLength = (constraints.maxWidth - contentPlaceable.width) / 2
+            val lineConstraints = Constraints.fixedWidth(lineLength)
+            val before = measurables[0].measure(lineConstraints)
+            val after = measurables[2].measure(lineConstraints)
+
+            val width: Int = constraints.maxWidth
+            val height: Int = contentPlaceable.height
+            val availableSize = IntSize(width, height)
+
+            val lineY = alignment.align(
+                before.size,
+                availableSize,
+                layoutDirection
+            ).y
+
+            layout(width, height) {
+                before.placeRelative(0, lineY)
+
+                contentPlaceable.placeRelative(lineLength, 0)
+
+                after.placeRelative(lineLength + contentPlaceable.width, lineY)
+            }
+        }
+    }
 }
