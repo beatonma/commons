f653d8e0c307012528dbede79e1e0597d228d4a2
Michael Beaton <beatonma@gmail.com>
2021-03-08 20:14:36 +0000

    Added setUp { } block - functionally the same as perform { } but helps visually separate actions that get the UI in the right start state and those that are directly associated with the test.

--- testcompose/src/main/java/org/beatonma/commons/testcompose/test/ComposeTest.kt
+++ testcompose/src/main/java/org/beatonma/commons/testcompose/test/ComposeTest.kt
@@ -1,18 +1,35 @@
 package org.beatonma.commons.testcompose.test
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Rule
 
 
 abstract class ComposeTest {
     @get:Rule
-    val composeTestRule: ComposeTestRule = createComposeRule()
+    val composeTestRule: ComposeContentTestRule = createComposeRule()
 
-    abstract fun withContent(content: @Composable () -> Unit): ComposeTestRule
+    /**
+     * Set content for [composeTestRule].
+     */
+    abstract fun withContent(content: @Composable () -> Unit): ComposeContentTestRule
 
-    infix fun perform(actions: ComposeTestRule.() -> Unit) {
+    /**
+     * Structural helper. Run the given [actions] on [composeTestRule] to get the UI to
+     * the state required for the test. Assertions should not be made here - see [perform].
+     */
+    infix fun setUp(actions: ComposeContentTestRule.() -> Unit) {
+        composeTestRule.actions()
+    }
+
+    /**
+     * Run the given [actions] on composeTestRule.
+     *
+     * Run any gestures that are directly related to what you are testing and make
+     * your assertions here.
+     */
+    infix fun perform(actions: ComposeContentTestRule.() -> Unit) {
         composeTestRule.actions()
     }
 }
