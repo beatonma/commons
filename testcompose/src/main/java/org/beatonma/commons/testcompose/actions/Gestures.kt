8abdfc5d9b5ce7d23086da7e5c82317dd489625c
Michael Beaton <beatonma@gmail.com>
2021-03-11 16:23:29 +0000

    Migrating Compose alpha10 -> beta01.

--- testcompose/src/main/java/org/beatonma/commons/testcompose/actions/Gestures.kt
+++ testcompose/src/main/java/org/beatonma/commons/testcompose/actions/Gestures.kt
@@ -4,30 +4,29 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.test.GestureScope
 import androidx.compose.ui.test.center
 import androidx.compose.ui.test.swipe
-import androidx.compose.ui.unit.milliseconds
 
-fun GestureScope.swipeUp(distance: Float, start: Offset = center, durationMillis: Int = 200) {
+fun GestureScope.swipeUp(distance: Float, start: Offset = center, durationMillis: Long = 200) {
     val end = Offset(start.x, start.y - distance)
     println("swipeUp($distance) $start -> $end")
-    swipe(start, end, durationMillis.milliseconds)
+    swipe(start, end, durationMillis)
 }
 
-fun GestureScope.swipeDown(distance: Float, start: Offset = center, durationMillis: Int = 200) {
+fun GestureScope.swipeDown(distance: Float, start: Offset = center, durationMillis: Long = 200) {
     val end = Offset(start.x, start.y + distance)
     println("swipeDown($distance) $start -> $end")
-    swipe(start, end, durationMillis.milliseconds)
+    swipe(start, end, durationMillis)
 }
 
-fun GestureScope.swipeLeft(distance: Float, start: Offset = center, durationMillis: Int = 200) {
+fun GestureScope.swipeLeft(distance: Float, start: Offset = center, durationMillis: Long = 200) {
     val x = start.x - distance
     val end = Offset(x, start.y)
     println("swipeLeft($distance) $start -> $end")
-    swipe(start, end, durationMillis.milliseconds)
+    swipe(start, end, durationMillis)
 }
 
-fun GestureScope.swipeRight(distance: Float, start: Offset = center, durationMillis: Int = 200) {
+fun GestureScope.swipeRight(distance: Float, start: Offset = center, durationMillis: Long = 200) {
     val x = start.x + distance
     val end = Offset(x, start.y)
     println("swipeRight($distance) $start -> $end")
-    swipe(start, end, durationMillis.milliseconds)
+    swipe(start, end, durationMillis)
 }
