9e664abbee1885c0d7fa70f8704126a680501f67
Michael Beaton <beatonma@gmail.com>
2021-07-28 19:51:13 +0100

    Dependencies and plugins updated.

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,15 +1,15 @@
-import org.beatonma.commons.buildsrc.kts.extensions.main
+import org.beatonma.commons.buildsrc.gradle.main
 
 plugins {
-    id(Plugins.COMMONS_LIBRARY_CONFIG)
-    id(Plugins.COMMONS_COMPOSE_MODULE)
+    id(Plugins.Commons.COMMONS_LIBRARY_CONFIG)
+    id(Plugins.Commons.COMMONS_COMPOSE_MODULE)
 }
 
 dependencies {
     main {
         implementations(
-            Dependencies.AndroidX.CORE_KTX,
-            Dependencies.AndroidX.CONSTRAINTLAYOUT_COMPOSE,
+            Dependencies.Jetpack.CORE_KTX,
+            Dependencies.Jetpack.CONSTRAINTLAYOUT_COMPOSE,
             Dependencies.Accompanist.INSETS,
             project(Modules.Core),
             project(Modules.Theme)
