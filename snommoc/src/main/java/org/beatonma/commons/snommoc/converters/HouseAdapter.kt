3f3d49dd1a5648d027ce1a1ea76e831b314856ea
Michael Beaton <beatonma@gmail.com>
2021-07-28 19:48:47 +0100

    Replaced deprecated String.toLowerCase/capitalize

--- snommoc/src/main/java/org/beatonma/commons/snommoc/converters/HouseAdapter.kt
+++ snommoc/src/main/java/org/beatonma/commons/snommoc/converters/HouseAdapter.kt
@@ -5,18 +5,22 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonReader
 import com.squareup.moshi.JsonWriter
 import org.beatonma.commons.core.House
-import java.util.*
+import java.util.Locale
 
 class HouseAdapter: JsonAdapter<House>() {
     @FromJson
-    override fun fromJson(reader: JsonReader): House? {
-        return House.valueOf(reader.nextString().toLowerCase(Locale.ROOT))
+    override fun fromJson(reader: JsonReader): House {
+        return House.valueOf(reader.nextString().lowercase(Locale.ROOT))
     }
 
     override fun toJson(writer: JsonWriter, value: House?) {
         when (value) {
             null -> writer.nullValue()
-            else -> writer.value(value.name.capitalize(Locale.ROOT))
+            else -> writer.value(value.name.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase(
+                    Locale.ROOT
+                ) else it.toString()
+            })
         }
     }
 }
