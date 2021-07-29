4036c49b89c31c89b6c7e70537a955998c26eb7c
Michael Beaton <beatonma@gmail.com>
2021-07-29 14:59:51 +0100

    All gradle plugin names are now defined as constants in Plugins object.

--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -1,32 +1,32 @@
 object Plugins {
     object Commons {
         /**
-         * [org.beatonma.commons.buildsrc.kts.plugin.CommonsApplicationModule]
+         * [org.beatonma.commons.buildsrc.gradle.plugins.CommonsApplicationModule]
          * Apply standard plugins, android configuration block, repositories and basic dependencies.
          */
         const val COMMONS_APPLICATION_CONFIG = "commons-application-module"
 
         /**
-         * [org.beatonma.commons.buildsrc.kts.plugin.CommonsLibraryModule]
+         * [org.beatonma.commons.buildsrc.gradle.plugins.CommonsLibraryModule]
          * Apply standard plugins, android configuration block, repositories and basic dependencies.
          */
         const val COMMONS_LIBRARY_CONFIG = "commons-library-module"
 
         /**
          * Apply required Dagger/Hilt plugins and dependencies.
-         * [org.beatonma.commons.buildsrc.kts.plugin.CommonsHiltModule]
+         * [org.beatonma.commons.buildsrc.gradle.plugins.CommonsHiltModule]
          */
         const val COMMONS_HILT_MODULE = "commons-hilt-module"
 
         /**
          * Apply required Room dependencies.
-         * [org.beatonma.commons.buildsrc.kts.plugin.CommonsRoomModule]
+         * [org.beatonma.commons.buildsrc.gradle.plugins.CommonsRoomModule]
          */
         const val COMMONS_ROOM_MODULE = "commons-room-module"
 
         /**
          * Apply required Jetpack Compose dependencies and android configuration block.
-         * [org.beatonma.commons.buildsrc.kts.plugin.CommonsComposeModule]
+         * [org.beatonma.commons.buildsrc.gradle.plugins.CommonsComposeModule]
          */
         const val COMMONS_COMPOSE_MODULE = "commons-compose-module"
     }
