a597e567e0d541b2b944f16f35daab1eba2d221e
Michael Beaton <beatonma@gmail.com>
2021-08-19 17:33:19 +0100

    Minor refactor.

--- themepreview/src/main/java/org/beatonma/compose/themepreview/ThemePreview.kt
+++ themepreview/src/main/java/org/beatonma/compose/themepreview/ThemePreview.kt
@@ -10,27 +10,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.FabPosition
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
+import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.MaterialTheme.shapes
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Android
-import androidx.compose.material.icons.filled.ColorLens
-import androidx.compose.material.icons.filled.InvertColors
-import androidx.compose.material.icons.filled.TextFields
-import androidx.compose.material.icons.filled.Widgets
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontFamily
@@ -100,11 +87,10 @@ fun ThemePreview(
     val screenOverride =
         if (currentScreen == Screen.Custom && customScreen != null) {
             customScreen
-        }
-        else null
+        } else null
 
-    Crossfade(isDark, animationSpec = crossfadeAnimSpec) {
-        theme(isDark) {
+    Crossfade(isDark, animationSpec = crossfadeAnimSpec) { dark ->
+        theme(dark) {
             ThemePreview(currentScreen,
                 screenOverride,
                 onScreenChanged,
