f95159694ac56d099bc671cdc0fe6bca23f1c51f
Michael Beaton <beatonma@gmail.com>
2021-08-19 17:45:21 +0100

    Basic activity.

--- app/src/main/java/org/beatonma/commons/kotlin/extensions/Permissions.kt
+++ app/src/main/java/org/beatonma/commons/kotlin/extensions/Permissions.kt
@@ -4,6 +4,19 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 
+private data class PermissionStatus(val name: String, val granted: Boolean)
+class PermissionResults(
+    permissions: Array<out String>,
+    grantResults: IntArray,
+) {
+    private val permissions: List<PermissionStatus> = permissions.zip(grantResults.toTypedArray())
+        .map { PermissionStatus(it.first, it.second == PackageManager.PERMISSION_GRANTED) }
+
+    fun check(permission: String, block: (granted: Boolean) -> Unit) {
+        block(permissions.firstOrNull { it.name == permission }?.granted == true)
+    }
+}
+
 fun Context.hasPermission(requestedPermission: String): Boolean =
     ContextCompat.checkSelfPermission(
         this,
