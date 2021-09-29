c95b5f7fb463c8ce92f5f8db5c783f75f6d5825a
Michael Beaton <beatonma@gmail.com>
2021-09-29 18:04:41 +0100

    Checkpoint using animation for measuring limits(!)

--- compose-core/src/main/java/org/beatonma/commons/compose/components/newcollapsibleheader/CollapsibleHeaderLayout.kt
+++ compose-core/src/main/java/org/beatonma/commons/compose/components/newcollapsibleheader/CollapsibleHeaderLayout.kt
@@ -1,5 +1,7 @@
 package org.beatonma.commons.compose.components.newcollapsibleheader
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -23,22 +25,28 @@ fun NewCollapsibleHeaderLayout(
     content: LazyListScope.() -> Unit,
     state: LazyListState = rememberLazyListState(),
 ) {
-    var expansion by remember { mutableStateOf(1f) }
+    var expansion by remember { mutableStateOf(0f) }
+    val expansionAnim by animateFloatAsState(targetValue = expansion, animationSpec = tween(20))
+
+
+    var limitsMeasured by remember { mutableStateOf(false) }
 
     var minHeight by remember { mutableStateOf(-1) }
     var maxHeight by remember { mutableStateOf(0) }
 
+    LaunchedEffect(minHeight) {
+        expansion = 1f
+    }
+
     LaunchedEffect(state.firstVisibleItemScrollOffset) {
+        if (minHeight < 0 || maxHeight < 0) {
+            return@LaunchedEffect
+        }
+
         val firstItemIndex = state.firstVisibleItemIndex
         val firstItemOffset = state.firstVisibleItemScrollOffset
 
         if (firstItemIndex == 0) {
-            val layoutInfo = state.layoutInfo
-
-            if (layoutInfo.totalItemsCount > 1) {
-                layoutInfo.visibleItemsInfo
-            }
-
             if (maxHeight != 0) {
                 expansion = 1f - (firstItemOffset / maxHeight.toFloat())
                 check(expansion in 0f..1f) { "Invalid expansion $expansion should be in 0..1f" }
@@ -53,7 +61,7 @@ fun NewCollapsibleHeaderLayout(
         stickyHeader {
             Layout(
                 content = {
-                    header(expansion)
+                    header(if (limitsMeasured) expansion else expansionAnim)
                 },
             ) { measurables, constraints ->
                 val placeables = if (maxHeight == 0 || minHeight < 0) {
@@ -72,9 +80,19 @@ fun NewCollapsibleHeaderLayout(
                 val height: Int = placeables.maxOf(Placeable::height)
 
                 when {
-                    maxHeight == 0 -> maxHeight = height
-                    expansion == 0f -> minHeight = height
+                    limitsMeasured -> { /* NOOP */
+                    }
+                    maxHeight == 0 && expansionAnim == 1f -> {
+                        maxHeight = height
+                        if (minHeight >= 0) limitsMeasured = true
+                    }
+                    expansionAnim == 0f -> {
+                        minHeight = height
+                        if (maxHeight > 0) limitsMeasured = true
+                    }
                 }
+
+                println("$minHeight | $maxHeight")
 
                 layout(width, maxHeight) {
                     placeables.forEach { it.placeRelative(0, 0) }
