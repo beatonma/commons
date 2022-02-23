3a3d0748d5ffe7563bfd4fc10ba12327d102e626
Michael Beaton <beatonma@gmail.com>
2022-02-23 11:29:44 +0000

    Removed BackPressConsumer

--- app/src/main/java/org/beatonma/commons/app/MainActivity.kt
+++ app/src/main/java/org/beatonma/commons/app/MainActivity.kt
@@ -4,13 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.beatonma.commons.R
 import org.beatonma.commons.app.ui.base.DayNightActivity
-import org.beatonma.commons.app.ui.navigation.BackPressConsumer
 import org.beatonma.commons.app.ui.screens.signin.LocalPlatformUserAccountActions
 import org.beatonma.commons.app.ui.screens.signin.PlatformUserAccountActions
 import org.beatonma.commons.app.ui.screens.signin.UserAccountViewModel
@@ -30,12 +28,12 @@ class MainActivity : DayNightActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         platformUserAccountActions = userProfileActions(this, accountViewModel)
-//
+
         setContentView(R.layout.activity_main)
         setDecorFitsSystemWindows(false)
-//
+
         navController = findNavController(R.id.nav_host_fragment)
-//
+
         handleIntent(intent)
     }
 
@@ -53,24 +51,4 @@ class MainActivity : DayNightActivity() {
         setIntent(intent)
         handleIntent(intent)
     }
-
-    override fun onBackPressed() {
-        val fragment = getContentFragment()
-        val consumedByContent = fragment is BackPressConsumer && fragment.onBackPressed()
-        if (!consumedByContent) {
-            super.onBackPressed()
-        }
-    }
-
-    /**
-     * FragmentManager instance that is responsible for navController content
-     */
-    private fun getNavigationFragmentManager() =
-        supportFragmentManager.primaryNavigationFragment?.childFragmentManager
-
-    /**
-     * Return the fragment that is currently the navController destination.
-     */
-    private fun getContentFragment(): Fragment? =
-        getNavigationFragmentManager()?.primaryNavigationFragment  // Foreground fragment
 }
