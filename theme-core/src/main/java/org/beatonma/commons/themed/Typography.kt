fb27a4ff434169221c8b94b35dae370aa1ffd865
Michael Beaton <beatonma@gmail.com>
2022-03-10 17:58:28 +0000

    Added Typography extension values.
    These provide a subset of the available styles with slightly more
    descriptive names.

---
+++ theme-core/src/main/java/org/beatonma/commons/themed/Typography.kt
@@ -0,0 +1,11 @@
+package org.beatonma.commons.themed
+
+import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+
+
+val Typography.titleLarge @Composable get() = h4
+val Typography.titleMedium @Composable get() = h5
+val Typography.titleSmall @Composable get() = h6
+
+val Typography.small @Composable get() = caption
