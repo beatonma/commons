745111f7fdcb06eba246afec2e0ce208cd1b389e
Michael Beaton <beatonma@gmail.com>
2022-03-18 19:20:33 +0000

    Removed obsolete/deleted CollapsibleHeaderLayoutTest.

--- compose-core/src/androidTest/java/org/beatonma/commons/compose/components/Suite.kt
+++ compose-core/src/androidTest/java/org/beatonma/commons/compose/components/Suite.kt
@@ -1,6 +1,5 @@
 package org.beatonma.commons.compose.components
 
-import org.beatonma.commons.compose.components.collapsibleheader.CollapsibleHeaderLayoutTest
 import org.beatonma.commons.compose.components.fabbottomsheet.FabBottomSheetTest
 import org.beatonma.commons.compose.components.text.TextTestSuite
 import org.junit.runner.RunWith
@@ -10,7 +9,6 @@ import org.junit.runners.Suite
 @Suite.SuiteClasses(
     CollapsedColumnTest::class,
     CollapsibleChipTest::class,
-    CollapsibleHeaderLayoutTest::class,
     DoubleConfirmationButtonTest::class,
     FabBottomSheetTest::class,
     FeedbackMessageLayoutTest::class,
