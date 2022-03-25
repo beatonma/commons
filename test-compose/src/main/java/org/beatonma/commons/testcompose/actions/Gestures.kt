d88afb7fbc6cbde51790cc94a9127392bd027906
Michael Beaton <beatonma@gmail.com>
2022-03-25 18:07:30 +0000

    GestureScope -> TouchInjectionScope

--- test-compose/src/main/java/org/beatonma/commons/testcompose/actions/Gestures.kt
+++ test-compose/src/main/java/org/beatonma/commons/testcompose/actions/Gestures.kt
@@ -1,30 +1,45 @@
 package org.beatonma.commons.testcompose.actions
 
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.test.GestureScope
-import androidx.compose.ui.test.center
+import androidx.compose.ui.test.TouchInjectionScope
 import androidx.compose.ui.test.swipe
 
-fun GestureScope.swipeUp(distance: Float, start: Offset = center, durationMillis: Long = 200) {
+fun TouchInjectionScope.swipeUp(
+    distance: Float,
+    start: Offset = center,
+    durationMillis: Long = 200,
+) {
     val end = Offset(start.x, start.y - distance)
     println("swipeUp($distance) $start -> $end")
     swipe(start, end, durationMillis)
 }
 
-fun GestureScope.swipeDown(distance: Float, start: Offset = center, durationMillis: Long = 200) {
+fun TouchInjectionScope.swipeDown(
+    distance: Float,
+    start: Offset = center,
+    durationMillis: Long = 200,
+) {
     val end = Offset(start.x, start.y + distance)
     println("swipeDown($distance) $start -> $end")
     swipe(start, end, durationMillis)
 }
 
-fun GestureScope.swipeLeft(distance: Float, start: Offset = center, durationMillis: Long = 200) {
+fun TouchInjectionScope.swipeLeft(
+    distance: Float,
+    start: Offset = center,
+    durationMillis: Long = 200,
+) {
     val x = start.x - distance
     val end = Offset(x, start.y)
     println("swipeLeft($distance) $start -> $end")
     swipe(start, end, durationMillis)
 }
 
-fun GestureScope.swipeRight(distance: Float, start: Offset = center, durationMillis: Long = 200) {
+fun TouchInjectionScope.swipeRight(
+    distance: Float,
+    start: Offset = center,
+    durationMillis: Long = 200,
+) {
     val x = start.x + distance
     val end = Offset(x, start.y)
     println("swipeRight($distance) $start -> $end")
